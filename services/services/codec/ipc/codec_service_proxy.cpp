3d26587509090081174b5dcfcfc33cb4b5395547
li-jianchao1993 <lijianchao9@huawei.com>
2023-06-24 19:15:19 +0800

    【修改说明】 修改框架侧编译错误
    Signed-off-by: li-jianchao1993 <lijianchao9@huawei.com>

--- services/services/codec/ipc/codec_service_proxy.cpp
+++ services/services/codec/ipc/codec_service_proxy.cpp
@@ -26,60 +26,6 @@ namespace {
 
 namespace OHOS {
 namespace MediaAVCodec {
-class CodecServiceProxy::CodecBufferCache : public NoCopyable {
-public:
-    CodecBufferCache() = default;
-    ~CodecBufferCache() = default;
-
-    int32_t ReadFromParcel(uint32_t index, MessageParcel &parcel, std::shared_ptr<AVSharedMemory> &memory)
-    {
-        std::lock_guard<std::mutex> lock(mutex_);
-        auto iter = caches_.find(index);
-        CacheFlag flag = static_cast<CacheFlag>(parcel.ReadUint8());
-        if (flag == CacheFlag::HIT_CACHE) {
-            if (iter == caches_.end()) {
-                AVCODEC_LOGE("Mark hit cache, but can find the index's cache, index: %{public}u", index);
-                return AVCS_ERR_INVALID_VAL;
-            }
-            memory = iter->second;
-            return AVCS_ERR_OK;
-        }
-
-        if (flag == CacheFlag::UPDATE_CACHE) {
-            memory = ReadAVSharedMemoryFromParcel(parcel);
-            CHECK_AND_RETURN_RET_LOG(memory != nullptr, AVCS_ERR_INVALID_VAL, "Read memory from parcel failed");
-
-            if (iter == caches_.end()) {
-                AVCODEC_LOGI("Add cache, index: %{public}u", index);
-                caches_.emplace(index, memory);
-            } else {
-                iter->second = memory;
-                AVCODEC_LOGI("Update cache, index: %{public}u", index);
-            }
-            return AVCS_ERR_OK;
-        }
-
-        // invalidate cache flag
-        if (iter != caches_.end()) {
-            iter->second = nullptr;
-            caches_.erase(iter);
-        }
-        memory = nullptr;
-        AVCODEC_LOGE("Invalidate cache for index: %{public}u, flag: %{public}hhu", index, flag);
-        return AVCS_ERR_INVALID_VAL;
-    }
-
-private:
-    std::mutex mutex_;
-    enum CacheFlag : uint8_t {
-        HIT_CACHE = 1,
-        UPDATE_CACHE,
-        INVALIDATE_CACHE,
-    };
-
-    std::unordered_map<uint32_t, std::shared_ptr<AVSharedMemory>> caches_;
-};
-
 CodecServiceProxy::CodecServiceProxy(const sptr<IRemoteObject> &impl)
     : IRemoteProxy<IStandardCodecService>(impl)
 {
