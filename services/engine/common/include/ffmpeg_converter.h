5f6cda9d3f9548f19f4911085d24525c1ae59074
yangjunhui10 <yangjunhui10@huawei.com>
2023-09-25 03:16:18 +0000

    add:amr、mkv、h265、metadata
    Signed-off-by: yangjunhui10 <yangjunhui10@huawei.com>

--- services/engine/common/include/ffmpeg_converter.h
+++ services/engine/common/include/ffmpeg_converter.h
@@ -45,7 +45,6 @@ public:
     static AudioSampleFormat ConvertFFMpegAVCodecIdToOHAudioFormat(AVCodecID codecId);
     static AudioSampleFormat ConvertFFMpegToOHAudioFormat(AVSampleFormat ffSampleFormat);
     static AVSampleFormat ConvertOHAudioFormatToFFMpeg(AudioSampleFormat sampleFormat);
-    static AudioSampleFormat ConvertFFMpegAVCodecIdToOHAudioFormat(AVCodecID codecId);
     static AudioChannelLayout ConvertFFToOHAudioChannelLayout(uint64_t ffChannelLayout);
     static uint64_t ConvertOHAudioChannelLayoutToFFMpeg(AudioChannelLayout channelLayout);
     static std::string_view ConvertOHAudioChannelLayoutToString(AudioChannelLayout layout);
