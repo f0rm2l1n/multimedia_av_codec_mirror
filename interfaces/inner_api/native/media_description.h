537bc806f262cdea6f757336edb3e150c93c0057
West <weihouyao@huawei.com>
2023-10-07 01:13:31 +0000

    Revert "!543 Add hardware codec max_frequency_mode key"
    Signed-off-by: West <weihouyao@huawei.com>
    Change-Id: I9dd48da69fb55f5f1d6bb2e43c0eeac92d7a7e1f

--- interfaces/inner_api/native/media_description.h
+++ interfaces/inner_api/native/media_description.h
@@ -238,11 +238,6 @@ public:
      */
     static constexpr std::string_view MD_KEY_VIDEO_DELAY = "video_delay";
 
-    /**
-     * Key for video decoder or encoder max frequency mode, value type is int32_t
-     */
-    static constexpr std::string_view MD_MAX_FREQUENCY_MODE = "max_frequency_mode";
-
 private:
     MediaDescriptionKey() = delete;
     ~MediaDescriptionKey() = delete;
