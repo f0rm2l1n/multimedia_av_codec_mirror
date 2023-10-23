54c94cc846a5522995955ab27e336c12964c7725
gao_ziyu <gaoziyu@huawei.com>
2023-10-23 20:17:47 +0800

    fix bug
    Signed-off-by: gao_ziyu <gaoziyu@huawei.com>

--- interfaces/inner_api/native/audio_base_codec_ext.h
+++ interfaces/inner_api/native/audio_base_codec_ext.h
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #ifndef AUDIO_BASE_ENCODER_PLUGIN
 #define AUDIO_BASE_ENCODER_PLUGIN
 #include <string>
