f1b8a64c0b693628ef4d018cf2cf49e5fa756fe9
shuboxu <xushubo1@huawei.com>
2023-11-13 11:59:06 +0000

    !629 添加muxer 原子能力和plugin
    Signed-off-by: xushubo1@huawei.com <xushubo1@huawei.com>

---
+++ interfaces/inner_api/native/media_muxer.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AVCODEC_MEDIA_MUXER_H
+#define AVCODEC_MEDIA_MUXER_H
+
+#include <vector>
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include "buffer/avbuffer_queue.h"
+#include "buffer/avbuffer_queue_define.h"
+#include "plugin/muxer_plugin.h"
+
+namespace OHOS {
+namespace Media {
+class MediaMuxer {
+public:
+    MediaMuxer(int32_t appUid, int32_t appPid);
+    ~MediaMuxer();
+    Status Init(int32_t fd, Plugin::OutputFormat format);
+    Status SetParameter(const std::shared_ptr<Meta> &param);
+    Status AddTrack(int32_t &trackIndex, const std::shared_ptr<Meta> &trackDesc);
+    sptr<AVBufferQueueProducer> GetInputBufferQueue(uint32_t trackIndex);
+    Status Start();
+    Status Stop();
+    Status Reset();
+
+private:
+    enum class State {
+        UNINITIALIZED,
+        INITIALIZED,
+        STARTED,
+        STOPPED
+    };
+
+    std::shared_ptr<Plugin::MuxerPlugin> CreatePlugin(Plugin::OutputFormat format);
+    bool CanAddTrack(const std::string &mimeType);
+    bool CheckKeys(const std::string &mimeType, const std::shared_ptr<Meta> &trackDesc);
+    std::string StateConvert(State state);
+
+    class Track : public IConsumerListener {
+    public:
+        Track() {};
+        virtual ~Track();
+        void Start();
+        void Stop() noexcept;
+        void ThreadProcessor();
+        void SetBufferAvailable(bool isAvailable);
+        void OnBufferAvailable() override;
+    public:
+        int32_t trackId_ = -1;
+        std::string mimeType_ = {};
+        std::shared_ptr<Meta> trackDesc_ = nullptr;
+        sptr<AVBufferQueueProducer> producer_ = nullptr;
+        sptr<AVBufferQueueConsumer> consumer_ = nullptr;
+        std::shared_ptr<AVBufferQueue> bufferQ_ = nullptr;
+        std::shared_ptr<Plugin::MuxerPlugin> muxer_ = nullptr;
+    private:
+        std::mutex mutex_;
+        std::condition_variable condBufferAvailable_;
+        std::condition_variable condBufferEmpty_;
+        std::atomic<bool> isEmpty_ = true;
+        std::atomic<bool> isBufferAvailable_ = false;
+        std::unique_ptr<std::thread> thread_ = nullptr;
+        bool isThreadExit_ = true;
+    };
+
+    int32_t appUid_ = -1;
+    int32_t appPid_ = -1;
+    int64_t fd_ = -1;
+    Plugin::OutputFormat format_;
+    std::atomic<State> state_ = State::UNINITIALIZED;
+    std::shared_ptr<Plugin::MuxerPlugin> muxer_ = nullptr;
+    std::vector<sptr<Track>> tracks_;
+    std::mutex mutex_;
+};
+} // namespace Media
+} // namespace OHOS
+#endif // AVCODEC_MEDIA_MUXER_H
