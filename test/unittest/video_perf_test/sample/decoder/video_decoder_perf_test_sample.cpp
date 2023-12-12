dce8f8a879a6248cf1f7f6c7f5677e0058f8f18f
West <weihouyao@huawei.com>
2023-12-12 21:38:09 +0800

    【修改说明】修复 buffer 创建 surface 的错误
    Signed-off-by: West <weihouyao@huawei.com>
    Change-Id: Ife71e155f6be87e2c1ed76ff98e1faf885f7dd7f

--- test/unittest/video_perf_test/sample/decoder/video_decoder_perf_test_sample.cpp
+++ test/unittest/video_perf_test/sample/decoder/video_decoder_perf_test_sample.cpp
@@ -70,8 +70,10 @@ int32_t VideoDecoderPerfTestSample::Create(SampleInfo sampleInfo)
     CHECK_AND_RETURN_RET_LOG(ret == AVCODEC_SAMPLE_ERR_OK, ret, "Create video decoder failed");
 
     context_ = new CodecUserData;
-    ret = CreateWindow(sampleInfo_.window);
-    CHECK_AND_RETURN_RET_LOG(ret == AVCODEC_SAMPLE_ERR_OK, ret, "Create window failed");
+    if (!(sampleInfo_.codecRunMode & 0b00)) { // 0b00: Buffer mode mask
+        ret = CreateWindow(sampleInfo_.window);
+        CHECK_AND_RETURN_RET_LOG(ret == AVCODEC_SAMPLE_ERR_OK, ret, "Create window failed");
+    }
     ret = videoDecoder_->Config(sampleInfo_, context_);
     CHECK_AND_RETURN_RET_LOG(ret == AVCODEC_SAMPLE_ERR_OK, ret, "Decoder config failed");
 
