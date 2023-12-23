ab53d4b1b8b42fb51e96eb4d420cfdf342681918
li-jianchao1993 <lijianchao9@huawei.com>
2023-12-23 17:56:41 +0800

    【修改说明】 去除CLIENT_SUPPORT_CODEC相关宏代码
    Signed-off-by: li-jianchao1993 <lijianchao9@huawei.com>

--- services/services/codec/server/codec_factory.cpp
+++ services/services/codec/server/codec_factory.cpp
@@ -23,10 +23,8 @@
 #include "codeclist_core.h"
 #include "codeclist_utils.h"
 #include "meta/format.h"
-#ifndef CLIENT_SUPPORT_CODEC
 #include "fcodec.h"
 #include "hcodec_loader.h"
-#endif
 namespace {
 constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_DOMAIN, "CodecFactory"};
 }
@@ -66,18 +64,15 @@ std::shared_ptr<CodecBase> CodecFactory::CreateCodecByName(const std::string &na
     CodecType codecType = codecListCore->FindCodecType(name);
     std::shared_ptr<CodecBase> codec = nullptr;
     switch (codecType) {
-#ifndef CLIENT_SUPPORT_CODEC
         case CodecType::AVCODEC_HCODEC:
             codec = HCodecLoader::CreateByName(name);
             break;
         case CodecType::AVCODEC_VIDEO_CODEC:
             codec = std::make_shared<Codec::FCodec>(name);
             break;
-#else
         case CodecType::AVCODEC_AUDIO_CODEC:
             codec = std::make_shared<AudioCodecAdapter>(name);
             break;
-#endif
         default:
             AVCODEC_LOGE("Create codec %{public}s failed", name.c_str());
             return codec;
