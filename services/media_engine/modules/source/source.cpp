3180abe2a3feb5a03cc765db72f82a2facaab173
kaaryachen <chenhang35@huawei.com>
2023-12-28 12:04:24 +0800

    datasource
    Signed-off-by: kaaryachen <chenhang35@huawei.com>
    Change-Id: I927c66137c69360475f21e1e0b98e65ce23c3091

--- services/media_engine/modules/source/source.cpp
+++ services/media_engine/modules/source/source.cpp
@@ -343,6 +343,10 @@ Status Source::FindPlugin(const std::shared_ptr<MediaSource>& source)
     auto nameList = pluginManager.ListPlugins(PluginType::SOURCE);
     for (const std::string& name : nameList) {
         std::shared_ptr<PluginInfo> info = pluginManager.GetPluginInfo(PluginType::SOURCE, name);
+        if (info == nullptr) {
+            MEDIA_LOG_W("info is null.");
+            continue;
+        }
         MEDIA_LOG_I("name: " PUBLIC_LOG_S ", info->name: " PUBLIC_LOG_S, name.c_str(), info->name.c_str());
         auto val = info->extra[PLUGIN_INFO_EXTRA_PROTOCOL];
         if (Any::IsSameTypeWith<std::vector<ProtocolType>>(val)) {
