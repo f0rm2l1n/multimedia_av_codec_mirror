979b981ec89a4038094c31be2e2531ee21d9b4a6
xushubo1@huawei.com <xushubo1@huawei.com>
2024-01-13 21:27:11 +0800

    修改本地seek问题
    Signed-off-by: xushubo1@huawei.com <xushubo1@huawei.com>

--- services/media_engine/modules/sink/audio_sink.cpp
+++ services/media_engine/modules/sink/audio_sink.cpp
@@ -245,9 +245,7 @@ bool AudioSink::DoSyncWrite(const std::shared_ptr<OHOS::Media::AVBuffer>& buffer
             MEDIA_LOG_W("failed to get latency");
         }
         if (syncCenter) {
-            const uint64_t latencyFix = 80 * HST_USECOND;
-            render = syncCenter->UpdateTimeAnchor(nowCt + latency + latencyFix,
-                buffer->pts_ - firstPts_, buffer->duration_, this);
+            render = syncCenter->UpdateTimeAnchor(nowCt + latency, buffer->pts_ - firstPts_, buffer->duration_, this);
         }
         lastReportedClockTime_ = nowCt;
         forceUpdateTimeAnchorNextTime_ = true;
