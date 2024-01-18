693af60c5ff8ef54683db3c4bef9b60bf71e16d7
mediaTest <wangyang453@huawei.com>
2024-01-18 09:26:34 +0800

    修改端到端demo，hdr编解码用例编解码器start顺序
    Signed-off-by: mediaTest <wangyang453@huawei.com>

--- test/nativedemo/e2e_demo/avcodec_e2e_demo.cpp
+++ test/nativedemo/e2e_demo/avcodec_e2e_demo.cpp
@@ -258,8 +258,8 @@ void AVCodecE2EDemo::Start()
     OH_VideoDecoder_Prepare(dec);
     OH_VideoEncoder_Prepare(enc);
     OH_AVMuxer_Start(muxer);
-    OH_VideoDecoder_Start(dec);
     OH_VideoEncoder_Start(enc);
+    OH_VideoDecoder_Start(dec);
     if (audioTrackID != -1) {
         audioThread = make_unique<thread>(&AVCodecE2EDemo::WriteAudioTrack, this);
     }
