5f3d352488ced620163939cde54a854e097f5d33
simoncat001 <liye87@huawei.com>
2024-01-29 09:45:37 +0800

    补充hls测试用例 继续解决代码门禁
    Signed-off-by: simoncat001 <liye87@huawei.com>

--- test/unittest/plugin_test/hls_media_downloader_unit_test.h
+++ test/unittest/plugin_test/hls_media_downloader_unit_test.h
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,8 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef hls_media_downloader_test_H
-#define hls_media_downloader_test_H
+
+#ifndef HLS_MEDIA_DOWNLOADER_UINT_TEST_H
+#define HLS_MEDIA_DOWNLOADER_UINT_TEST_H
 
 #include "hls_media_downloader.h"
 #include "gtest/gtest.h"
