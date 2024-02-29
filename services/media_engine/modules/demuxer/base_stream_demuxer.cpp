a8c621239b7d47d6368629378dd3bbeb343a6c49
assertsu <subenhui@huawei.com>
2024-02-29 02:03:56 +0000

    直播点播重构
    Signed-off-by: assertsu <subenhui@huawei.com>

---
+++ services/media_engine/modules/demuxer/base_stream_demuxer.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2023-2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define HST_LOG_TAG "BaseStreamDemuxer"
+
+#include "base_stream_demuxer.h"
+
+#include <algorithm>
+#include <map>
+#include <memory>
+
+#include "avcodec_common.h"
+#include "avcodec_trace.h"
+#include "cpp_ext/type_traits_ext.h"
+#include "buffer/avallocator.h"
+#include "common/event.h"
+#include "common/log.h"
+#include "frame_detector.h"
+#include "meta/media_types.h"
+#include "meta/meta.h"
+#include "osal/utils/dump_buffer.h"
+#include "plugin/plugin_buffer.h"
+#include "plugin/plugin_info.h"
+#include "plugin/plugin_manager.h"
+#include "plugin/plugin_time.h"
+
+namespace OHOS {
+namespace Media {
+
+BaseStreamDemuxer::BaseStreamDemuxer()
+    : typeFinder_(nullptr)
+{
+    MEDIA_LOG_I("BaseStreamDemuxer called");
+}
+
+BaseStreamDemuxer::~BaseStreamDemuxer()
+{
+    MEDIA_LOG_I("~BaseStreamDemuxer called");
+    typeFinder_ = nullptr;
+}
+
+void BaseStreamDemuxer::SetSource(std::shared_ptr<Source>& source)
+{
+    source_ = source;
+}
+
+void BaseStreamDemuxer::InitTypeFinder()
+{
+    if (!typeFinder_) {
+        typeFinder_ = std::make_shared<TypeFinder>();
+    }
+}
+
+void BaseStreamDemuxer::SetDemuxerState(DemuxerState state)
+{
+    pluginState_ = state;
+}
+
+} // namespace Media
+} // namespace OHOS
