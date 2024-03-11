b747633928f6dc7bd1aad67ca6c721e75e55d4e5
yinzihao <yinzihao3@huawei.com>
2024-03-11 17:26:53 +0800

    图库在线播放
    Signed-off-by: yinzihao <yinzihao3@huawei.com>

--- services/media_engine/modules/demuxer/vod_stream_demuxer.h
+++ services/media_engine/modules/demuxer/vod_stream_demuxer.h
@@ -57,6 +57,7 @@ private:
     bool PullDataWithoutCache(uint64_t offset, size_t size, std::shared_ptr<Buffer>& bufferPtr);
     bool PullDataWithCache(uint64_t offset, size_t size, std::shared_ptr<Buffer>& bufferPtr);
     bool GetPeekRange(uint64_t offset, size_t size, std::shared_ptr<Buffer>& bufferPtr);
+    bool GetPeekRangeSub(uint64_t offset, size_t size, std::shared_ptr<Buffer>& bufferPtr);
 private:
     struct CacheData {
         std::shared_ptr<Buffer> data = nullptr;
