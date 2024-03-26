17e0c4870c96ea063b6e4bccb5475eecd887d0c1
West <weihouyao@huawei.com>
2024-03-26 16:27:12 +0800

    【修改说明】add param checker
    Signed-off-by: West <weihouyao@huawei.com>
    Change-Id: I6c9baf60807f0103706997b8ef1d7e0ec4e87a79

--- services/services/codec/server/codec_param_checker.cpp
+++ services/services/codec/server/codec_param_checker.cpp
@@ -32,16 +32,16 @@ using namespace OHOS::Media;
 using namespace OHOS::MediaAVCodec;
 
 // Video codec checker
-int32_t WidthChecker(CapabilityData &capData, Format &format);
-int32_t HeightChecker(CapabilityData &capData, Format &format);
-int32_t PixelFormatChecker(CapabilityData &capData, Format &format);
-int32_t FramerateChecker(CapabilityData &capData, Format &format);
-int32_t BitrateAndQualityChecker(CapabilityData &capData, Format &format);
-int32_t VideoProfileChecker(CapabilityData &capData, Format &format);
-int32_t RotaitonChecker(CapabilityData &capData, Format &format);
+int32_t WidthChecker(CapabilityData &capData, Format &format, AVCodecType codecType);
+int32_t HeightChecker(CapabilityData &capData, Format &format, AVCodecType codecType);
+int32_t PixelFormatChecker(CapabilityData &capData, Format &format, AVCodecType codecType);
+int32_t FramerateChecker(CapabilityData &capData, Format &format, AVCodecType codecType);
+int32_t BitrateAndQualityChecker(CapabilityData &capData, Format &format, AVCodecType codecType);
+int32_t VideoProfileChecker(CapabilityData &capData, Format &format, AVCodecType codecType);
+int32_t RotaitonChecker(CapabilityData &capData, Format &format, AVCodecType codecType);
 
 // Checkers list define
-using CheckerType = int32_t (*)(CapabilityData &capData, Format &format);
+using CheckerType = int32_t (*)(CapabilityData &capData, Format &format, AVCodecType codecType);
 using CheckerListType = std::vector<CheckerType>;
 const CheckerListType VIDEO_ENCODER_PARAMS_CHECKER_LIST = {
     WidthChecker,
@@ -67,55 +67,59 @@ const std::unordered_map<AVCodecType, CheckerListType> CHECKERS_TABLE = {
 };
 
 // Checkers implementation
-int32_t WidthChecker(CapabilityData &capData, Format &format)
+int32_t WidthChecker(CapabilityData &capData, Format &format, AVCodecType codecType)
 {
+    (void)codecType;
     int32_t width = 0;
     bool paramExist = format.GetIntValue(MediaDescriptionKey::MD_KEY_WIDTH, width);
-    CHECK_AND_RETURN_RET_LOG(paramExist, -1, "Key param missing, %{public}s",
+    CHECK_AND_RETURN_RET_LOG(paramExist, AVCS_ERR_INVALID_VAL, "Key param missing, %{public}s",
         MediaDescriptionKey::MD_KEY_WIDTH.data());     // Missing width
 
     bool paramValid = capData.width.InRange(width);
-    CHECK_AND_RETURN_RET_LOG(paramValid, -1, "Param invalid, %{public}s: %{public}d",
+    CHECK_AND_RETURN_RET_LOG(paramValid, AVCS_ERR_INVALID_VAL, "Param invalid, %{public}s: %{public}d",
         MediaDescriptionKey::MD_KEY_WIDTH.data(), width);     // Invalid width
 
     AVCODEC_LOGI("Param valid, %{public}s: %{public}d", MediaDescriptionKey::MD_KEY_WIDTH.data(), width);
     return AVCS_ERR_OK;
 }
 
-int32_t HeightChecker(CapabilityData &capData, Format &format)
+int32_t HeightChecker(CapabilityData &capData, Format &format, AVCodecType codecType)
 {
+    (void)codecType;
     int32_t height = 0;
     bool paramExist = format.GetIntValue(MediaDescriptionKey::MD_KEY_HEIGHT, height);
-    CHECK_AND_RETURN_RET_LOG(paramExist, -1, "Key param missing, %{public}s",
+    CHECK_AND_RETURN_RET_LOG(paramExist, AVCS_ERR_INVALID_VAL, "Key param missing, %{public}s",
         MediaDescriptionKey::MD_KEY_HEIGHT.data());     // Missing height
 
     bool paramValid = capData.height.InRange(height);
-    CHECK_AND_RETURN_RET_LOG(paramValid, -1, "Param invalid, %{public}s: %{public}d",
+    CHECK_AND_RETURN_RET_LOG(paramValid, AVCS_ERR_INVALID_VAL, "Param invalid, %{public}s: %{public}d",
         MediaDescriptionKey::MD_KEY_HEIGHT.data(), height);     // Invalid height
 
     AVCODEC_LOGI("Param valid, %{public}s: %{public}d", MediaDescriptionKey::MD_KEY_HEIGHT.data(), height);
     return AVCS_ERR_OK;
 }
 
-int32_t PixelFormatChecker(CapabilityData &capData, Format &format)
+int32_t PixelFormatChecker(CapabilityData &capData, Format &format, AVCodecType codecType)
 {
     int32_t pixelFormat;
     bool paramExist = format.GetIntValue(MediaDescriptionKey::MD_KEY_PIXEL_FORMAT, pixelFormat);
-    CHECK_AND_RETURN_RET_LOG(paramExist, -1, "Key param missing, %{public}s",
-        MediaDescriptionKey::MD_KEY_PIXEL_FORMAT.data());     // Missing pixel format
+    CHECK_AND_RETURN_RET_LOG(!(codecType == AVCODEC_TYPE_VIDEO_ENCODER && !paramExist),
+        AVCS_ERR_INVALID_VAL, "Key param missing for encoder, %{public}s",
+        MediaDescriptionKey::MD_KEY_PIXEL_FORMAT.data());     // Encoder missing pixel format
 
     bool paramValid =
         std::find(capData.pixFormat.begin(), capData.pixFormat.end(), pixelFormat) != capData.pixFormat.end();
-    CHECK_AND_RETURN_RET_LOG(paramValid, -1, "Param invalid, %{public}s: %{public}d",
+    CHECK_AND_RETURN_RET_LOG(paramValid, AVCS_ERR_UNSUPPORT, "Param invalid, %{public}s: %{public}d",
         MediaDescriptionKey::MD_KEY_PIXEL_FORMAT.data(), pixelFormat);     // Invalid pixel format
 
     AVCODEC_LOGI("Param valid, %{public}s: %{public}d", MediaDescriptionKey::MD_KEY_PIXEL_FORMAT.data(), pixelFormat);
     return AVCS_ERR_OK;
 }
 
-int32_t FramerateChecker(CapabilityData &capData, Format &format)
+int32_t FramerateChecker(CapabilityData &capData, Format &format, AVCodecType codecType)
 {
     (void)capData;
+    (void)codecType;
     double framerate;
     bool paramExist = format.GetDoubleValue(MediaDescriptionKey::MD_KEY_FRAME_RATE, framerate);
     if (paramExist == false) {
@@ -123,57 +127,51 @@ int32_t FramerateChecker(CapabilityData &capData, Format &format)
     }
 
     bool paramValid = framerate > 0 ? true : false;
-    CHECK_AND_RETURN_RET_LOG(paramValid, -1, "Param invalid, %{public}s: %{public}.2f",
+    CHECK_AND_RETURN_RET_LOG(paramValid, AVCS_ERR_INVALID_VAL, "Param invalid, %{public}s: %{public}.2f",
         MediaDescriptionKey::MD_KEY_FRAME_RATE.data(), framerate);     // Invalid framerate
 
     AVCODEC_LOGI("Param valid, %{public}s: %{public}.2f", MediaDescriptionKey::MD_KEY_FRAME_RATE.data(), framerate);
     return AVCS_ERR_OK;
 }
 
-int32_t BitrateAndQualityChecker(CapabilityData &capData, Format &format)
+int32_t BitrateAndQualityChecker(CapabilityData &capData, Format &format, AVCodecType codecType)
 {
+    (void)codecType;
     int64_t bitrate;
     int32_t quality;
-    int32_t bitrateMode = VideoEncodeBitrateMode::VBR;
+    int32_t bitrateMode;
     bool bitrateExist = format.GetLongValue(MediaDescriptionKey::MD_KEY_BITRATE, bitrate);
     bool qualityExist = format.GetIntValue(MediaDescriptionKey::MD_KEY_QUALITY, quality);
     bool bitrateModeExist = format.GetIntValue(MediaDescriptionKey::MD_KEY_VIDEO_ENCODE_BITRATE_MODE, bitrateMode);
 
-    CHECK_AND_RETURN_RET_LOG(!(bitrateExist && qualityExist), -1,
+    CHECK_AND_RETURN_RET_LOG(!(bitrateExist && qualityExist), AVCS_ERR_INVALID_VAL,
         "Param invalid, bitrate and quality mutually include");  // bitrate and quality mutually include
 
     if (bitrateExist) {
         bool bitrateValid = capData.bitrate.InRange(bitrate);
-        CHECK_AND_RETURN_RET_LOG(bitrateValid, -1, "Param invalid, %{public}s: %{public}d",
+        CHECK_AND_RETURN_RET_LOG(bitrateValid, AVCS_ERR_INVALID_VAL, "Param invalid, %{public}s: %{public}d",
             MediaDescriptionKey::MD_KEY_BITRATE.data(), static_cast<int32_t>(bitrate));     // Invalid bitrate
     }
     if (qualityExist) {
         bool qualityValid = capData.bitrate.InRange(quality);
-        CHECK_AND_RETURN_RET_LOG(qualityValid, -1, "Param invalid, %{public}s: %{public}d",
+        CHECK_AND_RETURN_RET_LOG(qualityValid, AVCS_ERR_INVALID_VAL, "Param invalid, %{public}s: %{public}d",
             MediaDescriptionKey::MD_KEY_QUALITY.data(), quality);     // Invalid quality
     }
 
     if (bitrateModeExist) {
         bool bitrateModeValid = std::find(capData.bitrateMode.begin(), capData.bitrateMode.end(), bitrateMode) !=
             capData.bitrateMode.end();
-        CHECK_AND_RETURN_RET_LOG(bitrateModeValid, -1, "Param invalid, %{public}s: %{public}d",
+        CHECK_AND_RETURN_RET_LOG(bitrateModeValid, AVCS_ERR_UNSUPPORT, "Param invalid, %{public}s: %{public}d",
             MediaDescriptionKey::MD_KEY_VIDEO_ENCODE_BITRATE_MODE.data(), bitrateMode);     // Invalid bitrate mode
+        CHECK_AND_RETURN_RET_LOG(!(bitrateExist && bitrateMode == VideoEncodeBitrateMode::CQ),
+            AVCS_ERR_INVALID_VAL, "Param invalid, in CQ mode but set bitrate!");
+        CHECK_AND_RETURN_RET_LOG(!(qualityExist && bitrateMode != VideoEncodeBitrateMode::CQ),
+            AVCS_ERR_INVALID_VAL, "Param invalid, not in CQ mode but set quality!");
     } else {
-        if (bitrateExist) {
-            if (std::find(capData.bitrateMode.begin(), capData.bitrateMode.end(), VideoEncodeBitrateMode::VBR) !=
-                capData.bitrateMode.end()) {
-                bitrateMode = VideoEncodeBitrateMode::VBR;
-            } else if (std::find(capData.bitrateMode.begin(), capData.bitrateMode.end(), VideoEncodeBitrateMode::CBR) !=
-                    capData.bitrateMode.end()) {
-                bitrateMode = VideoEncodeBitrateMode::CBR;
-            } else {
-                AVCODEC_LOGE("No supported bitrate mode");
-                return -1;  // No supported bitrate mode
-            }
-        } else if (qualityExist) {
+        if (qualityExist) {
             bitrateMode = VideoEncodeBitrateMode::CQ;
+            format.PutIntValue(MediaDescriptionKey::MD_KEY_VIDEO_ENCODE_BITRATE_MODE, bitrateMode);
         }
-        format.PutIntValue(MediaDescriptionKey::MD_KEY_VIDEO_ENCODE_BITRATE_MODE, bitrateMode);
     }
 
     AVCODEC_LOGI("Param valid, %{public}s: %{public}d, %{public}s: %{public}d",
@@ -183,8 +181,9 @@ int32_t BitrateAndQualityChecker(CapabilityData &capData, Format &format)
     return AVCS_ERR_OK;
 }
 
-int32_t VideoProfileChecker(CapabilityData &capData, Format &format)
+int32_t VideoProfileChecker(CapabilityData &capData, Format &format, AVCodecType codecType)
 {
+    (void)codecType;
     int32_t profile;
     bool paramExist = format.GetIntValue(MediaDescriptionKey::MD_KEY_PROFILE, profile);
     if (paramExist == false) {
@@ -193,16 +192,17 @@ int32_t VideoProfileChecker(CapabilityData &capData, Format &format)
 
     bool paramValid =
         std::find(capData.profiles.begin(), capData.profiles.end(), profile) != capData.profiles.end();
-    CHECK_AND_RETURN_RET_LOG(paramValid, -1, "Param invalid, %{public}s: %{public}d",
+    CHECK_AND_RETURN_RET_LOG(paramValid, AVCS_ERR_UNSUPPORT, "Param invalid, %{public}s: %{public}d",
         MediaDescriptionKey::MD_KEY_PROFILE.data(), profile);     // Invalid pixel format
 
     AVCODEC_LOGI("Param valid, %{public}s: %{public}d", MediaDescriptionKey::MD_KEY_PROFILE.data(), profile);
     return AVCS_ERR_OK;
 }
 
-int32_t RotaitonChecker(CapabilityData &capData, Format &format)
+int32_t RotaitonChecker(CapabilityData &capData, Format &format, AVCodecType codecType)
 {
     (void)capData;
+    (void)codecType;
     int32_t rotation;
     bool paramExist = format.GetIntValue(MediaDescriptionKey::MD_KEY_ROTATION_ANGLE, rotation);
     if (paramExist == false) {
@@ -211,7 +211,7 @@ int32_t RotaitonChecker(CapabilityData &capData, Format &format)
 
     // valid rotation: 0, 90, 180, 270
     CHECK_AND_RETURN_RET_LOG(rotation == 0 || rotation == 90 || rotation == 180 || rotation == 270,
-        -1, "Param invalid, %{public}s: %{public}d",
+        AVCS_ERR_UNSUPPORT, "Param invalid, %{public}s: %{public}d",
         MediaDescriptionKey::MD_KEY_ROTATION_ANGLE.data(), rotation);    //  Invalid rotation
 
     AVCODEC_LOGI("Param valid, %{public}s: %{public}d", MediaDescriptionKey::MD_KEY_ROTATION_ANGLE.data(), rotation);
@@ -231,7 +231,7 @@ int32_t CodecParamChecker::CheckParamValid(Media::Format &format, AVCodecType co
     auto checkers = CHECKERS_TABLE.find(codecType);
     if (checkers != CHECKERS_TABLE.end()) {
         for (const auto &checker : checkers->second) {
-            int32_t ret = checker(capData.value(), format);
+            int32_t ret = checker(capData.value(), format, codecType);
             CHECK_AND_RETURN_RET_LOG(ret == AVCS_ERR_OK, ret, "Param check failed");
         }
     }
