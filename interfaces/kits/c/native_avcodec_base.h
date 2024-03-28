25b4dc24be6c047cc1ba1f7c704dabe408dc0689
li-jianchao1993 <lijianchao9@huawei.com>
2024-03-28 14:56:34 +0800

    【修改说明】 修改告警
    Signed-off-by: li-jianchao1993 <lijianchao9@huawei.com>

--- interfaces/kits/c/native_avcodec_base.h
+++ interfaces/kits/c/native_avcodec_base.h
@@ -230,16 +230,15 @@ extern const char *OH_AVCODEC_MIMETYPE_AUDIO_G711MU;
 
 /**
  * @brief Enumerates the MIME type of audio low bitrate voice codec.
- * 
+ *
  * @syscap SystemCapability.Multimedia.Media.CodecBase
  * @since 12
  */
 extern const char *OH_AVCODEC_MIMETYPE_AUDIO_LBVC;
 
-
 /**
  * @brief Enumerates the MIME type of versatile video coding.
- * 
+ *
  * @syscap SystemCapability.Multimedia.Media.CodecBase
  * @since 12
  */
