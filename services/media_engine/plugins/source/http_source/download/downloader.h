e21db5346d1eeeb9684d916ac8cd2ed84dd8bb3c
yinzihao <yinzihao3@huawei.com>
2024-04-02 17:27:57 +0800

    修复部分FLV直播无法播放
    Signed-off-by: yinzihao <yinzihao3@huawei.com>

--- services/media_engine/plugins/source/http_source/download/downloader.h
+++ services/media_engine/plugins/source/http_source/download/downloader.h
@@ -157,6 +157,7 @@ private:
     static size_t RxBodyData(void* buffer, size_t size, size_t nitems, void* userParam);
     static size_t RxHeaderData(void* buffer, size_t size, size_t nitems, void* userParam);
     static void FLVProcess(bool &isTrunck, std::string url);
+    static size_t StrncmpContentRange(HeaderInfo* info, char* key, char* next, size_t size, size_t nitems);
 
     std::string name_;
     std::shared_ptr<NetworkClient> client_;
