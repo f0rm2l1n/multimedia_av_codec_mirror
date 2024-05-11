2ca004b88214dece72686f4333fc10668a9bdfaa
wycang <cangweiye1@huawei.com>
2024-05-11 10:37:31 +0800

    update
    Signed-off-by: wycang <cangweiye1@huawei.com>

--- services/media_engine/plugins/ffmpeg_adapter/audio_encoder/mp3/audio_mp3_encoder_plugin.cpp
+++ services/media_engine/plugins/ffmpeg_adapter/audio_encoder/mp3/audio_mp3_encoder_plugin.cpp
@@ -18,7 +18,7 @@
 #include "avcodec_codec_name.h"
 #include "avcodec_log.h"
 #include "avcodec_mime_type.h"
-#include "include/lame.h"
+#include "lame.h"
 #include "plugin/codec_plugin.h"
 #include "plugin/plugin_definition.h"
 
