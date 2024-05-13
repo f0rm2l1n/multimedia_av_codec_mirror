0659751bdfc0735318b565c69b6d4a2703f520c8
yj <yujie57@huawei.com>
2024-05-13 06:27:50 +0000

    add srt sdk_c
    Signed-off-by: yj <yujie57@huawei.com>

--- interfaces/kits/c/native_avcodec_base.h
+++ interfaces/kits/c/native_avcodec_base.h
@@ -253,6 +253,14 @@ extern const char *OH_AVCODEC_MIMETYPE_AUDIO_APE;
 extern const char *OH_AVCODEC_MIMETYPE_VIDEO_VVC;
 
 /**
+ * @brief Enumerates the MIME type of srt subtitle demuxer
+ *
+ * @syscap SystemCapability.Multimedia.Media.CodecBase
+ * @since 12
+ */
+extern const char *OH_AVCODEC_MIMETYPE_SUBTITLE_SRT
+
+/**
  * @brief The extra data's key of surface Buffer
  * @syscap SystemCapability.Multimedia.Media.CodecBase
  * @since 9
