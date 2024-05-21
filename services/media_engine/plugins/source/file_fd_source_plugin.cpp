bb9af16bf33bb82fe46017ea1ae06bf0872c1491
武雪山 <wuxueshan@h-partners.com>
2024-05-21 18:05:44 +0800

    调试信息细化3
    Signed-off-by: 武雪山 <wuxueshan@h-partners.com>

--- services/media_engine/plugins/source/file_fd_source_plugin.cpp
+++ services/media_engine/plugins/source/file_fd_source_plugin.cpp
@@ -146,6 +146,7 @@ void FileFdSourcePlugin::PauseDownloadTask(bool isAsync)
 
 void FileFdSourcePlugin::PauseTimerTask()
 {
+    MEDIA_LOG_I("PauseTimerTask in.");
     if (timerTask_ != nullptr) {
         timerTask_->Pause();
     }
