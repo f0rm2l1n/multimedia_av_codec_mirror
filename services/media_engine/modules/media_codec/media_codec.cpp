cfdece4cbf1a16bdf9ee12f8457e62112c033a79
liyu233 <liyu287@huawei.com>
2024-05-31 16:59:26 +0800

    音频解码获取输出buffer有限重试
    Signed-off-by: liyu233 <liyu287@huawei.com>

--- services/media_engine/modules/media_codec/media_codec.cpp
+++ services/media_engine/modules/media_codec/media_codec.cpp
@@ -691,9 +691,16 @@ Status MediaCodec::HandleOutputBuffer(uint32_t eosStatus)
     Status ret = Status::OK;
     std::shared_ptr<AVBuffer> emptyOutputBuffer;
     AVBufferConfig avBufferConfig;
+    const int32_t maxRetryTimes = 3; // max retry count is 3
+    int32_t retryCount = 0;
     do {
         ret = outputBufferQueueProducer_->RequestBuffer(emptyOutputBuffer, avBufferConfig, TIME_OUT_MS);
-    } while (ret != Status::OK && state_ == CodecState::RUNNING);
+        retryCount++;
+    } while (ret != Status::OK && state_ == CodecState::RUNNING && retryCount < maxRetryTimes);
+
+    if (retryCount == maxRetryTimes && emptyOutputBuffer == nullptr) {
+        MEDIA_LOG_E("request audio decoder output buffer failed");
+    }
 
     if (emptyOutputBuffer) {
         emptyOutputBuffer->flag_ = eosStatus;
