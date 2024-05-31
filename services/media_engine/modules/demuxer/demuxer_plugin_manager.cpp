e8aeeaf38e6e35270d5228200dee596cc53bac56
韩汶钊 <hanwenzhao@huawei.com>
2024-05-31 09:10:43 +0000

    添加判空
    Signed-off-by: 韩汶钊 <hanwenzhao@huawei.com>

--- services/media_engine/modules/demuxer/demuxer_plugin_manager.cpp
+++ services/media_engine/modules/demuxer/demuxer_plugin_manager.cpp
@@ -472,13 +472,14 @@ void DemuxerPluginManager::MediaTypeFound(std::shared_ptr<BaseStreamDemuxer> str
 
 Status DemuxerPluginManager::SeekTo(int64_t seekTime, Plugins::SeekMode mode, int64_t& realSeekTime)
 {
-    if (curAudioStreamID_ != -1) {
+    if (curAudioStreamID_ != -1 && streamInfoMap_[curAudioStreamID_].plugin != nullptr) {
         Status ret = streamInfoMap_[curAudioStreamID_].plugin->SeekTo(-1, seekTime, mode, realSeekTime);
         if (ret != Status::OK) {
             return ret;
         }
     }
-    if (curVideoStreamID_ != -1) {
+    FALSE_RETURN_V(streamInfoMap_[curVideoStreamID_].plugin != nullptr, Status::ERROR_WRONG_STATE, "no video plugin");
+    if (curVideoStreamID_ != -1 && streamInfoMap_[curVideoStreamID_].plugin != nullptr) {
         Status ret = streamInfoMap_[curVideoStreamID_].plugin->SeekTo(-1, seekTime, mode, realSeekTime);
         if (ret != Status::OK) {
             return ret;
