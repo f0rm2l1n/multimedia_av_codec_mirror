110231eeaa98e69bdfb9d66862adf14b3185e57b
zzm <zhangzhiming23@huawei.com>
2024-06-03 06:33:12 +0000

    【修改说明】：将cpp对应头文件放在最前面
    Signed-off-by: zzm <zhangzhiming23@huawei.com>

--- test/nativedemo/audio_demo/avcodec_audio_avbuffer_lbvc_encoder_inner_demo.cpp
+++ test/nativedemo/audio_demo/avcodec_audio_avbuffer_lbvc_encoder_inner_demo.cpp
@@ -12,6 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#include "avcodec_audio_avbuffer_lbvc_encoder_inner_demo.h"
 #include <unistd.h>
 #include <string>
 #include <string_view>
@@ -23,7 +25,6 @@
 #include "meta/audio_types.h"
 #include "inner_api/native/avcodec_audio_codec.h"
 #include "avcodec_errors.h"
-#include "avcodec_audio_avbuffer_lbvc_encoder_inner_demo.h"
 
 using namespace OHOS::Media;
 using namespace OHOS::MediaAVCodec;
