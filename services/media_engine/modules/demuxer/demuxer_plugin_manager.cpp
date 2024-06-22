a73c674f2c82de3763839aed94cf709ed6f1a024
wangyan <wangyan431@huawei.com>
2024-06-22 18:39:18 +0800

    代码告警修改
    Signed-off-by: wangyan <wangyan431@huawei.com>

--- services/media_engine/modules/demuxer/demuxer_plugin_manager.cpp
+++ services/media_engine/modules/demuxer/demuxer_plugin_manager.cpp
@@ -282,8 +282,8 @@ Status DemuxerPluginManager::AddTempTrackInfo(const Plugins::MediaInfo& mediaInf
                 continue;
             }
         }
-        size_t tempIndex = tempTrackInfoMap_.size();
-        MEDIA_LOG_I("AddTempTrackInfo index =  "  PUBLIC_LOG_U64 " id = " PUBLIC_LOG_D32
+        int32_t tempIndex = static_cast<int32_t>(tempTrackInfoMap_.size());
+        MEDIA_LOG_I("AddTempTrackInfo index =  "  PUBLIC_LOG_D32 " id = " PUBLIC_LOG_D32
             " innerIndex = " PUBLIC_LOG_D32, tempIndex, streamId, index);
         tempTrackInfoMap_[tempIndex].streamID = streamId;
         tempTrackInfoMap_[tempIndex].innerTrackIndex = static_cast<int32_t>(index);
