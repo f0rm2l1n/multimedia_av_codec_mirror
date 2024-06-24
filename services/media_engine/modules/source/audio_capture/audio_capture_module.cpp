942c5aacc7cce4805ea6f7cd2f410c7926ab86b7
任军旗 <renjunqi3@huawei.com>
2024-06-24 02:22:14 +0000

    修改传参位置不对并优化空指针判空日志打印问题
    Signed-off-by: 任军旗 <renjunqi3@huawei.com>

--- services/media_engine/modules/source/audio_capture/audio_capture_module.cpp
+++ services/media_engine/modules/source/audio_capture/audio_capture_module.cpp
@@ -117,21 +117,6 @@ Status AudioCaptureModule::Deinit()
 Status AudioCaptureModule::Prepare()
 {
     MEDIA_LOG_I("Prepare enter.");
-    AudioStandard::AudioEncodingType audioEncoding = AudioStandard::ENCODING_INVALID;
-    auto supportedEncodingTypes = OHOS::AudioStandard::AudioCapturer::GetSupportedEncodingTypes();
-    for (auto& supportedEncodingType : supportedEncodingTypes) {
-        if (supportedEncodingType == AudioStandard::ENCODING_PCM) {
-            audioEncoding = AudioStandard::ENCODING_PCM;
-            break;
-        }
-    }
-
-    if (audioEncoding != AudioStandard::ENCODING_PCM) {
-        MEDIA_LOG_E("audioCapturer do not support pcm encoding");
-        SetFaultEvent("AudioCaptureModule::Prepare, audioCapturer do not support pcm encoding");
-        return Status::ERROR_UNKNOWN;
-    }
-    options_.streamInfo.encoding = AudioStandard::ENCODING_PCM;
     size_t size;
     {
         AutoLock lock (captureMutex_);
@@ -255,6 +240,21 @@ Status AudioCaptureModule::SetParameter(const std::shared_ptr<Meta> &meta)
             Status::ERROR_INVALID_PARAMETER, "SampleFormat is unsupported by audiocapturer");
     }
 
+    AudioStandard::AudioEncodingType audioEncoding = AudioStandard::ENCODING_INVALID;
+    auto supportedEncodingTypes = OHOS::AudioStandard::AudioCapturer::GetSupportedEncodingTypes();
+    for (auto& supportedEncodingType : supportedEncodingTypes) {
+        if (supportedEncodingType == AudioStandard::ENCODING_PCM) {
+            audioEncoding = AudioStandard::ENCODING_PCM;
+            break;
+        }
+    }
+
+    if (audioEncoding != AudioStandard::ENCODING_PCM) {
+        MEDIA_LOG_E("audioCapturer do not support pcm encoding");
+        SetFaultEvent("AudioCaptureModule::Prepare, audioCapturer do not support pcm encoding");
+        return Status::ERROR_UNKNOWN;
+    }
+    options_.streamInfo.encoding = AudioStandard::ENCODING_PCM;
     return Status::OK;
 }
 
@@ -377,10 +377,8 @@ Status AudioCaptureModule::SetAudioCapturerInfoChangeCallback(
 
 Status AudioCaptureModule::GetCurrentCapturerChangeInfo(AudioStandard::AudioCapturerChangeInfo &changeInfo)
 {
-    if (audioCapturer_ == nullptr) {
-        MEDIA_LOG_E("audioCapturer is nullptr, cannot get audio capturer change info");
-        return Status::ERROR_INVALID_OPERATION;
-    }
+    FALSE_RETURN_V_MSG_E(audioCapturer_ != nullptr, Status::ERROR_INVALID_OPERATION,
+        "audioCapturer is nullptr, cannot get audio capturer change info");
     audioCapturer_->GetCurrentCapturerChangeInfo(changeInfo);
     return Status::OK;
 }
