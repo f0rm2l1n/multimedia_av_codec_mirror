bd31ae7e2dc637cc7b71fbcd97a3eda4d246c7ff
liye <liye87@huawei.com>
2024-06-25 11:55:40 +0800

    修改hls http_media_downloader downloader.cpp代码告警
    Signed-off-by: liye <liye87@huawei.com>

--- services/media_engine/plugins/source/http_source/download/downloader.cpp
+++ services/media_engine/plugins/source/http_source/download/downloader.cpp
@@ -543,6 +543,8 @@ size_t Downloader::DropRetryData(void* buffer, size_t dataLen, Downloader* media
         dropRet = currentRequest_->saveData_(static_cast<uint8_t *>(buffer) + writeOffSet,
                                              static_cast<int64_t>(dataLen) - writeOffSet);
         currentRequest_->dropedDataLen_ = currentRequest_->dropedDataLen_ + writeOffSet;
+        MEDIA_LOG_D("DropRetryData: drop, droped len " PUBLIC_LOG_D64 ", startPos_ " PUBLIC_LOG_D64,
+                    currentRequest_->dropedDataLen_, currentRequest_->startPos_);
     } else if (writeOffSet == 0) {
         currentRequest_->dropedDataLen_ = currentRequest_->dropedDataLen_ +
                                             static_cast<int64_t>(dataLen);
