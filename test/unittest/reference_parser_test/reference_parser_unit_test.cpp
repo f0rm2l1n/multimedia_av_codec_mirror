a1599baa58f36bd84c378136170e56d2f6b71a9e
qihaonan <qihaonan3@huawei.com>
2024-06-25 12:53:41 +0000

    【修改说明】参考关系解析测试代码
    Signed-off-by: qihaonan <qihaonan3@huawei.com>

--- test/unittest/reference_parser_test/reference_parser_unit_test.cpp
+++ test/unittest/reference_parser_test/reference_parser_unit_test.cpp
@@ -12,3 +12,50 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#include "gtest/gtest.h"
+#include "meta/format.h"
+#include "avcodec_errors.h"
+#include "avcodec_common.h"
+#include "securec.h"
+#include "reference_parser_demo.h"
+
+using namespace std;
+using namespace OHOS;
+using namespace OHOS::MediaAVCodec;
+using namespace testing::ext;
+
+namespace {
+class ReferenceParserTest : public testing::Test {
+public:
+    // SetUpTestCase: CALLed before all test cases
+    static void SetUpTestCase(void);
+    // TearDownTestCase: CALLed after all test cases
+    static void TearDownTestCase(void);
+    // SetUp: Called before each test cases
+    void SetUp(void);
+    // TearDown: Called after each test cases;
+    void TearDown(void);
+};
+
+void ReferenceParserTest::SetUpTestCase() {}
+void ReferenceParserTest::TearDownTestCase() {}
+void ReferenceParserTest::SetUp() {}
+void ReferenceParserTest::TearDown() {}
+} // namespace
+
+namespace {
+/**
+ * @tc.number       : RP_FUNC_SCALIBILITY_0100
+ * @tc.name         : do accurate seek
+ * @tc.desc         : func test
+ */
+HWTEST_F(ReferenceParserTest, RP_FUNC_SEEK_IPBBB_0100, TestSize.Level1)
+{
+    shared_ptr<ReferenceParserDemo> refParserDemo = make_shared<ReferenceParserDemo>();
+    refParserDemo->SetDecIntervalMs(6); // 6
+    ASSERT_EQ(0, refParserDemo->InitScene(MP4Scene::LOWDELAY_B_SCALA));
+    ASSERT_EQ(true, refParserDemo->DoAccurateSeek(1650)); // 1650 is frame 99
+}
+
+} // namespace
