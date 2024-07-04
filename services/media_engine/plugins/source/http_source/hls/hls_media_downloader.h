db3b72099f52e9c007e76f5cc39be6e350c5e3d0
mark1010 <ligaokui3@huawei.com>
2024-07-04 17:31:20 +0800

    播放列表支持动态扩容
    Signed-off-by: mark1010 <ligaokui3@huawei.com>

--- services/media_engine/plugins/source/http_source/hls/hls_media_downloader.h
+++ services/media_engine/plugins/source/http_source/hls/hls_media_downloader.h
@@ -113,6 +113,8 @@ PRIVATE:
     Status CheckPlaylist(unsigned char* buff, ReadDataInfo& readDataInfo);
     bool CheckReadTimeOut();
     bool CheckBreakCondition();
+    void ResetPlaylistCapacity(size_t size);
+    void PlaylistBackup(const PlayInfo& fragment);
 PRIVATE:
     std::shared_ptr<RingBuffer> buffer_;
     size_t totalRingBufferSize_ {0};
