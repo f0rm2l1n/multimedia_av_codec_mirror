19e89b26daa9eaeafb61fc20d67c52f6d00d525b
yu_ssai <yusaisai1@huawei.com>
2024-07-05 08:39:23 +0000

    【dash】读buffer时，超时检查时间过短，弱网下会报错
    Signed-off-by: yu_ssai <yusaisai1@huawei.com>

--- test/unittest/dash_test/dash_segment_downloader_unit_test.cpp
+++ test/unittest/dash_test/dash_segment_downloader_unit_test.cpp
@@ -165,9 +165,13 @@ HWTEST_F(DashSegmentDownloaderUnitTest, TEST_READ, TestSize.Level1)
     }
 
     unsigned char buffer[1024];
-    uint32_t realReadLength = 0;
-    int32_t realStreamId = 1;
-    DashReadRet result = segmentDownloader->Read(1, buffer, 1024, realReadLength, realStreamId);
+    ReadDataInfo readDataInfo;
+    readDataInfo.streamId_ = 1;
+    readDataInfo.wantReadLength_ = 1024;
+    readDataInfo.realReadLength_ = 0;
+    readDataInfo.nextStreamId_ = 1;
+    std::atomic<bool> isInterruptNeeded{false};
+    DashReadRet result = segmentDownloader->Read(buffer, readDataInfo, isInterruptNeeded);
     segmentDownloader->Close(true, true);
     segmentDownloader = nullptr;
 
