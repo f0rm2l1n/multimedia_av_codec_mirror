2f88ca2f649fbc7c6acc4c8a16aab073d957403d
韩汶钊 <hanwenzhao@huawei.com>
2024-07-08 02:32:52 +0000

    update services/media_engine/plugins/ffmpeg_adapter/common/stream_parser_manager.cpp.
    Signed-off-by: 韩汶钊 <hanwenzhao@huawei.com>

--- services/media_engine/plugins/ffmpeg_adapter/common/stream_parser_manager.cpp
+++ services/media_engine/plugins/ffmpeg_adapter/common/stream_parser_manager.cpp
@@ -21,7 +21,7 @@
 
 namespace {
 const std::string HEVC_LIB_PATH = "libav_codec_hevc_parser.z.so";
-constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, LOG_DOMAIN_SYSTEM_PLAYER, "HiStreamer" };
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, LOG_DOMAIN_DEMUXER, "HiStreamer" };
 }
 
 namespace OHOS {
