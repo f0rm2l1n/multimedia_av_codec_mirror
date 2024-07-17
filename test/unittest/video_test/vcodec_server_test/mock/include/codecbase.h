f4ac328e3fc3aa30e9e98645d66b7977c1e7e24a
wangyongkai <wangyongkai5@h-partners.com>
2024-07-17 13:31:49 +0800

    【修改说明】add codec list core coverage
    Signed-off-by: wangyongkai <wangyongkai5@h-partners.com>

--- test/unittest/video_test/vcodec_server_test/mock/include/codecbase.h
+++ test/unittest/video_test/vcodec_server_test/mock/include/codecbase.h
@@ -40,8 +40,12 @@ enum class CallbackFlag : uint8_t {
 };
 const OHOS::MediaAVCodec::Range DEFAULT_RANGE = {96, 4096};
 const OHOS::MediaAVCodec::Range HEVC_DECODER_RANGE = {2, 1920};
+const OHOS::MediaAVCodec::Range DEFALUT_BITRATE_RANGE = {1, 40000000};
+const OHOS::MediaAVCodec::Range DEFALUT_FRAMERATE_RANGE = {1, 60};
+const OHOS::MediaAVCodec::Range DEFALUT_CHANNELS_RANGE = {1,30};
 const std::vector<int32_t> DEFALUT_PIXFORMAT = {1, 2, 3};
 const std::vector<int32_t> HEVC_DECODER_PIXFORMAT = {2, 3};
+const std::vector<int32_t> DEFALUT_SAMPLE_RATE = {1, 2, 3, 4, 5};
 
 const std::string CODEC_MIME_MOCK_00 = "video/codec_mime_00";
 const std::string CODEC_MIME_MOCK_01 = "video/codec_mime_01";
@@ -89,6 +93,17 @@ const std::vector<CapabilityData> HEVC_DECODER_CAPS = {{.codecName = "video.Hevc
                                                         .pixFormat = HEVC_DECODER_PIXFORMAT}};
 const std::vector<CapabilityData> HCODEC_CAPS = {{.codecName = "video.H.Decoder.Name.00",
                                                   .codecType = AVCODEC_TYPE_VIDEO_DECODER,
+                                                  .bitrate = DEFALUT_BITRATE_RANGE,
+                                                  .frameRate = DEFALUT_FRAMERATE_RANGE,
+                                                  .channels = DEFALUT_CHANNELS_RANGE,
+                                                  .sampleRate = DEFALUT_SAMPLE_RATE,
+                                                  .mimeType = CODEC_MIME_MOCK_00,
+                                                  .isVendor = false,
+                                                  .width = DEFAULT_RANGE,
+                                                  .height = DEFAULT_RANGE,
+                                                  .pixFormat = DEFALUT_PIXFORMAT},
+                                                 {.codecName = "video.H.Decoder.Name.01",
+                                                  .codecType = AVCODEC_TYPE_VIDEO_DECODER,
                                                   .mimeType = CODEC_MIME_MOCK_00,
                                                   .isVendor = true,
                                                   .width = DEFAULT_RANGE,
@@ -101,7 +116,7 @@ const std::vector<CapabilityData> HCODEC_CAPS = {{.codecName = "video.H.Decoder.
                                                   .width = DEFAULT_RANGE,
                                                   .height = DEFAULT_RANGE,
                                                   .pixFormat = DEFALUT_PIXFORMAT},
-                                                 {.codecName = "video.H.Decoder.Name.01",
+                                                 {.codecName = "video.H.Decoder.Name.02",
                                                   .codecType = AVCODEC_TYPE_VIDEO_DECODER,
                                                   .mimeType = CODEC_MIME_MOCK_01,
                                                   .isVendor = true,
