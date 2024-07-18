16837faacde0a6edde5d9eee78b84520cefde315
liye <liye87@huawei.com>
2024-07-18 19:00:48 +0800

    解决冲突
    Signed-off-by: liye <liye87@huawei.com>

--- services/media_engine/plugins/source/data_stream_source_plugin.cpp
+++ services/media_engine/plugins/source/data_stream_source_plugin.cpp
@@ -172,11 +172,11 @@ Status DataStreamSourcePlugin::Read(std::shared_ptr<Plugins::Buffer>& buffer, ui
     } else {
         buffer = WrapAVSharedMemory(memory, realLen);
     }
-    FALSE_RETUEN_V(buffer != nullptr, Status::ERROR_AGAIN);
+    FALSE_RETURN_V(buffer != nullptr, Status::ERROR_AGAIN);
     MEDIA_LOG_D("DataStreamSourcePlugin Read, size: " PUBLIC_LOG_ZU ", realLen: " PUBLIC_LOG_D32
         ", retryTimes: " PUBLIC_LOG_U32, (buffer && buffer->GetMemory()) ?
         buffer->GetMemory()->GetSize() : -100, realLen, retryTimes_); // -100 invalid size
-    FALSE_RETUEN_V(realLen != 0, Status::ERROR_AGAIN);
+    FALSE_RETURN_V(realLen != 0, Status::ERROR_AGAIN);
     return Status::OK;
 }
 
