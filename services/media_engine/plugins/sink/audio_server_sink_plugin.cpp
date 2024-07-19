a26bb2044e654048e414705e9d491c54920b6fe3
任军旗 <renjunqi3@huawei.com>
2024-07-19 10:05:14 +0000

    当AVPlayer实例设置音量为0时，不影响其他AVPlayer实例播放
    Signed-off-by: 任军旗 <renjunqi3@huawei.com>

--- services/media_engine/plugins/sink/audio_server_sink_plugin.cpp
+++ services/media_engine/plugins/sink/audio_server_sink_plugin.cpp
@@ -32,7 +32,7 @@
 #include "osal/utils/steady_clock.h"
 #include "plugin/plugin_time.h"
 #include "param_wrapper.h"
-
+#include "osal/utils/util.h"
 
 namespace {
 constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, LOG_DOMAIN_SYSTEM_PLAYER, "HiStreamer" };
@@ -748,17 +748,22 @@ Status AudioServerSinkPlugin::GetVolume(float &volume)
 Status AudioServerSinkPlugin::SetVolume(float volume)
 {
     MEDIA_LOG_I_T("SetVolume entered.");
-    if (audioRenderer_ != nullptr) {
-        int32_t ret = audioRenderer_->SetVolume(volume);
-        if (ret != OHOS::AudioStandard::SUCCESS) {
-            MEDIA_LOG_E_T("set volume failed with code " PUBLIC_LOG_D32, ret);
-            return Status::ERROR_UNKNOWN;
-        }
-        MEDIA_LOG_I_T("SetVolume succ");
-        audioRendererVolume_ = volume;
-        return Status::OK;
+    FALSE_RETURN_V(audioRenderer_ != nullptr, Status::ERROR_WRONG_STATE);
+    int32_t ret = audioRenderer_->SetVolume(volume);
+    if (ret != OHOS::AudioStandard::SUCCESS) {
+        MEDIA_LOG_E_T("set volume failed with code " PUBLIC_LOG_D32, ret);
+        return Status::ERROR_UNKNOWN;
     }
-    return Status::ERROR_WRONG_STATE;
+    MEDIA_LOG_I_T("SetVolume succ");
+    if (!OSAL::IsFloatEqualZero(audioRendererVolume_) && OSAL::IsFloatEqualZero(volume)) {
+        MEDIA_LOG_I_T("set volume from %{public}f to %{public}f", audioRendererVolume_, volume);
+        audioRenderer_->SetSilentModeAndMixWithOthers(true);
+    } else if (OSAL::IsFloatEqualZero(audioRendererVolume_) && !OSAL::IsFloatEqualZero(volume)) {
+        MEDIA_LOG_I_T("set volume from %{public}f to %{public}f", audioRendererVolume_, volume);
+        audioRenderer_->SetSilentModeAndMixWithOthers(false);
+    }
+    audioRendererVolume_ = volume;
+    return Status::OK;
 }
 
 Status AudioServerSinkPlugin::GetAudioEffectMode(int32_t &effectMode)
