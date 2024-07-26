ba45d0fb8e983ab403de77b080e4910b4f4f3373
liye <liye87@huawei.com>
2024-07-26 09:52:44 +0800

    HLS代码告警整改
    Signed-off-by: liye <liye87@huawei.com>

--- services/media_engine/plugins/source/http_source/http/http_media_downloader.cpp
+++ services/media_engine/plugins/source/http_source/http/http_media_downloader.cpp
@@ -945,8 +945,8 @@ void HttpMediaDownloader::UpdateCachedPercent(BufferingInfoType infoType)
     }
     int32_t deltaSize = bufferSize - lastCachedSize_;
     if (deltaSize >= UPDATE_CACHE_STEP) {
-        int percent = (bufferSize >= static_cast<int>(waterLineAbove_)) ?
-                        100 : bufferSize * 100 / static_cast<int>(waterLineAbove_); // 100
+        int percent = (bufferSize >= static_cast<int32_t>(waterLineAbove_)) ?
+                        100 : bufferSize * 100 / static_cast<int32_t>(waterLineAbove_); // 100
         callback_->OnEvent({PluginEventType::EVENT_BUFFER_PROGRESS, {percent}, "buffer percent"});
         lastCachedSize_ = bufferSize;
     }
