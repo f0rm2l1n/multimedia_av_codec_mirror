45b301da79b792f8475ec6f7c8469084656a1199
fanpingan <fanpingan1@huawei.com>
2024-07-27 15:52:17 +0000

    update services/engine/codec/video/hcodec/hencoder.cpp.
    Signed-off-by: fanpingan <fanpingan1@huawei.com>

--- services/engine/codec/video/hcodec/hencoder.cpp
+++ services/engine/codec/video/hcodec/hencoder.cpp
@@ -1230,6 +1230,7 @@ void HEncoder::SubmitOneBuffer(InSurfaceBufferEntry& entry, BufferInfo &info)
         int32_t err = NotifyOmxToEmptyThisInBuffer(info);
         if (err != AVCS_ERR_OK) {
             ResetSlot(info);
+            callback_->OnError(AVCODEC_ERROR_INTERNAL, AVCS_ERR_EMPTY_INPUT_FAILED);
         }
     }
 }
