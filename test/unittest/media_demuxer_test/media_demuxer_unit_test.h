c3002926fcb7f8965c4957ee70c5dda7388be5cc
assertsu <subenhui@huawei.com>
2024-07-29 08:29:10 +0000

    1
    Signed-off-by: assertsu <subenhui@huawei.com>

--- test/unittest/media_demuxer_test/media_demuxer_unit_test.h
+++ test/unittest/media_demuxer_test/media_demuxer_unit_test.h
@@ -38,6 +38,87 @@ public:
     void TearDown(void);
 };
 
+class DemuxerPluginMock : public Plugins::DemuxerPlugin {
+public:
+    explicit DemuxerPluginMock(std::string name) : DemuxerPlugin(std::move(name)) {
+        
+    }
+    ~DemuxerPluginMock() override {
+
+    }
+    virtual Status Reset() override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status Start() override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status Stop() override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status Flush() override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status SetDataSource(const std::shared_ptr<DataSource>& source) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status GetMediaInfo(MediaInfo& mediaInfo) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status GetUserMeta(std::shared_ptr<Meta> meta) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status SelectTrack(uint32_t trackId) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status UnselectTrack(uint32_t trackId) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status SeekTo(int32_t trackId, int64_t seekTime, SeekMode mode, int64_t& realSeekTime) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status ReadSample(uint32_t trackId, std::shared_ptr<AVBuffer> sample) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status GetNextSampleSize(uint32_t trackId, int32_t& size) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status GetDrmInfo(std::multimap<std::string, std::vector<uint8_t>>& drmInfo) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual void ResetEosStatus() override {
+        return;
+    }
+    virtual Status ParserRefUpdatePos(int64_t timeStampMs, bool isForward = true) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status ParserRefInfo() override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status GetFrameLayerInfo(std::shared_ptr<AVBuffer> videoSample, FrameLayerInfo &frameLayerInfo) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status GetFrameLayerInfo(uint32_t frameId, FrameLayerInfo &frameLayerInfo) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status GetGopLayerInfo(uint32_t gopId, GopLayerInfo &gopLayerInfo) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status GetIFramePos(std::vector<uint32_t> &IFramePos) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status Dts2FrameId(int64_t dts, uint32_t &frameId, bool offset = true) override {
+        return Status::ERROR_UNKNOWN;
+    }
+    virtual Status GetFrameIndexByPresentationTimeUs(uint32_t trackIndex,
+        int64_t presentationTimeUs, uint32_t &frameIndex) override {
+            return Status::ERROR_UNKNOWN;
+        }
+   virtual  Status GetPresentationTimeUsByFrameIndex(uint32_t trackIndex,
+        uint32_t frameIndex, int64_t &presentationTimeUs) override {
+            return Status::ERROR_UNKNOWN;
+        }
+};
+
 }
 }
 
