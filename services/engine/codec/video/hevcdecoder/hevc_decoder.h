c09cd47cd957213937263635ed6235531b6eb0e1
Yanghaolong <yanghaolong2@huawei.com>
2024-08-10 06:24:18 +0000

    265软解新增surface切换功能
    Signed-off-by: Yanghaolong <yanghaolong2@huawei.com>

--- services/engine/codec/video/hevcdecoder/hevc_decoder.h
+++ services/engine/codec/video/hevcdecoder/hevc_decoder.h
@@ -138,6 +138,9 @@ private:
     int32_t FillFrameBuffer(const std::shared_ptr<HBuffer> &frameBuffer);
     int32_t CheckFormatChange(uint32_t index, int width, int height, int bitDepth);
     void SetSurfaceParameter(const Format &format, const std::string_view &formatKey, FormatDataType formatType);
+    int32_t ReplaceOutputSurfaceWhenRunning(sptr<Surface> newSurface);
+    int32_t SetQueueSize(const sptr<Surface> &surface, uint32_t targetSize);
+    int32_t AttachToNewSurface(const sptr<Surface> &newSurface);
     int32_t FlushSurfaceMemory(std::shared_ptr<FSurfaceMemory> &surfaceMemory, int64_t pts);
     int32_t GetSurfaceBufferStride(const std::shared_ptr<HBuffer> &frameBuffer);
     int32_t SetSurfaceCfg(int32_t bufferCnt);
