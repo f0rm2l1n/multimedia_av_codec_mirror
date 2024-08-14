13d4116a0a143cc4e11d60ac9d9f2f0011051c46
assertsu <subenhui@huawei.com>
2024-08-14 11:57:04 +0000

    1
    Signed-off-by: assertsu <subenhui@huawei.com>

--- services/media_engine/modules/demuxer/demuxer_plugin_manager.cpp
+++ services/media_engine/modules/demuxer/demuxer_plugin_manager.cpp
@@ -300,7 +300,7 @@ Status DemuxerPluginManager::LoadCurrentSubtitlePlugin(std::shared_ptr<BaseStrea
 void DemuxerPluginManager::AddMediaInfo(int32_t streamID, Plugins::MediaInfo& mediaInfo)
 {
     MEDIA_LOG_I("AddMediaInfo enter");
-    AddGeneral(streamInfoMap_[streamID].mediaInfo.general, mediaInfo.general);
+    AddGeneral(streamInfoMap_[streamID], mediaInfo.general);
     for (uint32_t index = 0; index < streamInfoMap_[streamID].mediaInfo.tracks.size(); index++) {
         auto trackMeta = streamInfoMap_[streamID].mediaInfo.tracks[index];
         mediaInfo.tracks.push_back(trackMeta);
@@ -386,19 +386,26 @@ Status DemuxerPluginManager::UpdateGeneralValue(int32_t trackCount, const Meta& 
     return Status::OK;
 }
 
-Status DemuxerPluginManager::AddGeneral(const Meta& format, Meta& formatNew)
+Status DemuxerPluginManager::AddGeneral(const MediaStreamInfo& info, Meta& formatNew)
 {
-    if (formatNew.Empty()) {
-        formatNew = format;
-        return Status::OK;
+    FileType fileType = FileType::UNKNOW;
+    if (formatNew.Get<Tag::MEDIA_FILE_TYPE>(fileType) == false && info.activated == true) {
+        info.mediaInfo.general.Get<Tag::MEDIA_FILE_TYPE>(fileType);
+        formatNew.Set<Tag::MEDIA_FILE_TYPE>(fileType);
+        int64_t durationMs = 0;
+        info.mediaInfo.general.Get<Tag::MEDIA_DURATION>(durationMs);
+        formatNew.Set<Tag::MEDIA_DURATION>(durationMs);
     }
-    // 合并
+
     int32_t curTrackCount = 0;
-    int32_t newTrackCount = 0;
     formatNew.Get<Tag::MEDIA_TRACK_COUNT>(curTrackCount);
-    format.Get<Tag::MEDIA_TRACK_COUNT>(newTrackCount);
+
+    int32_t newTrackCount = 0;
+    if (info.mediaInfo.general.Get<Tag::MEDIA_TRACK_COUNT>(newTrackCount) == false) {
+        newTrackCount = 1;
+    }
     int32_t totalTrackCount = newTrackCount + curTrackCount;
-    UpdateGeneralValue(totalTrackCount, format, formatNew);
+    UpdateGeneralValue(totalTrackCount, info.mediaInfo.general, formatNew);
 
     return Status::OK;
 }
