1c993de1ec5463c1f4f878804f799d77d6d31e12
gao_ziyu <gaoziyu@huawei.com>
2024-08-19 16:35:30 +0800

    add coverage
    Signed-off-by: gao_ziyu <gaoziyu@huawei.com>

--- services/engine/codec/audio/decoder/audio_ffmpeg_decoder_plugin.cpp
+++ services/engine/codec/audio/decoder/audio_ffmpeg_decoder_plugin.cpp
@@ -262,7 +262,7 @@ int32_t AudioFfmpegDecoderPlugin::AllocateContext(const std::string &name)
         cachedFrame_ = std::shared_ptr<AVFrame>(av_frame_alloc(), [](AVFrame *fp) { av_frame_free(&fp); });
     }
     CHECK_AND_RETURN_RET_LOG(avCodec_ != nullptr,
-        AVCodecServiceErrCode::AVCS_ERR_UNSUPPORT_PROTOCOL_TYPE, "AllocateContext failed %{public}s", name.c_str());
+        AVCodecServiceErrCode::AVCS_ERR_INVALID_OPERATION, "AllocateContext failed %{public}s", name.c_str());
 
     name_ = name;
     AVCodecContext *context = nullptr;
