a7c2a8118e6ec8eefc7ac304be7ceeaba6a5e637
国庆 <xinguoqing2@huawei.com>
2024-08-20 13:50:21 +0000

    av codec
    Signed-off-by: 国庆 <xinguoqing2@huawei.com>

--- test/unittest/filter_test/muxer_filter_unit_test.h
+++ test/unittest/filter_test/muxer_filter_unit_test.h
@@ -15,8 +15,13 @@
 #ifndef HISTREAMER_MUXER_FILTER_UNIT_TEST_H
 #define HISTREAMER_MUXER_FILTER_UNIT_TEST_H
 
+#include <cstring>
+#include <map>
 #include "gtest/gtest.h"
 #include "muxer_filter.h"
+#include "filter/filter.h"
+#include "common/status.h"
+#include "meta/media_types.h"
 
 namespace OHOS {
 namespace Media {
@@ -34,6 +39,97 @@ public:
 
 protected:
     std::shared_ptr<MuxerFilter> muxerFilter_{ nullptr };
+};
+class MyFilterLinkCallback : public FilterLinkCallback {
+public:
+    ~MyFilterLinkCallback() = default;
+    void OnLinkedResult(const sptr<AVBufferQueueProducer>& queue,
+                                        std::shared_ptr<Meta>& meta)
+    {
+        return;
+    }
+    void OnUnlinkedResult(std::shared_ptr<Meta>& meta)
+    {
+        return;
+    }
+    void OnUpdatedResult(std::shared_ptr<Meta>& meta)
+    {
+        return;
+    }
+};
+class MyEventReceiver : public EventReceiver {
+public:
+    ~MyEventReceiver() = default;
+    void OnEvent(const Event& event)
+    {
+        return;
+    }
+};
+class MyAVBufferQueueProducer : public IRemoteStub<AVBufferQueueProducer> {
+public:
+    uint32_t GetQueueSize()
+    {
+        return 0;
+    }
+    Status SetQueueSize(uint32_t size)
+    {
+        return  Status::OK;
+    }
+
+    Status RequestBuffer(std::shared_ptr<AVBuffer>& outBuffer,
+                                 const AVBufferConfig& config, int32_t timeoutMs)
+    {
+        return  Status::OK;
+    }
+    Status PushBuffer(const std::shared_ptr<AVBuffer>& inBuffer, bool available)
+    {
+        return  Status::OK;
+    }
+    Status ReturnBuffer(const std::shared_ptr<AVBuffer>& inBuffer, bool available)
+    {
+        return  Status::OK;
+    }
+
+    Status AttachBuffer(std::shared_ptr<AVBuffer>& inBuffer, bool isFilled)
+    {
+        return  Status::OK;
+    }
+    Status DetachBuffer(const std::shared_ptr<AVBuffer>& outBuffer)
+    {
+        return  Status::OK;
+    }
+
+    Status SetBufferFilledListener(sptr<IBrokerListener>& listener)
+    {
+        return  Status::OK;
+    }
+    Status RemoveBufferFilledListener(sptr<IBrokerListener>& listener)
+    {
+        return  Status::OK;
+    }
+    Status SetBufferAvailableListener(sptr<IProducerListener>& listener)
+    {
+        return  Status::OK;
+    }
+    Status Clear()
+    {
+        return  Status::OK;
+    }
+    DECLARE_INTERFACE_DESCRIPTOR(u"Media.MyAVBufferQueueProducer");
+
+protected:
+    enum: uint32_t {
+        PRODUCER_GET_QUEUE_SIZE = 0,
+        PRODUCER_SET_QUEUE_SIZE = 1,
+        PRODUCER_REQUEST_BUFFER = 2,
+        PRODUCER_PUSH_BUFFER = 3,
+        PRODUCER_RETURN_BUFFER = 4,
+        PRODUCER_ATTACH_BUFFER = 5,
+        PRODUCER_DETACH_BUFFER = 6,
+        PRODUCER_SET_FILLED_LISTENER = 7,
+        PRODUCER_REMOVE_FILLED_LISTENER = 8,
+        PRODUCER_SET_AVAILABLE_LISTENER = 9
+    };
 };
 }  // namespace Pipeline
 }  // namespace Media
