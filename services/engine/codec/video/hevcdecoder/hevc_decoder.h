4d5be31e068ef61b937850a39aa2fcd51085429b
Yanghaolong <yanghaolong2@huawei.com>
2024-08-23 05:34:42 +0000

    265软解dump功能user版本控制
    Signed-off-by: Yanghaolong <yanghaolong2@huawei.com>

--- services/engine/codec/video/hevcdecoder/hevc_decoder.h
+++ services/engine/codec/video/hevcdecoder/hevc_decoder.h
@@ -110,10 +110,12 @@ private:
         BIT_DEPTH8BIT = 8,
         BIT_DEPTH10BIT = 10,
     };
-    
+
+#ifdef BUILD_ENG_VERSION
     void OpenDumpFile();
     void DumpOutputBuffer(int32_t bitDepth);
     void DumpConvertOut(struct SurfaceInfo &surfaceInfo);
+#endif
     bool IsActive() const;
     void CalculateBufferSize();
     int32_t AllocateBuffers();
@@ -194,9 +196,11 @@ private:
     std::shared_ptr<MediaCodecCallback> callback_;
     std::atomic<bool> isSendEos_ = false;
     std::atomic<bool> isBufferAllocated_ = false;
+#ifdef BUILD_ENG_VERSION
     std::shared_ptr<std::ofstream> dumpInFile_ = nullptr;
     std::shared_ptr<std::ofstream> dumpOutFile_ = nullptr;
     std::shared_ptr<std::ofstream> dumpConvertFile_ = nullptr;
+#endif
 };
 
 void HevcDecLog(UINT32 channelId, IHW265VIDEO_ALG_LOG_LEVEL eLevel, INT8 *pMsg, ...);
