d79abedef2dcca24c516f112dc02e778c9d7d562
国庆 <xinguoqing2@huawei.com>
2024-08-26 11:32:30 +0000

    av  codec
    Signed-off-by: 国庆 <xinguoqing2@huawei.com>

--- test/unittest/filter_test/audio_data_source_filter_unit_test.h
+++ test/unittest/filter_test/audio_data_source_filter_unit_test.h
@@ -17,6 +17,12 @@
 
 #include "gtest/gtest.h"
 #include "audio_data_source_filter.h"
+#include "filter/filter.h"
+#include "common/status.h"
+#include "osal/task/task.h"
+#include "audio_capturer.h"
+#include "media_data_source.h"
+
 
 namespace OHOS {
 namespace Media {
@@ -35,6 +41,143 @@ public:
 protected:
     std::shared_ptr<AudioDataSourceFilter> audioDataSourceFilter_{ nullptr };
 };
+
+class MyAVBufferQueueProducer : public IRemoteStub<AVBufferQueueProducer> {
+public:
+    uint32_t GetQueueSize()
+    {
+        return 0;
+    }
+    Status SetQueueSize(uint32_t size)
+    {
+        return  Status::OK;
+    }
+
+    Status RequestBuffer(std::shared_ptr<AVBuffer>& outBuffer,
+                                 const AVBufferConfig& config, int32_t timeoutMs)
+    {
+        if (outBuffer == nullptr) {
+            return  Status::ERROR_NULL_POINTER;
+        } else {
+            return  Status::OK;
+        }
+    }
+    Status PushBuffer(const std::shared_ptr<AVBuffer>& inBuffer, bool available)
+    {
+        return  Status::OK;
+    }
+    Status ReturnBuffer(const std::shared_ptr<AVBuffer>& inBuffer, bool available)
+    {
+        return  Status::OK;
+    }
+
+    Status AttachBuffer(std::shared_ptr<AVBuffer>& inBuffer, bool isFilled)
+    {
+        return  Status::OK;
+    }
+    Status DetachBuffer(const std::shared_ptr<AVBuffer>& outBuffer)
+    {
+        return  Status::OK;
+    }
+
+    Status SetBufferFilledListener(sptr<IBrokerListener>& listener)
+    {
+        return  Status::OK;
+    }
+    Status RemoveBufferFilledListener(sptr<IBrokerListener>& listener)
+    {
+        return  Status::OK;
+    }
+    Status SetBufferAvailableListener(sptr<IProducerListener>& listener)
+    {
+        return  Status::OK;
+    }
+    Status Clear()
+    {
+        return  Status::OK;
+    }
+    DECLARE_INTERFACE_DESCRIPTOR(u"Media.MyAVBufferQueueProducer");
+
+protected:
+    enum: uint32_t {
+        PRODUCER_GET_QUEUE_SIZE = 0,
+        PRODUCER_SET_QUEUE_SIZE = 1,
+        PRODUCER_REQUEST_BUFFER = 2,
+        PRODUCER_PUSH_BUFFER = 3,
+        PRODUCER_RETURN_BUFFER = 4,
+        PRODUCER_ATTACH_BUFFER = 5,
+        PRODUCER_DETACH_BUFFER = 6,
+        PRODUCER_SET_FILLED_LISTENER = 7,
+        PRODUCER_REMOVE_FILLED_LISTENER = 8,
+        PRODUCER_SET_AVAILABLE_LISTENER = 9
+    };
+};
+
+class MyIAudioDataSource : public IAudioDataSource {
+public:
+    ~MyIAudioDataSource() = default;
+    int32_t ReadAt(std::shared_ptr<AVBuffer> buffer, uint32_t length)
+    {
+        if (buffer == nullptr) {
+            return  0;
+        } else {
+            return  1;
+        }
+    }
+    int32_t GetSize(int64_t& size)
+    {
+        if (size == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+};
+
+class TestEventReceiver : public Pipeline::EventReceiver {
+public:
+    ~TestEventReceiver() = default;
+
+    void OnEvent(const Event &event)
+    {
+        return;
+    }
+
+private:
+};
+
+class TestFilterCallback : public FilterCallback {
+public:
+    explicit TestFilterCallback()
+    {
+        std::cout << "filter back constructor" << std::endl;
+    }
+
+    Status OnCallback(const std::shared_ptr<Filter>& filter,
+        FilterCallBackCommand cmd, StreamType outType)
+
+    {
+        return Status::OK;
+    }
+};
+
+class TestFilter : public Filter {
+public:
+    TestFilter():Filter("TestFilter", FilterType::FILTERTYPE_SOURCE) {}
+    ~TestFilter() = default;
+    Status OnLinked(StreamType inType, const std::shared_ptr<Meta>& meta,
+                            const std::shared_ptr<FilterLinkCallback>& callback)
+    {
+        (void)inType;
+        (void)meta;
+        (void)callback;
+        return onLinked_;
+    }
+protected:
+    Status onLinked_;
+};
+
+
 }  // namespace Pipeline
 }  // namespace Media
 }  // namespace OHOS
