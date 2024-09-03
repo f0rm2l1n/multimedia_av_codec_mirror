570e4f5ccf4d438f34fe428571efdd13c67d6675
道常参商 <yangzhenjun2@huawei.com>
2024-09-03 07:45:52 +0000

    update services/media_engine/plugins/source/http_source/http/http_media_downloader.cpp.
    Signed-off-by: 道常参商 <yangzhenjun2@huawei.com>

--- services/media_engine/plugins/source/http_source/http/http_media_downloader.cpp
+++ services/media_engine/plugins/source/http_source/http/http_media_downloader.cpp
@@ -272,8 +272,8 @@ bool HttpMediaDownloader::HandleBuffering()
     }
 
     if (!isBuffering_ && isFirstFrameArrived_ && callback_ != nullptr) {
-        MEDIA_LOG_I("HTTP CacheData onEvent BUFFERING_END, bufferSize: " PUBLIC_LOG_ZU ", waterLineAbove_: " PUBLIC_LOG_ZU
-            ", isBuffering: " PUBLIC_LOG_D32 ", canWrite: " PUBLIC_LOG_D32,
+        MEDIA_LOG_I("HTTP CacheData onEvent BUFFERING_END, bufferSize: " PUBLIC_LOG_ZU ", waterLineAbove_: "
+        PUBLIC_LOG_ZU ", isBuffering: " PUBLIC_LOG_D32 ", canWrite: " PUBLIC_LOG_D32,
             GetCurrentBufferSize(), waterLineAbove_, isBuffering_, canWrite_.load());
         UpdateCachedPercent(BufferingInfoType::BUFFERING_END);
         callback_->OnEvent({PluginEventType::BUFFERING_END, {BufferingInfoType::BUFFERING_END}, "end"});
