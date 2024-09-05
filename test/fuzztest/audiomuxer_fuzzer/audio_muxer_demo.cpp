f3b1e03a7ebe3a7889c919d89584f6fc527d6e30
韩琦 <hqinup@163.com>
2024-09-05 08:08:11 +0000

    update test/fuzztest/audiomuxer_fuzzer/audio_muxer_demo.cpp.
    Signed-off-by: 韩琦 <hqinup@163.com>

--- test/fuzztest/audiomuxer_fuzzer/audio_muxer_demo.cpp
+++ test/fuzztest/audiomuxer_fuzzer/audio_muxer_demo.cpp
@@ -257,14 +257,12 @@ int32_t AVMuxerDemo::AddTrack(OH_AVMuxer* muxer, int32_t& trackIndex, AudioTrack
     // set codec config
     int extraSize = 0;
     unsigned char buffer[configBufferSize] = {0};
-    if (extraSize <= configBufferSize) {
-        errno_t res = 0;
-        res = strncpy_s(reinterpret_cast<char*>(buffer), extraSize, inputdata.c_str(), extraSize);
-        if (res != 0) {
-            return res;
-        }
-        OH_AVFormat_SetBuffer(trackFormat, OH_MD_KEY_CODEC_CONFIG, buffer, extraSize);
+    errno_t res = 0;
+    res = strncpy_s(reinterpret_cast<char*>(buffer), extraSize, inputdata.c_str(), extraSize);
+    if (res != 0) {
+        return res;
     }
+    OH_AVFormat_SetBuffer(trackFormat, OH_MD_KEY_CODEC_CONFIG, buffer, extraSize);
     OH_AVFormat_SetStringValue(trackFormat, OH_MD_KEY_CODEC_MIME, param.mimeType);
     // audio
     OH_AVFormat_SetIntValue(trackFormat, OH_MD_KEY_AUD_SAMPLE_RATE, param.sampleRate);
