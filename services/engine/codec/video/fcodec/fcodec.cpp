c4986076c2e189a86d7da2a474668ff4b271a214
qihaonan <qihaonan3@huawei.com>
2024-09-11 09:05:43 +0000

    【修改说明】surface轮询改回调&蓝黄差异同步
    Signed-off-by: qihaonan <qihaonan3@huawei.com>

--- services/engine/codec/video/fcodec/fcodec.cpp
+++ services/engine/codec/video/fcodec/fcodec.cpp
@@ -1300,9 +1300,8 @@ GSError FCodec::BufferReleasedByConsumer(uint64_t surfaceId)
 GSError FCodec::RegisterListenerToSurface(const sptr<Surface> &surface)
 {
     uint64_t surfaceId = surface->GetUniqueId();
-    GSError err = surface->RegisterReleaseListener([this, surfaceId](sptr<SurfaceBuffer> &){
-        return BufferReleasedByConsumer(surfaceId);
-    });
+    GSError err = surface->RegisterReleaseListener(
+        [this, surfaceId](sptr<SurfaceBuffer> &) { return BufferReleasedByConsumer(surfaceId); });
     return err;
 }
 
