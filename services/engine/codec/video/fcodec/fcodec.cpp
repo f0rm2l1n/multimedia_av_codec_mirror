626b209fb8712a4dfce8651575c0b49bad4b4fb7
Yanghaolong <yanghaolong2@huawei.com>
2024-09-12 11:40:44 +0000

    修复h264软解surface切换黑屏
    Signed-off-by: Yanghaolong <yanghaolong2@huawei.com>

--- services/engine/codec/video/fcodec/fcodec.cpp
+++ services/engine/codec/video/fcodec/fcodec.cpp
@@ -1241,7 +1241,7 @@ int32_t FCodec::SwitchBetweenSurface(const sptr<Surface> &newSurface)
                 ownedBySurfaceBufferIndex.push_back(index);
             }
         } else {
-            surfaceBuffer = buffers_[INDEX_OUTPUT][index]->sMemory->GetSurfaceBuffer();
+            surfaceBuffer = buffers_[INDEX_OUTPUT][index]->sMemory_->GetSurfaceBuffer();
         }
         if (surfaceBuffer == nullptr) {
             AVCODEC_LOGE("Get old surface buffer error!");
@@ -1275,7 +1275,7 @@ int32_t FCodec::SwitchBetweenSurface(const sptr<Surface> &newSurface)
 
 int32_t FCodec::RenderNewSurfaceWithOldBuffer(const sptr<Surface> &newSurface, uint32_t index)
 {
-    std::shared_ptr<FSurfaceMemory> surfaceMemory = buffers_[INDEX_OUTPUT][index]->sMemory;
+    std::shared_ptr<FSurfaceMemory> surfaceMemory = buffers_[INDEX_OUTPUT][index]->sMemory_;
     sptr<SurfaceBuffer> surfaceBuffer = renderSurfaceBufferMap_[index];
     OHOS::BufferFlushConfig flushConfig = {{0, 0, surfaceBuffer->GetWidth(), surfaceBuffer->GetHeight()},
         outAVBuffer4Surface_[index]->pts_};
