6fe8eee3129bb0aa7078d6721a71e7a823e320d8
Yanghaolong <yanghaolong2@huawei.com>
2024-09-12 07:48:41 +0000

    修复h265软解surface切换会黑屏的问题
    Signed-off-by: Yanghaolong <yanghaolong2@huawei.com>

--- services/engine/codec/video/hevcdecoder/hevc_decoder.h
+++ services/engine/codec/video/hevcdecoder/hevc_decoder.h
@@ -142,7 +142,7 @@ private:
     int32_t ReplaceOutputSurfaceWhenRunning(sptr<Surface> newSurface);
     int32_t SetQueueSize(const sptr<Surface> &surface, uint32_t targetSize);
     int32_t AttachToNewSurface(const sptr<Surface> &newSurface);
-    int32_t FlushSurfaceMemory(std::shared_ptr<FSurfaceMemory> &surfaceMemory, int64_t pts);
+    int32_t FlushSurfaceMemory(std::shared_ptr<FSurfaceMemory> &surfaceMemory, uint32_t index);
     int32_t GetSurfaceBufferStride(const std::shared_ptr<HBuffer> &frameBuffer);
     int32_t SetSurfaceCfg(int32_t bufferCnt);
     int32_t DecodeFrameOnce();
@@ -190,6 +190,7 @@ private:
     std::shared_ptr<BlockQueue<uint32_t>> inputAvailQue_;
     std::shared_ptr<BlockQueue<uint32_t>> codecAvailQue_;
     std::shared_ptr<BlockQueue<uint32_t>> renderAvailQue_;
+    std::map<uint32_t, sptr<SurfaceBuffer>> renderSurfaceBufferMap_;
     sptr<Surface> surface_ = nullptr;
     std::shared_ptr<TaskThread> sendTask_ = nullptr;
     std::shared_ptr<TaskThread> renderTask_ = nullptr;
