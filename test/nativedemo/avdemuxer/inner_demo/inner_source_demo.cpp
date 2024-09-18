929b591a3a4920328672ebbf4e90b6610773b508
y30025806 <yangjunhui10@huawei.com>
2024-09-18 19:04:29 +0800

    fix review
    Signed-off-by: y30025806 <yangjunhui10@huawei.com>
    Change-Id: I47057689033e81526f379c1d77c7f21ab2e55b4f

--- test/nativedemo/avdemuxer/inner_demo/inner_source_demo.cpp
+++ test/nativedemo/avdemuxer/inner_demo/inner_source_demo.cpp
@@ -67,26 +67,22 @@ int32_t InnerSourceDemo::CreateWithFD(int32_t fd, int64_t offset, int64_t size)
 
 Format InnerSourceDemo::GetSourceFormat()
 {
-    if (this->avsource_ == nullptr) {
-        printf("GetSourceFormat is failed\n");
-        return -1;
-    }
-    int32_t ret = this->avsource_->GetSourceFormat(source_format_);
-    if (ret != 0) {
-        printf("GetSourceFormat is failed\n");
+    if (this->avsource_ != nullptr) {
+        int32_t ret = this->avsource_->GetSourceFormat(source_format_);
+        if (ret != 0) {
+            printf("GetSourceFormat is failed\n");
+        }
     }
     return source_format_;
 }
 
 Format InnerSourceDemo::GetTrackFormat(uint32_t trackIndex)
 {
-    if (this->avsource_ == nullptr) {
-        printf("GetSourceFormat is failed\n");
-        return -1;
-    }
-    int32_t ret = this->avsource_->GetTrackFormat(track_format_, trackIndex);
-    if (ret != 0) {
-        printf("GetTrackFormat is failed\n");
+    if (this->avsource_ != nullptr) {
+        int32_t ret = this->avsource_->GetTrackFormat(track_format_, trackIndex);
+        if (ret != 0) {
+            printf("GetTrackFormat is failed\n");
+        }
     }
     return track_format_;
 }
