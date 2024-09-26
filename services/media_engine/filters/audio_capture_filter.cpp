52d14e22d55ddc1a1f01f95965a9ed2c9f905b1e
彭同辉 <pengtonghui@huawei.com>
2024-09-26 02:47:59 +0000

    update services/media_engine/filters/audio_capture_filter.cpp.
    Signed-off-by: 彭同辉 <pengtonghui@huawei.com>

--- services/media_engine/filters/audio_capture_filter.cpp
+++ services/media_engine/filters/audio_capture_filter.cpp
@@ -20,7 +20,7 @@
 #include "avcodec_trace.h"
 
 namespace {
-constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, LOG_DOMAIN_SYSTEM_PLAYER, "AudioCaptureFilter" };
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, LOG_DOMAIN_SCREENCAPTURE, "AudioCaptureFilter" };
 }
 
 namespace OHOS {
