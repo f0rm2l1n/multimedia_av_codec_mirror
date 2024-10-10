e96895ef40ab832642d7965cb1fc18773ade1d1f
west <weihouyao@huawei.com>
2024-10-10 20:27:53 +0800

    【修改说明】delete encoder surface set usage
    Signed-off-by: west <weihouyao@huawei.com>
    Change-Id: Ie69f13fe35e83bfdd88e3ef2a9116b0021836271

--- test/unittest/video_test/video_test/capbilities/video_codec/video_encoder/video_encoder.cpp
+++ test/unittest/video_test/video_test/capbilities/video_codec/video_encoder/video_encoder.cpp
@@ -146,7 +146,6 @@ int32_t VideoEncoder::GetSurface(SampleInfo &sampleInfo)
         "Get surface failed, ret: %{public}d", ret);
     (void)OH_NativeWindow_NativeWindowHandleOpt(window, SET_BUFFER_GEOMETRY,
         sampleInfo.videoWidth, sampleInfo.videoHeight);
-    (void)OH_NativeWindow_NativeWindowHandleOpt(window, SET_USAGE, 16427);      // 16427: Window usage
     (void)OH_NativeWindow_NativeWindowHandleOpt(window, SET_FORMAT,
         ToGraphicPixelFormat(sampleInfo.pixelFormat, sampleInfo.profile));
 
