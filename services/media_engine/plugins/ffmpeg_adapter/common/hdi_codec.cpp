0e1cd43fef5e8bcbe188ff7390f9ac36c300fb8d
gao_ziyu <gaoziyu@huawei.com>
2024-10-18 11:12:29 +0800

    fix lbvc
    Signed-off-by: gao_ziyu <gaoziyu@huawei.com>

--- services/media_engine/plugins/ffmpeg_adapter/common/hdi_codec.cpp
+++ services/media_engine/plugins/ffmpeg_adapter/common/hdi_codec.cpp
@@ -25,7 +25,7 @@ namespace OHOS {
 namespace Media {
 namespace Plugins {
 namespace Hdi {
-#define SAMPLE_RATE_NUM 16
+constexpr int SAMPLE_RATE_NUM 16
 HdiCodec::HdiCodec()
     : componentId_(0),
       componentName_(""),
