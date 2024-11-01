e9efd00d7675f94b2689e12b0169815550efa6f5
xhj <13735532946@163.com>
2024-11-01 11:01:11 +0800

    提取stop接口,新增单独的测试接口。
    Signed-off-by: xhj <13735532946@163.com>

--- test/moduletest/vcodec/hwdecoder/src/hwh265_hdr2sdr_state_test.cpp
+++ test/moduletest/vcodec/hwdecoder/src/hwh265_hdr2sdr_state_test.cpp
@@ -334,11 +334,23 @@ HWTEST_F(HwdecHdr2SdrStateNdkTest, HEVC_HW_HDR2SDR_STATE_1100, TestSize.Level2)
     vDecSample2->SF_OUTPUT = true;
     int32_t ret = vDecSample2->DecodeSetSurface();
     ASSERT_EQ(AV_ERR_OK, ret);
-    ret = vDecSample2->Stop();
-    ASSERT_EQ(AV_ERR_OK, ret);
     ret = vDecSample2->Flush();
     ASSERT_EQ(AV_ERR_INVALID_STATE, ret);
     ret = vDecSample2->SetVideoDecoderCallback();
+    ASSERT_EQ(AV_ERR_OK, ret);
+}
+
+/**
+ * @tc.number    : HEVC_HW_HDR2SDR_STATE_1101
+ * @tc.name      : create-configure-stop-surface
+ * @tc.desc      : function test
+ */
+HWTEST_F(HwdecHdr2SdrStateNdkTest, HEVC_HW_HDR2SDR_STATE_1101, TestSize.Level2)
+{
+    vDecSample2->SF_OUTPUT = true;
+    int32_t ret = vDecSample2->DecodeSetSurface();
+    ASSERT_EQ(AV_ERR_OK, ret);
+    ret = vDecSample2->Stop();
     ASSERT_EQ(AV_ERR_OK, ret);
 }
 
