17b3e439a995f8248b6e59af342cbd45075dc5c1
彼此彼此 <chenyuchen8@huawei.com>
2024-11-01 14:15:13 +0800

    fix code id
    Signed-off-by: 彼此彼此 <chenyuchen8@huawei.com>
    Change-Id: I5cee3e91afc01672e0440e06c23273feac25ef67

--- services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_format_helper.cpp
+++ services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_format_helper.cpp
@@ -705,7 +705,7 @@ void FFmpegFormatHelper::ParseAudioTrackInfo(const AVStream& avStream, Meta &for
     format.Set<Tag::AUDIO_BITS_PER_CODED_SAMPLE>(avStream.codecpar->bits_per_coded_sample);
     format.Set<Tag::AUDIO_BITS_PER_RAW_SAMPLE>(avStream.codecpar->bits_per_raw_sample);
 
-    if (avStream.codecpar->codec_id == AV_CODEC_ID_AV3A) {
+    if (avStream.codecpar->codec_id == AV_CODEC_ID_AVS3DA) {
         ParseAv3aInfo(avStream, format);
     }
 }
