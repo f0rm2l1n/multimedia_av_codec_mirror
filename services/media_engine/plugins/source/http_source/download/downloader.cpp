0c0854b963351706f539c3646ad419ba460c5360
liye <liye87@huawei.com>
2024-11-06 17:50:22 +0800

    解决httpsource部分crash
    Signed-off-by: liye <liye87@huawei.com>

--- services/media_engine/plugins/source/http_source/download/downloader.cpp
+++ services/media_engine/plugins/source/http_source/download/downloader.cpp
@@ -159,7 +159,7 @@ void DownloadRequest::Close()
 
 void DownloadRequest::WaitHeaderUpdated() const
 {
-    isHeaderUpdating_ = true;
+    isHeaderUpdating_.store(true, std::memory_order_release);
     MediaAVCodec::AVCodecTrace trace("DownloadRequest::WaitHeaderUpdated");
     // Wait Header(fileContentLen etc.) updated
     while (!isHeaderUpdated_ && times_ < RETRY_TIMES && !isInterruptNeeded_ && !headerInfo_.isClosed) {
@@ -168,7 +168,7 @@ void DownloadRequest::WaitHeaderUpdated() const
     }
     MEDIA_LOG_D("isHeaderUpdated_ " PUBLIC_LOG_D32 ", times " PUBLIC_LOG_ZU ", isClosed " PUBLIC_LOG_D32,
         isHeaderUpdated_.load(), times_.load(), headerInfo_.isClosed.load());
-    isHeaderUpdating_ = false;
+    isHeaderUpdating_.store(false, std::memory_order_release);
 }
 
 double DownloadRequest::GetDuration() const
