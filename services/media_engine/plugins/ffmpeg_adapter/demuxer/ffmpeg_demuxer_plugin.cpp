766900e7594151ab9bf9d0e95dcf2a882357e3ac
杨俊晖 <yangjunhui10@huawei.com>
2024-11-27 22:27:21 +0800

    fix first frame in hdrvivid stream, when dynamic metadata in hvc1 box
    Signed-off-by: 杨俊晖 <yangjunhui10@huawei.com>

--- services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.cpp
+++ services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.cpp
@@ -1367,6 +1367,11 @@ Status FFmpegDemuxerPlugin::SeekTo(int32_t trackId, int64_t seekTime, SeekMode m
         cacheQueue_.RemoveTrackQueue(selectedTrackIds_[i]);
         cacheQueue_.AddTrackQueue(selectedTrackIds_[i]);
     }
+    // fix first frame in hdrvivid stream, when dynamic metadata in hvc1 box
+    if (avStream->codecpar != nullptr && avStream->codecpar->codec_id == AV_CODEC_ID_HEVC &&
+        streamParser_ != nullptr && streamParser_->IsHdrVivid() && seekTime == 0) {
+        streamParser_->ResetXPSSendStatus();        
+    }
     return Status::OK;
 }
 
