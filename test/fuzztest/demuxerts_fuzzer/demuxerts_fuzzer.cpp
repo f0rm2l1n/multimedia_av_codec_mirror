93b9d35d49965af5a8f986f83bd96ab6a3c23a46
dwl <dengwanlin1@h-partners.com>
2024-11-28 14:08:14 +0800

    修改fuzz用例
    Signed-off-by: dwl <dengwanlin1@h-partners.com>

--- test/fuzztest/demuxerts_fuzzer/demuxerts_fuzzer.cpp
+++ test/fuzztest/demuxerts_fuzzer/demuxerts_fuzzer.cpp
@@ -29,29 +29,29 @@ using namespace std;
 using namespace OHOS::Media;
 namespace OHOS {
 const char *TS_PATH = "/data/test/fuzz_create.ts";
-const int64_t g_expectSize = 37;
-const size_t g_timeSize = 5;
-const size_t g_uriSize = 25;
-const size_t g_uriBufferSize = 21;
-const int64_t g_uriCount = 20;
-const char g_flag = '\0';
-const size_t g_trackTypeSize = 26;
-const size_t g_durationSize = 27;
-const size_t g_heightSize = 28;
-const size_t g_frameRateSize = 29;
-const size_t g_languageSize = 31;
-const size_t g_languageBufferSize = 3;
-const size_t g_languageCount = 2;
-const size_t g_codecConfigSize = 32;
-const size_t g_sampleRateSize = 33;
-const size_t g_channelCount = 34;
-const size_t g_videoHeightSize = 35;
-const size_t g_videoWidthSize = 36;
+const int64_t EXPECT_SIZE = 37;
+const size_t TIME_SIZE = 5;
+const size_t URI_SIZE = 25;
+const size_t URI_BUFFER_SIZE = 21;
+const int64_t URI_COUNT = 20;
+const char FLAG = '\0';
+const size_t TRACK_TYPE_SIZE = 26;
+const size_t DURATION_SIZE = 27;
+const size_t HEIGHT_SIZE = 28;
+const size_t FRAME_RATE_SIZE = 29;
+const size_t LANGUAGE_SIZE = 31;
+const size_t LANGUAGE_BUFFER_SIZE = 3;
+const size_t LANGUAGE_COUNT = 2;
+const size_t CODEC_CONFIG_SIZE = 32;
+const size_t SAMPLE_RATE_SIZE = 33;
+const size_t CHANNEL_COUNT = 34;
+const size_t VIDEO_HEIGHT_SIZE = 35;
+const size_t VIDEO_WIDTH_SIZE = 36;
 
 
 bool DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
 {
-    if (size < g_expectSize) {
+    if (size < EXPECT_SIZE) {
         return false;
     }
     int32_t fd = open(TS_PATH, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
@@ -64,29 +64,29 @@ bool DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
     }
     close(fd);
     struct Params params;
-    params.time = data[size - g_timeSize];
-    char *uri = new char[g_uriBufferSize];
-    if (memcpy_s(uri, g_uriBufferSize, data  + size - g_uriSize, g_uriCount) != 0) {
+    params.time = data[size - TIME_SIZE];
+    char *uri = new char[URI_BUFFER_SIZE];
+    if (memcpy_s(uri, URI_BUFFER_SIZE, data  + size - URI_SIZE, URI_COUNT) != 0) {
         delete[] uri;
         return false;
     }
-    uri[g_uriCount] = g_flag;
-    params.setTrackType = data[size - g_trackTypeSize];
-    params.setDuration = data[size - g_durationSize];
-    params.setHeight = data[size - g_heightSize];
-    params.setFrameRate = data[size - g_frameRateSize];
-    char *setLanguage = new char[g_languageBufferSize];
-    if (memcpy_s(setLanguage, g_languageBufferSize, data + size - g_languageSize, g_languageCount) != 0) {
+    uri[URI_COUNT] = FLAG;
+    params.setTrackType = data[size - TRACK_TYPE_SIZE];
+    params.setDuration = data[size - DURATION_SIZE];
+    params.setHeight = data[size - HEIGHT_SIZE];
+    params.setFrameRate = data[size - FRAME_RATE_SIZE];
+    char *setLanguage = new char[LANGUAGE_BUFFER_SIZE];
+    if (memcpy_s(setLanguage, LANGUAGE_BUFFER_SIZE, data + size - LANGUAGE_SIZE, LANGUAGE_COUNT) != 0) {
         delete[] uri;
         delete[] setLanguage;
         return false;
     }
-    setLanguage[g_languageCount] = g_flag;
-    params.setCodecConfigSize = data[size - g_codecConfigSize];
-    params.sampleRate = data[size - g_sampleRateSize];
-    params.channelCount = data[size - g_channelCount];
-    params.setVideoHeight = data[size - g_videoHeightSize];
-    params.setVideoWidth = data[size - g_videoWidthSize];
+    setLanguage[LANGUAGE_COUNT] = FLAG;
+    params.setCodecConfigSize = data[size - CODEC_CONFIG_SIZE];
+    params.sampleRate = data[size - SAMPLE_RATE_SIZE];
+    params.channelCount = data[size - CHANNEL_COUNT];
+    params.setVideoHeight = data[size - VIDEO_HEIGHT_SIZE];
+    params.setVideoWidth = data[size - VIDEO_WIDTH_SIZE];
     uint8_t *dataConver = const_cast<uint8_t *>(data);
     uint32_t *createSize = reinterpret_cast<uint32_t *>(dataConver + size - 4);
     shared_ptr<DemuxerSample> demuxerSample = make_shared<DemuxerSample>();
