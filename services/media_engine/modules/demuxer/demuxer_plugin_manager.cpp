636b2533f782e8f3bbed9edda51ef694b95c9f47
fanpingan <fanpingan1@huawei.com>
2024-12-06 06:23:42 +0000

    update services/media_engine/modules/demuxer/demuxer_plugin_manager.cpp.
    Signed-off-by: fanpingan <fanpingan1@huawei.com>

--- services/media_engine/modules/demuxer/demuxer_plugin_manager.cpp
+++ services/media_engine/modules/demuxer/demuxer_plugin_manager.cpp
@@ -574,7 +574,7 @@ Status DemuxerPluginManager::RebootPlugin(int32_t streamId, TrackType trackType,
         return Status::ERROR_INVALID_PARAMETER;
     }
 
-    // Start to reboot demuxer plugin while streamId is not changed 
+    // Start to reboot demuxer plugin while streamId is not changed
     streamInfoMap_[streamId].activated = true;
     if (streamInfoMap_[streamId].plugin != nullptr) {
         streamInfoMap_[streamId].plugin.reset();
