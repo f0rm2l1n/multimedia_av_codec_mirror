09dc9514460bdccac41e81cbe12611328ef564e3
west <weihouyao@huawei.com>
2025-01-08 15:02:44 +0800

    【修改说明】av_codec_service 支持资源管理
    Signed-off-by: west <weihouyao@huawei.com>
    Change-Id: I9dd087bea1e0d93bf8894d2643ef54ae14426ea6

--- services/services/common/event_manager/event_handler/instance_memory_update_event_handler/instance_memory_calculator.cpp
+++ services/services/common/event_manager/event_handler/instance_memory_update_event_handler/instance_memory_calculator.cpp
@@ -63,7 +63,7 @@ struct CalculatorParameter {
     }
 };
 
-static const CalculatorParameter HardwareDecoderHevc10BitYUV420Parameter = {
+static const CalculatorParameter HARDWARE_DECODER_HEVC_10BIT_YUV420_PARAMETER = {
     AVCODEC_TYPE_VIDEO_DECODER,
     CodecMimeType::VIDEO_HEVC.data(),
     CalculatorParameterPixelFormat::YUV420,
@@ -72,7 +72,7 @@ static const CalculatorParameter HardwareDecoderHevc10BitYUV420Parameter = {
     false
 };
 
-static const CalculatorParameter HardwareDecoderHevc10BitYUV420PostProcessingParameter = {
+static const CalculatorParameter HARDWARE_DECODER_HEVC_10BIT_YUV420_POSTPROCESSING_PARAMETER = {
     AVCODEC_TYPE_VIDEO_DECODER,
     CodecMimeType::VIDEO_HEVC.data(),
     CalculatorParameterPixelFormat::YUV420,
@@ -81,7 +81,7 @@ static const CalculatorParameter HardwareDecoderHevc10BitYUV420PostProcessingPar
     true
 };
 
-static const CalculatorParameter HardwareDecoderVvc10BitYUV420Parameter = {
+static const CalculatorParameter HARDWARE_DECODER_VVC_10BIT_YUV420_PARAMETER = {
     AVCODEC_TYPE_VIDEO_DECODER,
     CodecMimeType::VIDEO_VVC.data(),
     CalculatorParameterPixelFormat::YUV420,
@@ -90,7 +90,7 @@ static const CalculatorParameter HardwareDecoderVvc10BitYUV420Parameter = {
     false
 };
 
-static const CalculatorParameter HardwareDecoderVvcYUV420Parameter = {
+static const CalculatorParameter HARDWARE_DECODER_VVC_YUV420_PARAMETER = {
     AVCODEC_TYPE_VIDEO_DECODER,
     CodecMimeType::VIDEO_VVC.data(),
     CalculatorParameterPixelFormat::YUV420,
@@ -99,7 +99,7 @@ static const CalculatorParameter HardwareDecoderVvcYUV420Parameter = {
     false
 };
 
-static const CalculatorParameter HardwareDecoderAvcYUV420Parameter = {
+static const CalculatorParameter HARDWARE_DECODER_AVC_YUV420_PARAMETER = {
     AVCODEC_TYPE_VIDEO_DECODER,
     CodecMimeType::VIDEO_AVC.data(),
     CalculatorParameterPixelFormat::YUV420,
@@ -108,7 +108,7 @@ static const CalculatorParameter HardwareDecoderAvcYUV420Parameter = {
     false
 };
 
-static const CalculatorParameter HardwareDecoderHevcYUV420Parameter = {
+static const CalculatorParameter HARDWARE_DECODER_HEVC_YUV420_PARAMETER = {
     AVCODEC_TYPE_VIDEO_DECODER,
     CodecMimeType::VIDEO_HEVC.data(),
     CalculatorParameterPixelFormat::YUV420,
@@ -117,7 +117,7 @@ static const CalculatorParameter HardwareDecoderHevcYUV420Parameter = {
     false
 };
 
-static const CalculatorParameter HardwareEncoderHevc10BitYUV420Parameter = {
+static const CalculatorParameter HARDWARE_ENCODER_HEVC_10BIT_YUV420_PARAMETER = {
     AVCODEC_TYPE_VIDEO_ENCODER,
     CodecMimeType::VIDEO_HEVC.data(),
     CalculatorParameterPixelFormat::YUV420,
@@ -126,7 +126,7 @@ static const CalculatorParameter HardwareEncoderHevc10BitYUV420Parameter = {
     false
 };
 
-static const CalculatorParameter HardwareEncoderAvcRGBAParameter = {
+static const CalculatorParameter HARDWARE_ENCODER_AVC_RGBA_PARAMETER = {
     AVCODEC_TYPE_VIDEO_ENCODER,
     CodecMimeType::VIDEO_AVC.data(),
     CalculatorParameterPixelFormat::RGBA,
@@ -135,7 +135,7 @@ static const CalculatorParameter HardwareEncoderAvcRGBAParameter = {
     false
 };
 
-static const CalculatorParameter HardwareEncoderHevcRGBAParameter = {
+static const CalculatorParameter HARDWARE_ENCODER_HEVC_RGBA_PARAMETER = {
     AVCODEC_TYPE_VIDEO_ENCODER,
     CodecMimeType::VIDEO_HEVC.data(),
     CalculatorParameterPixelFormat::RGBA,
@@ -144,7 +144,7 @@ static const CalculatorParameter HardwareEncoderHevcRGBAParameter = {
     false
 };
 
-static const CalculatorParameter HardwareEncoderAvcYUV420Parameter = {
+static const CalculatorParameter HARDWARE_ENCODER_AVC_YUV420_PARAMETER = {
     AVCODEC_TYPE_VIDEO_ENCODER,
     CodecMimeType::VIDEO_AVC.data(),
     CalculatorParameterPixelFormat::YUV420,
@@ -153,7 +153,7 @@ static const CalculatorParameter HardwareEncoderAvcYUV420Parameter = {
     false
 };
 
-static const CalculatorParameter HardwareEncoderHevcYUV420Parameter = {
+static const CalculatorParameter HARDWARE_ENCODER_HEVC_YUV420_PARAMETER = {
     AVCODEC_TYPE_VIDEO_ENCODER,
     CodecMimeType::VIDEO_HEVC.data(),
     CalculatorParameterPixelFormat::YUV420,
@@ -162,7 +162,7 @@ static const CalculatorParameter HardwareEncoderHevcYUV420Parameter = {
     false
 };
 
-static const CalculatorParameter SoftwareDecoderAvcRGBAParameter = {
+static const CalculatorParameter SOFTWARE_DECODER_AVC_RGBA_PARAMETER = {
     AVCODEC_TYPE_VIDEO_DECODER,
     CodecMimeType::VIDEO_AVC.data(),
     CalculatorParameterPixelFormat::RGBA,
@@ -171,7 +171,7 @@ static const CalculatorParameter SoftwareDecoderAvcRGBAParameter = {
     false
 };
 
-static const CalculatorParameter SoftwareDecoderAvcYUV420Parameter = {
+static const CalculatorParameter SOFTWARE_DECODER_AVC_YUV420_PARAMETER = {
     AVCODEC_TYPE_VIDEO_DECODER,
     CodecMimeType::VIDEO_AVC.data(),
     CalculatorParameterPixelFormat::YUV420,
@@ -180,7 +180,7 @@ static const CalculatorParameter SoftwareDecoderAvcYUV420Parameter = {
     false
 };
 
-static const CalculatorParameter SoftwareDecoderHevcYUV420Parameter = {
+static const CalculatorParameter SOFTWARE_DECODER_HEVC_YUV420_PARAMETER = {
     AVCODEC_TYPE_VIDEO_DECODER,
     CodecMimeType::VIDEO_HEVC.data(),
     CalculatorParameterPixelFormat::YUV420,
@@ -197,36 +197,56 @@ constexpr uint32_t BLOCK_SIZE_HARDWARED_PROFILE_LEVEL_5_1 = 36686;
 
 uint32_t HardwareDecoderHevc10BitYUV420(uint32_t blockSize)
 {
+    auto linearSlope = 0.0;
+    auto linearIntercept = 0U;
     if (blockSize <= BLOCK_SIZE_HARDWARED_PROFILE_LEVEL_3_1) {
-        return 9.616 * blockSize + 29596;
+        linearSlope = 9.616;
+        linearIntercept = 29596;
     } else if (blockSize <= BLOCK_SIZE_HARDWARED_PROFILE_LEVEL_4_1) {
-        return 9.270 * blockSize + 43637;
+        linearSlope = 9.270;
+        linearIntercept = 43637;
     } else if (blockSize <= BLOCK_SIZE_HARDWARED_PROFILE_LEVEL_5_1) {
-        return 9.319 * blockSize + 55833;
+        linearSlope = 9.319;
+        linearIntercept = 55833;
     }
-    return 9.319 * blockSize + 129228;
+    linearSlope = 9.319;
+    linearIntercept = 129228;
+    return static_cast<uint32_t>(linearSlope * blockSize + linearIntercept);
 }
 
 uint32_t HardwareDecoderHevc10BitYUV420PostProcessing(uint32_t blockSize)
 {
+    auto linearSlope = 0.0;
+    auto linearIntercept = 0U;
     if (blockSize <= BLOCK_SIZE_HARDWARED_PROFILE_LEVEL_3_1) {
-        return 11.428 * blockSize + 64363;
+        linearSlope = 11.428;
+        linearIntercept = 64363;
     } else if (blockSize <= BLOCK_SIZE_HARDWARED_PROFILE_LEVEL_4_1) {
-        return 10.999 * blockSize + 78945;
+        linearSlope = 10.999;
+        linearIntercept = 78945;
     } else if (blockSize <= BLOCK_SIZE_HARDWARED_PROFILE_LEVEL_5_1) {
-        return 11.149 * blockSize + 90380;
+        linearSlope = 11.149;
+        linearIntercept = 90380;
     }
-    return 11.048 * blockSize + 167618;
+    linearSlope = 11.048;
+    linearIntercept = 167618;
+    return static_cast<uint32_t>(linearSlope * blockSize + linearIntercept);
 }
 
 uint32_t HardwareDecoderVvc10BitYUV420(uint32_t blockSize)
 {
+    auto linearSlope = 0.0;
+    auto linearIntercept = 0U;
     if (blockSize <= BLOCK_SIZE_HARDWARED_PROFILE_LEVEL_3_1) {
-        return 4.469 * blockSize + 13035;
+        linearSlope = 4.469;
+        linearIntercept = 13035;
     } else if (blockSize <= BLOCK_SIZE_HARDWARED_PROFILE_LEVEL_4_1) {
-        return 4.175 * blockSize + 20644;
+        linearSlope = 4.175;
+        linearIntercept = 20644;
     }
-    return 4.221 * blockSize + 32769;
+    linearSlope = 4.221;
+    linearIntercept = 32769;
+    return static_cast<uint32_t>(linearSlope * blockSize + linearIntercept);
 }
 
 uint32_t HardwareDecoderVvcYUV420(uint32_t blockSize)
@@ -237,61 +257,80 @@ uint32_t HardwareDecoderVvcYUV420(uint32_t blockSize)
 
 uint32_t HardwareDecoderYUV420(uint32_t blockSize)
 {
+    auto linearSlope = 0.0;
+    auto linearIntercept = 0U;
     if (blockSize <= BLOCK_SIZE_HARDWARED_PROFILE_LEVEL_3_1) {
-        return 5.702 * blockSize + 28934;
+        linearSlope = 5.702;
+        linearIntercept = 28934;
     } else if (blockSize <= BLOCK_SIZE_HARDWARED_PROFILE_LEVEL_4_1) {
-        return 5.478 * blockSize + 42493;
+        linearSlope = 5.478;
+        linearIntercept = 42493;
     } else if (blockSize <= BLOCK_SIZE_HARDWARED_PROFILE_LEVEL_5_1) {
-        return 5.513 * blockSize + 54700;
+        linearSlope = 5.513;
+        linearIntercept = 54700;
     }
-    return 5.487 * blockSize + 129299;
+    linearSlope = 5.487;
+    linearIntercept = 129299;
+    return static_cast<uint32_t>(linearSlope * blockSize + linearIntercept);
 }
 
 uint32_t HardwareEncoderHevc10BitYUV420(uint32_t blockSize)
 {
-    return 5.930 * blockSize + 40394;
+    auto linearSlope = 5.930;
+    auto linearIntercept = 40394;
+    return static_cast<uint32_t>(linearSlope * blockSize + linearIntercept);
 }
 
 uint32_t HardwareEncoder8BitRGBA(uint32_t blockSize)
 {
-    return 7.518 * blockSize + 6475;
+    auto linearSlope = 7.518;
+    auto linearIntercept = 6475;
+    return static_cast<uint32_t>(linearSlope * blockSize + linearIntercept);
 }
 
 uint32_t HardwareEncoderYUV420(uint32_t blockSize)
 {
-    return 4.541 * blockSize + 7989;
+    auto linearSlope = 4.541;
+    auto linearIntercept = 7989;
+    return static_cast<uint32_t>(linearSlope * blockSize + linearIntercept);
 }
 
 uint32_t SoftwareDecoderAvcRGBA(uint32_t blockSize)
 {
-    return 6.922 * blockSize + 10293;
+    auto linearSlope = 6.922;
+    auto linearIntercept = 10293;
+    return static_cast<uint32_t>(linearSlope * blockSize + linearIntercept);
 }
 
 uint32_t SoftwareDecoderAvcYUV420(uint32_t blockSize)
 {
-    return 3.273 * blockSize + 10659;
+    auto linearSlope = 3.273;
+    auto linearIntercept = 10659;
+    return static_cast<uint32_t>(linearSlope * blockSize + linearIntercept);
 }
 
 uint32_t SoftwareDecoderHevcYUV420(uint32_t blockSize)
 {
-    return 1.510 * blockSize + 277115;
+    auto linearSlope = 1.510;
+    auto linearIntercept = 277115;
+    return static_cast<uint32_t>(linearSlope * blockSize + linearIntercept);
 }
 
 const std::map<CalculatorParameter, uint32_t (*)(uint32_t)> CALCULATOR_MAP = {
-    {HardwareDecoderHevc10BitYUV420Parameter, HardwareDecoderHevc10BitYUV420},
-    {HardwareDecoderHevc10BitYUV420PostProcessingParameter, HardwareDecoderHevc10BitYUV420PostProcessing},
-    {HardwareDecoderVvc10BitYUV420Parameter, HardwareDecoderVvc10BitYUV420},
-    {HardwareDecoderVvcYUV420Parameter, HardwareDecoderVvcYUV420},
-    {HardwareDecoderAvcYUV420Parameter, HardwareDecoderYUV420},
-    {HardwareDecoderHevcYUV420Parameter, HardwareDecoderYUV420},
-    {HardwareEncoderHevc10BitYUV420Parameter, HardwareEncoderHevc10BitYUV420},
-    {HardwareEncoderAvcRGBAParameter, HardwareEncoder8BitRGBA},
-    {HardwareEncoderHevcRGBAParameter, HardwareEncoder8BitRGBA},
-    {HardwareEncoderAvcYUV420Parameter, HardwareEncoderYUV420},
-    {HardwareEncoderHevcYUV420Parameter, HardwareEncoderYUV420},
-    {SoftwareDecoderAvcRGBAParameter, SoftwareDecoderAvcRGBA},
-    {SoftwareDecoderAvcYUV420Parameter, SoftwareDecoderAvcYUV420},
-    {SoftwareDecoderHevcYUV420Parameter, SoftwareDecoderHevcYUV420}
+    {HARDWARE_DECODER_HEVC_10BIT_YUV420_PARAMETER, HardwareDecoderHevc10BitYUV420},
+    {HARDWARE_DECODER_HEVC_10BIT_YUV420_POSTPROCESSING_PARAMETER, HardwareDecoderHevc10BitYUV420PostProcessing},
+    {HARDWARE_DECODER_VVC_10BIT_YUV420_PARAMETER, HardwareDecoderVvc10BitYUV420},
+    {HARDWARE_DECODER_VVC_YUV420_PARAMETER, HardwareDecoderVvcYUV420},
+    {HARDWARE_DECODER_AVC_YUV420_PARAMETER, HardwareDecoderYUV420},
+    {HARDWARE_DECODER_HEVC_YUV420_PARAMETER, HardwareDecoderYUV420},
+    {HARDWARE_ENCODER_HEVC_10BIT_YUV420_PARAMETER, HardwareEncoderHevc10BitYUV420},
+    {HARDWARE_ENCODER_AVC_RGBA_PARAMETER, HardwareEncoder8BitRGBA},
+    {HARDWARE_ENCODER_HEVC_RGBA_PARAMETER, HardwareEncoder8BitRGBA},
+    {HARDWARE_ENCODER_AVC_YUV420_PARAMETER, HardwareEncoderYUV420},
+    {HARDWARE_ENCODER_HEVC_YUV420_PARAMETER, HardwareEncoderYUV420},
+    {SOFTWARE_DECODER_AVC_RGBA_PARAMETER, SoftwareDecoderAvcRGBA},
+    {SOFTWARE_DECODER_AVC_YUV420_PARAMETER, SoftwareDecoderAvcYUV420},
+    {SOFTWARE_DECODER_HEVC_YUV420_PARAMETER, SoftwareDecoderHevcYUV420}
 };
 
 std::optional<CalculatorType> InstanceMemoryUpdateEventHandler::GetCalculator(const Media::Meta &meta)
