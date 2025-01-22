cf2487ec78db457cb7b947d041542ad4f40e6f20
wanganzhou <wanganzhou1@h-partners.com>
2025-01-22 01:25:43 +0000

    update services/media_engine/plugins/ffmpeg_adapter/muxer/ffmpeg_muxer_plugin.cpp.
    Signed-off-by: wanganzhou <wanganzhou1@h-partners.com>

--- services/media_engine/plugins/ffmpeg_adapter/muxer/ffmpeg_muxer_plugin.cpp
+++ services/media_engine/plugins/ffmpeg_adapter/muxer/ffmpeg_muxer_plugin.cpp
@@ -1015,7 +1015,10 @@ uint8_t *FFmpegMuxerPlugin::FindNalStartCode(const uint8_t *buf, const uint8_t *
 
 bool FFmpegMuxerPlugin::IsAvccSample(const uint8_t* sample, int32_t size, int32_t nalSizeLen)
 {
-    FALSE_RETURN_V_MSG_E(sample != nullptr, Status::ERROR_INVALID_PARAMETER, "sample is null");
+    if (sample == nullptr) {
+        MEDIA_LOG_E("sample is null");
+        return false;
+    }
     if (size < nalSizeLen) {
         return false;
     }
