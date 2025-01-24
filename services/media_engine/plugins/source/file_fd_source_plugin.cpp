bc356c6af66b516f68d462b1f9fc161764ccfc8e
Feng Lin <linfeng67@huawei.com>
2025-01-24 08:30:38 +0000

    c
    Signed-off-by: Feng Lin <linfeng67@huawei.com>

--- services/media_engine/plugins/source/file_fd_source_plugin.cpp
+++ services/media_engine/plugins/source/file_fd_source_plugin.cpp
@@ -34,6 +34,7 @@
 
 namespace {
 constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, LOG_DOMAIN_SYSTEM_PLAYER, "FileFdSourcePlugin" };
+template<typename T, typename = std::enable_if_t<std::is_same_v<int64_t, T> || std::is_same_v<int32_t, T>>>
 bool StrToLong(const std::string_view& str, T& value)
 {
     FALSE_RETURN_V_MSG_E(!str.empty() && (isdigit(str.front()) || (str.front() == '-')),
