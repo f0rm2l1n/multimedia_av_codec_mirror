967e150614dbb0c4ec94715fa0312ca32d6f7e81
万之凡 <wanzhifan1@huawei.com>
2025-02-10 14:30:21 +0800

    【修改说明】bugfix
    Signed-off-by: 万之凡 <wanzhifan1@huawei.com>
    Change-Id: I8bed959cb3f26212d4d76b60a65d7c84776bd38c

--- services/services/common/event_manager/event_handler/background_event_handler/background_event_handler.cpp
+++ services/services/common/event_manager/event_handler/background_event_handler/background_event_handler.cpp
@@ -19,46 +19,77 @@
 #include "avcodec_server.h"
 #include "avcodec_server_manager.h"
 #include "codec_service_stub.h"
-#ifdef USE_EFFICIENCY_MANAGER
-#include "syspara/parameters.h"
-#include "suspend_manager_client.h"
-#include "suspend_state_observer_stub.h"
-#endif //USE_EFFICIENCY_MANAGER
 
+#ifdef USE_EFFICIENCY_MANAGER
 namespace {
 constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_DOMAIN_FRAMEWORK, "BackGroundEventHandler"};
 } // namespace
+#endif //USE_EFFICIENCY_MANAGER
 namespace OHOS {
 namespace MediaAVCodec {
 #ifdef USE_EFFICIENCY_MANAGER
-class BackGroundEventHandler::SuspendStateObserverStubObj : public SuspendManager::SuspendStateObserverStub {
-public:
-    void OnActive(const std::vector<int32_t> &pidList, const int32_t uid) override
-    {
-        AVCODEC_LOGI("OnActive, pidList size:%{public}zu, uid:%{public}d", pidList.size(), uid);
-        BackGroundEventHandler::GetInstance().NotifyActive(pidList);
-        return;
+static std::vector<sptr<IRemoteObject>> GetFreezeInfoList(pid_t pid)
+{
+    std::vector<sptr<IRemoteObject>> instanceList;
+    std::vector<std::pair<sptr<IRemoteObject>, InstanceInfo>> instanceInfos =
+        AVCodecServerManager::GetInstance().GetInstanceInfoListByPid(pid);
+    for (auto &codecStub : instanceInfos) {
+        if ((codecStub.second.caller.pid == pid) && (codecStub.second.codecType == AVCODEC_TYPE_VIDEO_DECODER)) {
+            instanceList.push_back(codecStub.first);
+        }
     }
+    return instanceList;
+}
 
-    void OnDoze(const int32_t uid) override
-    {
-        AVCODEC_LOGI("OnDoze, uid:%{public}d", uid);
-        return;
+static void NotifyFrozen(const std::vector<int32_t> &pidList)
+{
+    for (auto pid : pidList) {
+        std::vector<sptr<IRemoteObject>> instanceList = GetFreezeInfoList(pid);
+        for (auto &instance : instanceList) {
+            CHECK_AND_CONTINUE_LOG(instance != nullptr, "instance is nullptr");
+            static_cast<CodecServiceStub *>(instance.GetRefPtr())->NotifyMemoryRecycle();
+        }
     }
+    return;
+}
 
-    void OnFrozen(const std::vector<int32_t> &pidList, const int32_t uid) override
-    {
-        AVCODEC_LOGI("OnFrozen, pidList size:%{public}zu, uid:%{public}d", pidList.size(), uid);
-        BackGroundEventHandler::GetInstance().NotifyFrozen(pidList);
-        return;
+static void NotifyActive(const std::vector<int32_t> &pidList)
+{
+    for (auto pid : pidList) {
+        std::vector<sptr<IRemoteObject>> instanceList = GetFreezeInfoList(pid);
+        for (auto &instance : instanceList) {
+            CHECK_AND_CONTINUE_LOG(instance != nullptr, "instance is nullptr");
+            static_cast<CodecServiceStub *>(instance.GetRefPtr())->NotifyMemoryWriteBack();
+        }
     }
+    return;
+}
 
-    void OnFrozenUid(const int32_t uid, const uint32_t reasonId) override
-    {
-        AVCODEC_LOGI("OnFrozenUid, uid:%{public}d, reasonId:%{public}u", uid, reasonId);
-        return;
-    }
-};
+void SuspendStateObserverStubObj::OnActive(const std::vector<int32_t> &pidList, const int32_t uid)
+{
+    AVCODEC_LOGI("OnActive, pidList size:%{public}zu, uid:%{public}d", pidList.size(), uid);
+    NotifyActive(pidList);
+    return;
+}
+
+void SuspendStateObserverStubObj::OnDoze(const int32_t uid)
+{
+    AVCODEC_LOGI("OnDoze, uid:%{public}d", uid);
+    return;
+}
+
+void SuspendStateObserverStubObj::OnFrozen(const std::vector<int32_t> &pidList, const int32_t uid)
+{
+    AVCODEC_LOGI("OnFrozen, pidList size:%{public}zu, uid:%{public}d", pidList.size(), uid);
+    NotifyFrozen(pidList);
+    return;
+}
+
+void SuspendStateObserverStubObj::OnFrozenUid(const int32_t uid, const uint32_t reasonId)
+{
+    AVCODEC_LOGI("OnFrozenUid, uid:%{public}d, reasonId:%{public}u", uid, reasonId);
+    return;
+}
 #endif //USE_EFFICIENCY_MANAGER
 
 BackGroundEventHandler::BackGroundEventHandler() {}
@@ -69,8 +100,10 @@ void BackGroundEventHandler::RegisterSuspendObserver()
     bool recycleMemory = OHOS::system::GetBoolParameter("resourceschedule.memmgr.dma.reclaimable", false);
     AVCODEC_LOGI("recycleMemory is %{public}d", recycleMemory);
     if (recycleMemory) {
-        suspendObservers_ = sptr<SuspendStateObserverStubObj>(new SuspendStateObserverStubObj());
-        CHECK_AND_RETURN_LOG(suspendObservers_ != nullptr, "Create Suspend Observer failed");
+        if (suspendObservers_ != nullptr){
+            suspendObservers_ = sptr<SuspendStateObserverStubObj>(new SuspendStateObserverStubObj());
+            CHECK_AND_RETURN_LOG(suspendObservers_ != nullptr, "Create Suspend Observer failed");
+        }
         int32_t ret = SuspendManager::SuspendManagerClient::GetInstance().RegisterSuspendObserver(suspendObservers_);
         CHECK_AND_RETURN_LOG(ret == ERR_OK, "RegisterSuspendObserver failed, return: %{public}d", ret);
         AVCODEC_LOGI("RegisterSuspendObserver succeed");
@@ -96,44 +129,6 @@ BackGroundEventHandler& BackGroundEventHandler::GetInstance()
 {
     static BackGroundEventHandler instance;
     return instance;
-}
-
-
-std::vector<sptr<IRemoteObject>> BackGroundEventHandler::GetFreezeInfoList(pid_t pid)
-{
-    std::vector<sptr<IRemoteObject>> instanceList;
-    std::vector<std::pair<sptr<IRemoteObject>, InstanceInfo>> instanceInfos =
-        AVCodecServerManager::GetInstance().GetInstanceInfoListByPid(pid);
-    for (auto &codecStub : instanceInfos) {
-        if ((codecStub.second.caller.pid == pid) && (codecStub.second.codecType == AVCODEC_TYPE_VIDEO_DECODER)) {
-            instanceList.push_back(codecStub.first);
-        }
-    }
-    return instanceList;
-}
-
-void BackGroundEventHandler::NotifyFrozen(const std::vector<int32_t> &pidList)
-{
-    for (auto pid : pidList) {
-        std::vector<sptr<IRemoteObject>> instanceList = GetFreezeInfoList(pid);
-        for (auto &instance : instanceList) {
-            CHECK_AND_CONTINUE_LOG(instance != nullptr, "instance is nullptr");
-            static_cast<CodecServiceStub *>(instance.GetRefPtr())->NotifyMemoryRecycle();
-        }
-    }
-    return;
-}
-
-void BackGroundEventHandler::NotifyActive(const std::vector<int32_t> &pidList)
-{
-    for (auto pid : pidList) {
-        std::vector<sptr<IRemoteObject>> instanceList = GetFreezeInfoList(pid);
-        for (auto &instance : instanceList) {
-            CHECK_AND_CONTINUE_LOG(instance != nullptr, "instance is nullptr");
-            static_cast<CodecServiceStub *>(instance.GetRefPtr())->NotifyMemoryWriteBack();
-        }
-    }
-    return;
 }
 } // namespace MediaAVCodec
 } // namespace OHOS
