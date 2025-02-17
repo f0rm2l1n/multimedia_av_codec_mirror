67612f2925c1d9467e05e0b151c810a77d9e72ed
彼此彼此 <chenyuchen8@huawei.com>
2025-02-17 09:46:48 +0800

    Add media info verification
    Signed-off-by: 彼此彼此 <chenyuchen8@huawei.com>
    Change-Id: I41d736396cb6216304be5d3aabe44830e3e66f05

--- services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.cpp
+++ services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.cpp
@@ -1082,6 +1082,7 @@ bool FFmpegDemuxerPlugin::HasCodecParameters()
     int32_t param;
     for (uint32_t i = 0; i < formatContext_->nb_streams; i++) {
         auto avStream = formatContext_->streams[i];
+        FALSE_RETURN_V_MSG_E(static_cast<size_t>(i) < mediaInfo_.tracks.size(), false, "mediaInfo is error");
         Meta &format = mediaInfo_.tracks[i];
         bool flag = !HaveValidParser(avStream->codecpar->codec_id) ||
             (HaveValidParser(avStream->codecpar->codec_id) && streamParser_);
