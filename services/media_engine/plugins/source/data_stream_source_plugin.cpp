70a4d7086e6505992875df85c7f1e4b9d77e0d95
huangtianyu <huangtianyu15@huawei.com>
2025-02-17 08:31:39 +0000

    update services/media_engine/plugins/source/data_stream_source_plugin.cpp.
    Signed-off-by: huangtianyu <huangtianyu15@huawei.com>

--- services/media_engine/plugins/source/data_stream_source_plugin.cpp
+++ services/media_engine/plugins/source/data_stream_source_plugin.cpp
@@ -136,8 +136,8 @@ void DataStreamSourcePlugin::ResetPool()
 void DataStreamSourcePlugin::WaitForRetry(uint32_t time)
 {
     std::unique_lock<std::mutex> lock(mutex_);
-    readCond_.wait_for(lock, std::chrono::milliseconds(time), [&] { 
-        return isInterrupted_.load() || isExitRead_.load(); 
+    readCond_.wait_for(lock, std::chrono::milliseconds(GetRetryTime()), [&] {
+        return isInterrupted_.load() || isExitRead_.load();
     });
 }
 
