a1cd6dbdf3e7b5a7ec3ef4e980e82505e85f7448
Joshua <wangjie407@huawei.com>
2025-02-18 15:48:32 +0800

    bugfix for flush timeout
    Signed-off-by: Joshua <wangjie407@huawei.com>

--- services/engine/codec/video/hcodec/hcodec_state.cpp
+++ services/engine/codec/video/hcodec/hcodec_state.cpp
@@ -163,11 +163,13 @@ void HCodec::UninitializedState::OnStateEntered()
     codec_->outRecord_.totalCnt = 0;
     codec_->outRecord_.totalCostUs = 0;
     codec_->inTimeMap_.clear();
+    codec_->lastInPts_ = -1;
+    codec_->lastOutPts_ = -1;
     codec_->inputWaitFenceCostUs_ = 0;
     codec_->outputWaitFenceCostUs_ = 0;
     codec_->inputDiscardCnt_ = 0;
     codec_->outputDiscardCnt_ = 0;
-    codec_->circulateWarnPrintedTimes_ = 0;
+    codec_->circulateHasStopped_ = false;
     codec_->OnEnterUninitializedState();
     codec_->ReleaseComponent();
 }
@@ -863,18 +865,25 @@ bool HCodec::FlushingState::IsFlushCompleteOnAllPorts()
 
 void HCodec::FlushingState::ChangeStateIfWeOwnAllBuffers()
 {
-    if (!IsFlushCompleteOnAllPorts() || !codec_->IsAllBufferOwnedByUsOrSurface()) {
+    if (!IsFlushCompleteOnAllPorts()) {
         return;
+    }
+    int32_t ret = AVCS_ERR_OK;
+    if (!codec_->IsAllBufferOwnedByUsOrSurface()) {
+        ret = AVCS_ERR_UNKNOWN;
+        codec_->SendAsyncMsg(MsgWhat::FORCE_SHUTDOWN, nullptr);
     }
     MsgInfo msg {MsgWhat::FLUSH, 0, nullptr};
     if (codec_->GetFirstSyncMsgToReply(msg)) {
-        ReplyErrorCode(msg.id, AVCS_ERR_OK);
+        ReplyErrorCode(msg.id, ret);
     }
-    codec_->inputPortEos_ = false;
-    codec_->outputPortEos_ = false;
-    codec_->gotFirstInput_ = false;
-    codec_->gotFirstOutput_ = false;
-    codec_->ChangeStateTo(codec_->runningState_);
+    if (ret == AVCS_ERR_OK) {
+        codec_->inputPortEos_ = false;
+        codec_->outputPortEos_ = false;
+        codec_->gotFirstInput_ = false;
+        codec_->gotFirstOutput_ = false;
+        codec_->ChangeStateTo(codec_->runningState_);
+    }
 }
 
 void HCodec::FlushingState::OnShutDown(const MsgInfo &info)
