9ea6b8a7dd9c69be0cff5ac45ac49f11963da0e1
y30025806 <yangjunhui10@huawei.com>
2025-02-18 20:00:10 +0800

    add note
    Signed-off-by: y30025806 <yangjunhui10@huawei.com>
    Change-Id: I5e405a7729cf54a9f7fb05ae4f2932c3184dcdc7

--- services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.cpp
+++ services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.cpp
@@ -226,6 +226,7 @@ int ConvertFlagsToFFmpeg(AVStream *avStream, int64_t ffTime, SeekMode mode, int6
         if (streamDuration == AV_NOPTS_VALUE || streamDuration <= 0) {
             streamDuration = GetStreamDuration(*avStream);
         }
+        // When the seekTime is within the last 1s, still use BACKWARD mode to ensure consistent performance.
         int64_t buffering = ConvertTimeToFFmpeg(1000 * MS_TO_NS, avStream->time_base);
         int64_t limit = streamDuration < buffering ? streamDuration : streamDuration - buffering;
         if (streamDuration > 0 && ffTime >= limit) {
