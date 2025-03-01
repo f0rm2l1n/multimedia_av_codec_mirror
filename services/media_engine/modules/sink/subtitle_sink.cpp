c7bd1238b4df69e45a168e239a93e2f05b7ea111
卢经伟 <lujingwei4@h-partners.com>
2025-03-01 16:13:54 +0800

    播放视频时字幕只显示部分问题修复
    Signed-off-by: 卢经伟 <lujingwei4@h-partners.com>

--- services/media_engine/modules/sink/subtitle_sink.cpp
+++ services/media_engine/modules/sink/subtitle_sink.cpp
@@ -174,14 +174,16 @@ Status SubtitleSink::Flush()
             subtitleInfoVec_.clear();
         }
     }
-    uint32_t queueSize = inputBufferQueueConsumer_->GetQueueSize();
-    std::shared_ptr<AVBuffer> filledOutputBuffer;
-    for (int i = 0; i<queueSize; i++) {
-        Status ret = inputBufferQueueConsumer_->AcquireBuffer(filledOutputBuffer);
-        if (ret != Status::OK || filledOutputBuffer == nullptr || filledOutputBuffer->memory_ == nullptr) {
-            break;
+    if (inputBufferQueueConsumer_ != nullptr) {
+        uint32_t queueSize = inputBufferQueueConsumer_->GetQueueSize();
+        std::shared_ptr<AVBuffer> filledOutputBuffer;
+        for (uint32_t i = 0; i < queueSize; i++) {
+            Status ret = inputBufferQueueConsumer_->AcquireBuffer(filledOutputBuffer);
+            if (ret != Status::OK || filledOutputBuffer == nullptr || filledOutputBuffer->memory_ == nullptr) {
+                break;
+            }
+            inputBufferQueueConsumer_->ReleaseBuffer(filledOutputBuffer);
         }
-        inputBufferQueueConsumer_->ReleaseBuffer(filledOutputBuffer);
     }
     isFlush_.store(true);
     updateCond_.notify_all();
