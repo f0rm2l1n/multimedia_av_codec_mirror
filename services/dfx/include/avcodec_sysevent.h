33d46aec3d545d21ec89a4b2a721bcf829f9a03c
y30025806 <yangjunhui10@huawei.com>
2025-03-03 21:03:12 +0800

    delete unuseful event
    Signed-off-by: y30025806 <yangjunhui10@huawei.com>
    Change-Id: Ib7b58139806ac253f4945de6658f68a7e2b15797

--- services/dfx/include/avcodec_sysevent.h
+++ services/dfx/include/avcodec_sysevent.h
@@ -97,7 +97,6 @@ __attribute__((visibility("default"))) void ServiceStartEventWrite(uint32_t useT
 __attribute__((visibility("default"))) void CodecStartEventWrite(CodecDfxInfo& codecDfxInfo);
 __attribute__((visibility("default"))) void CodecStopEventWrite(pid_t clientPid, uid_t clientUid,
                                                                 int32_t codecInstanceId);
-__attribute__((visibility("default"))) void DemuxerInitEventWrite(uint32_t downloadSize, std::string sourceType);
 __attribute__((visibility("default"))) void FaultDemuxerEventWrite(DemuxerFaultInfo& demuxerFaultInfo);
 __attribute__((visibility("default"))) void FaultAudioCodecEventWrite(AudioCodecFaultInfo& audioCodecFaultInfo);
 __attribute__((visibility("default"))) void FaultVideoCodecEventWrite(VideoCodecFaultInfo& videoCodecFaultInfo);
