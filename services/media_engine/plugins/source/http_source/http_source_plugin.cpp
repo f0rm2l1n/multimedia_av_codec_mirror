9f90c4afd6ea22e4240266f9ef76dce1460abb94
xuzhongwu <xuzhongwu@huawei.com>
2025-03-10 15:40:31 +0000

    update services/media_engine/plugins/source/http_source/http_source_plugin.cpp.
    Signed-off-by: xuzhongwu <xuzhongwu@huawei.com>

--- services/media_engine/plugins/source/http_source/http_source_plugin.cpp
+++ services/media_engine/plugins/source/http_source/http_source_plugin.cpp
@@ -531,7 +531,7 @@ void HttpSourcePlugin::RestartAndClearBuffer()
 
 bool HttpSourcePlugin::IsFlvLive()
 {
-    FALSE_RETURN_V_MSG_E(downloader_ != nullptr, "downloader_ is nullptr");
+    FALSE_RETURN_V_MSG_E(downloader_ != nullptr, false, "downloader_ is nullptr");
     return downloader_->IsFlvLive();
 }
 }
