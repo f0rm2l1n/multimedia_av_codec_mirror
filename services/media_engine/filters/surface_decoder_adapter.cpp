5c532173fa4d6fffbd2a08dd9a29cf34ff774f24
wanglingyi <wanglingyi4@huawei.com>
2025-03-22 15:13:21 +0800

    0322
    Signed-off-by: wanglingyi <wanglingyi4@huawei.com>

--- services/media_engine/filters/surface_decoder_adapter.cpp
+++ services/media_engine/filters/surface_decoder_adapter.cpp
@@ -21,6 +21,7 @@
 #include "meta/format.h"
 #include "media_description.h"
 #include "avcodec_trace.h"
+#include "codec_capability_adapter.h"
 
 namespace {
 constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_ONLY_PRERELEASE, LOG_DOMAIN_SYSTEM_PLAYER,
@@ -43,7 +44,10 @@ public:
     void OnError(MediaAVCodec::AVCodecErrorType errorType, int32_t errorCode) override
     {
         if (auto surfaceDecoderAdapter = surfaceDecoderAdapter_.lock()) {
-            surfaceDecoderAdapter->decoderAdapterCallback_->OnError(errorType, errorCode);
+            if (!transCoderErrorCbOnce_) {
+                transCoderErrorCbOnce_ = true;
+                surfaceDecoderAdapter->decoderAdapterCallback_->OnError(errorType, errorCode);
+            }
         } else {
             MEDIA_LOG_I("invalid surfaceEncoderAdapter");
         }
@@ -81,6 +85,7 @@ public:
 
 private:
     std::weak_ptr<SurfaceDecoderAdapter> surfaceDecoderAdapter_;
+    bool transCoderErrorCbOnce_ = false;
 };
 
 class AVBufferAvailableListener : public OHOS::Media::IConsumerListener {
@@ -126,6 +131,36 @@ Status SurfaceDecoderAdapter::Init(const std::string &mime)
     }
     if (!releaseBufferTask_) {
         releaseBufferTask_ = std::make_shared<Task>("SurfaceDecoder");
+        FALSE_RETURN_V(releaseBufferTask_ != nullptr, Status::ERROR_NULL_POINTER);
+        releaseBufferTask_->RegisterJob([this] {
+            ReleaseBuffer();
+            return 0;
+        });
+    }
+    return Status::OK;
+}
+
+Status SurfaceDecoderAdapter::Init(const std::string &mime, bool isHdr)
+{
+    FALSE_RETURN_V_NOLOG(isHdr, Init(mime));
+    MEDIA_LOG_I("Init mime: " PUBLIC_LOG_S " for hdr", mime.c_str());
+    std::shared_ptr<MediaAVCodec::AVCodecList> avCodecList = MediaAVCodec::AVCodecListFactory::CreateAVCodecList();
+    FALSE_RETURN_V_MSG(avCodecList != nullptr, Status::ERROR_UNKNOWN, "get codec list failed");
+    MediaAVCodec::CapabilityData *capabilityData = avCodecList->GetCapability(mime, false,
+        MediaAVCodec::AVCodecCategory::AVCODEC_HARDWARE);
+    FALSE_RETURN_V_MSG(capabilityData != nullptr, Status::ERROR_UNKNOWN, "get capability data failed");
+    FALSE_RETURN_V_MSG(capabilityData->isVendor, Status::ERROR_UNKNOWN, "not hw decoder");
+    FALSE_RETURN_V_MSG(
+        capabilityData->codecType == static_cast<int32_t>(MediaAVCodec::AVCodecType::AVCODEC_TYPE_VIDEO_DECODER),
+        Status::ERROR_UNKNOWN,
+        "not video decoder");
+    FALSE_RETURN_V_MSG(capabilityData->mimeType == mime, Status::ERROR_UNKNOWN, "not correct mime");
+    FALSE_RETURN_V_MSG(capabilityData->codecName != "", Status::ERROR_UNKNOWN, "empty codec name");
+    codecServer_ = MediaAVCodec::VideoDecoderFactory::CreateByName(capabilityData->codecName);
+    FALSE_RETURN_V_MSG(codecServer_ != nullptr, Status::ERROR_UNKNOWN, "get capability data failed");
+    if (!releaseBufferTask_) {
+        releaseBufferTask_ = std::make_shared<Task>("SurfaceDecoder");
+        FALSE_RETURN_V(releaseBufferTask_ != nullptr, Status::ERROR_NULL_POINTER);
         releaseBufferTask_->RegisterJob([this] {
             ReleaseBuffer();
             return 0;
