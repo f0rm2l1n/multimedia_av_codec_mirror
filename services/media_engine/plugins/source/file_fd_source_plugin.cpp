508471982de91ac350cbdf35dd82e5d22920e3bd
卢经伟 <lujingwei4@h-partners.com>
2025-03-26 10:27:53 +0800

    告警清零-混用符号数
    Signed-off-by: 卢经伟 <lujingwei4@h-partners.com>

--- services/media_engine/plugins/source/file_fd_source_plugin.cpp
+++ services/media_engine/plugins/source/file_fd_source_plugin.cpp
@@ -176,9 +176,11 @@ Status FileFdSourcePlugin::ReadOfflineFile(int32_t streamId, std::shared_ptr<Buf
     expectedLen = std::min(bufData->GetCapacity(), expectedLen);
     MEDIA_LOG_D("ReadLocal buffer pos: " PUBLIC_LOG_U64 " , len:" PUBLIC_LOG_ZU, position_.load(), expectedLen);
 
-    uint64_t offsetCur = lseek(fd_, 0, SEEK_CUR);
-    if (offsetCur != position_) {
-        MEDIA_LOG_E("Fd offsetCur has changed. offsetCur " PUBLIC_LOG_U64 ", offsetOld " PUBLIC_LOG_U64,
+    int64_t offsetCur = lseek(fd_, 0, SEEK_CUR);
+    if (offsetCur < 0) {
+        MEDIA_LOG_E("Fd get offset failed ");
+    } else if (static_cast<uint64_t>(offsetCur) != position_.load()) {
+        MEDIA_LOG_E("Fd offsetCur has changed. offsetCur " PUBLIC_LOG_D64 ", offsetOld " PUBLIC_LOG_U64,
             offsetCur, position_.load());
     }
     auto size = read(fd_, bufData->GetWritableAddr(expectedLen), expectedLen);
@@ -326,10 +328,10 @@ Status FileFdSourcePlugin::ParseUriInfo(const std::string& uri)
     if (fdUriMatch.size() == 4) { // 4：4 sub match
         offset_ = std::stoll(fdUriMatch[2].str()); // 2: sub match offset subscript
         if (static_cast<uint64_t>(offset_) > fileSize_) {
-            offset_ = fileSize_;
+            offset_ = static_cast<int64_t>(fileSize_);
         }
         size_ = static_cast<uint64_t>(std::stoll(fdUriMatch[3].str())); // 3: sub match size subscript
-        uint64_t remainingSize = fileSize_ - offset_;
+        uint64_t remainingSize = fileSize_ - static_cast<uint64_t>(offset_);
         if (size_ > remainingSize) {
             size_ = remainingSize;
         }
