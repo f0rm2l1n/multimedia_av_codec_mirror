77ff854656eeec3a0da694ce0cf138122b96000f
想要的都会有 <wangyincheng3@h-partners.com>
2025-04-15 11:39:37 +0800

    AI告警修复
    Signed-off-by: 想要的都会有 <wangyincheng3@h-partners.com>

--- services/media_engine/modules/source/audio_capture/audio_capture_module.cpp
+++ services/media_engine/modules/source/audio_capture/audio_capture_module.cpp
@@ -332,9 +332,7 @@ Status AudioCaptureModule::Read(std::shared_ptr<AVBuffer> &buffer, size_t expect
         if (audioCapturer_->GetStatus() != AudioStandard::CAPTURER_RUNNING) {
             return Status::ERROR_AGAIN;
         }
-        if (bufData->GetAddr() == nullptr) {
-            return Status::ERROR_INVALID_PARAMETER;
-        }
+        FALSE_RETURN_V_MSG_E(bufData->GetAddr() == nullptr, Status::ERROR_NOT_ENOUGH_DATA, "audioCapturer Read() fail")
         size = audioCapturer_->Read(*bufData->GetAddr(), expectedLen, true);
     }
     FALSE_RETURN_V_MSG_E(size >= 0, Status::ERROR_NOT_ENOUGH_DATA, "audioCapturer Read() fail");
