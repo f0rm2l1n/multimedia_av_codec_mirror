a0d723663b3f4408df9e854fb1b0018b552cf470
Corvo_s <shuzhengjie@h-partners.com>
2025-04-18 12:21:22 +0800

    Flush 卡住修复
    Signed-off-by: Corvo_s <shuzhengjie@h-partners.com>

--- services/engine/codec/video/hevcdecoder/hevc_decoder.cpp
+++ services/engine/codec/video/hevcdecoder/hevc_decoder.cpp
@@ -508,7 +508,7 @@ int32_t HevcDecoder::Flush()
     ResetBuffers();
     int32_t ret = 0;
     while (ret == 0 && hevcDecoderFlushFrameFunc_ != nullptr) {
-        ret = ret = hevcDecoderFlushFrameFunc_(hevcSDecoder_, &hevcDecoderOutpusArgs_);
+        ret = hevcDecoderFlushFrameFunc_(hevcSDecoder_, &hevcDecoderOutpusArgs_);
     }
     state_ = State::FLUSHED;
     AVCODEC_LOGI("Flush codec successful, state: Flushed");
