deba9b91ea44fbe617e0039c45cfe5e104f212d5
万之凡 <wanzhifan1@huawei.com>
2025-04-25 12:11:43 +0800

    0425删除重复日志
    Signed-off-by: 万之凡 <wanzhifan1@huawei.com>

--- services/services/common/event_manager/event_handler/background_event_handler/background_event_handler.cpp
+++ services/services/common/event_manager/event_handler/background_event_handler/background_event_handler.cpp
@@ -45,14 +45,14 @@ void BackGroundEventHandler::NotifyFrozen(const std::vector<int32_t> &pidList)
 {
     std::lock_guard<std::mutex> lock(mutex_);
     bool recycleMemory = OHOS::system::GetBoolParameter("resourceschedule.memmgr.dma.reclaimable", false);
+    if (!recycleMemory) {
+        return;
+    }
     for (auto pid : pidList) {
         std::vector<sptr<IRemoteObject>> instanceList = GetFreezeInfoList(pid);
-        if (recycleMemory && !instanceList.empty()) {
+        if (!instanceList.empty()) {
             frozenPidList_.insert(pid);
             AVCODEC_LOGI("Freeze pid: %{public}d, frozenPidList_ size: %{public}zu", pid, frozenPidList_.size());
-        } else if (!recycleMemory && !instanceList.empty()) {
-            AVCODEC_LOGI("recycle memory is not supported on this platform");
-            continue;
         }
         for (auto &instance : instanceList) {
             CHECK_AND_CONTINUE_LOG(instance != nullptr, "instance is nullptr");
