7cadc929b333df2a8d6ebdb1d85f652dbf8667dd
卢经伟 <lujingwei4@h-partners.com>
2025-05-15 09:49:23 +0800

    播放框架增加边播边处理
    Signed-off-by: 卢经伟 <lujingwei4@h-partners.com>

--- test/unittest/media_engine/filters/decoder_surface_filter_unit_test.cpp
+++ test/unittest/media_engine/filters/decoder_surface_filter_unit_test.cpp
@@ -411,43 +411,6 @@ HWTEST_F(DecoderSurfaceFilterUnitTest, DecoderSurfaceFilter_IsPostProcessorSuppo
 }
 #endif
 
-#ifdef USE_VIDEO_PROCESSING_ENGINE
-HWTEST_F(DecoderSurfaceFilterUnitTest, DecoderSurfaceFilter_IsPostProcessorSupported_CameraInsertFrame, TestSize.Level1)
-{
-    decoderSurfaceFilter_->postProcessorType_ = VideoPostProcessorType::CAMERA_INSERT_FRAME;
-
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_WIDTH, 1280);
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_HEIGHT, 1920);
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_IS_HDR_VIVID, false);
-    decoderSurfaceFilter_->meta_->SetData(Tag::AV_PLAYER_IS_DRM_PROTECTED, false);
-    EXPECT_EQ(decoderSurfaceFilter_->IsPostProcessorSupported(), false);
-
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_WIDTH, 3840);
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_HEIGHT, 2160);
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_IS_HDR_VIVID, false);
-    decoderSurfaceFilter_->meta_->SetData(Tag::AV_PLAYER_IS_DRM_PROTECTED, false);
-    EXPECT_EQ(decoderSurfaceFilter_->IsPostProcessorSupported(), true);
-
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_WIDTH, 0);
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_HEIGHT, 0);
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_IS_HDR_VIVID, false);
-    decoderSurfaceFilter_->meta_->SetData(Tag::AV_PLAYER_IS_DRM_PROTECTED, false);
-    EXPECT_EQ(decoderSurfaceFilter_->IsPostProcessorSupported(), false);
-
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_WIDTH, 3840);
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_HEIGHT, 2160);
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_IS_HDR_VIVID, true);
-    decoderSurfaceFilter_->meta_->SetData(Tag::AV_PLAYER_IS_DRM_PROTECTED, false);
-    EXPECT_EQ(decoderSurfaceFilter_->IsPostProcessorSupported(), false);
-
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_WIDTH, 3840);
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_HEIGHT, 2160);
-    decoderSurfaceFilter_->meta_->SetData(Tag::VIDEO_IS_HDR_VIVID, false);
-    decoderSurfaceFilter_->meta_->SetData(Tag::AV_PLAYER_IS_DRM_PROTECTED, true);
-    EXPECT_EQ(decoderSurfaceFilter_->IsPostProcessorSupported(), false);
-}
-#endif
-
 HWTEST_F(DecoderSurfaceFilterUnitTest, DecoderSurfaceFilter_SetPostProcessorOn, TestSize.Level1)
 {
     decoderSurfaceFilter_->postProcessorType_ = VideoPostProcessorType::SUPER_RESOLUTION;
@@ -514,26 +477,6 @@ HWTEST_F(DecoderSurfaceFilterUnitTest, DecoderSurfaceFilter_SetVideoSurface, Tes
     EXPECT_CALL(*postProcessor, SetOutputSurface(_)).WillRepeatedly(Return(Status::ERROR_UNKNOWN));
     ret = decoderSurfaceFilter_->SetVideoSurface(surface);
     EXPECT_EQ(ret, Status::ERROR_UNKNOWN);
-}
-
-HWTEST_F(DecoderSurfaceFilterUnitTest, DecoderSurfaceFilter_InitPostProcessorType, TestSize.Level1)
-{
-    decoderSurfaceFilter_->postProcessorType_ = VideoPostProcessorType::NONE;
-    decoderSurfaceFilter_->meta_->SetData(ENHANCE_FLAG, "1");
-    decoderSurfaceFilter_->meta_->SetData(VIDEO_ID, "1111");
-    decoderSurfaceFilter_->InitPostProcessorType();
-    EXPECT_EQ(decoderSurfaceFilter_->postProcessorType_, VideoPostProcessorType::CAMERA_INSERT_FRAME);
-
-    decoderSurfaceFilter_->postProcessorType_ = VideoPostProcessorType::NONE;
-    decoderSurfaceFilter_->meta_->SetData(ENHANCE_FLAG, "0");
-    decoderSurfaceFilter_->InitPostProcessorType();
-    EXPECT_EQ(decoderSurfaceFilter_->postProcessorType_, VideoPostProcessorType::NONE);
-
-    decoderSurfaceFilter_->postProcessorType_ = VideoPostProcessorType::SUPER_RESOLUTION;
-    decoderSurfaceFilter_->meta_->SetData(ENHANCE_FLAG, "1");
-    decoderSurfaceFilter_->meta_->SetData(VIDEO_ID, "1111");
-    decoderSurfaceFilter_->InitPostProcessorType();
-    EXPECT_EQ(decoderSurfaceFilter_->postProcessorType_, VideoPostProcessorType::SUPER_RESOLUTION);
 }
 
 HWTEST_F(DecoderSurfaceFilterUnitTest, DecoderSurfaceFilter_SetPostProcessorFd, TestSize.Level1)
