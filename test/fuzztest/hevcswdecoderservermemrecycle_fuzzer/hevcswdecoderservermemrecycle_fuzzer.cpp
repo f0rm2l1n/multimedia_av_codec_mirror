4d4372cc138c040a4c05720066df401b1b9cea8a
Corvo_s <shuzhengjie@h-partners.com>
2025-05-19 14:50:09 +0800

    内存回收 DT_FUZZ
    Signed-off-by: Corvo_s <shuzhengjie@h-partners.com>

--- test/fuzztest/hevcswdecoderservermemrecycle_fuzzer/hevcswdecoderservermemrecycle_fuzzer.cpp
+++ test/fuzztest/hevcswdecoderservermemrecycle_fuzzer/hevcswdecoderservermemrecycle_fuzzer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -30,6 +30,9 @@ const size_t PIXELFORMAT_SIZE = 5;
 namespace OHOS {
 bool SwdecoderServerNormalFuzzTest(const uint8_t *data, size_t size)
 {
+    if (data == nullptr) {
+        return false;
+    }
     if (size < EXPECT_SIZE) {
         return false;
     }
