12a63f67e976f807b91621bb1644344c2f9a46c6
Sxs <sxs233333@163.com>
2025-05-22 06:29:08 +0000

    update services/engine/codec/video/hcodec/hencoder.cpp.
    Signed-off-by: Sxs <sxs233333@163.com>

--- services/engine/codec/video/hcodec/hencoder.cpp
+++ services/engine/codec/video/hcodec/hencoder.cpp
@@ -1409,6 +1409,12 @@ void HEncoder::OnQueueInputBuffer(const MsgInfo &msg, BufferOperationMode mode)
     ChangeOwner(*bufferInfo, BufferOwner::OWNED_BY_US);
     WrapSurfaceBufferToSlot(*bufferInfo, bufferInfo->surfaceBuffer, bufferInfo->avBuffer->pts_,
         UserFlagToOmxFlag(static_cast<AVCodecBufferFlag>(bufferInfo->avBuffer->flag_)));
+
+    if (bufferInfo->avBuffer->memory_ != nullptr && bufferInfo->avBuffer->memory_->GetSize() == 0) {
+        bufferInfo->omxBuffer->bufferhandle = nullptr;
+        bufferInfo->omxBuffer->filledLen = 0;
+        bufferInfo->surfaceBuffer = nullptr;
+    }
     WrapPerFrameParamIntoOmxBuffer(bufferInfo->omxBuffer, bufferInfo->avBuffer->meta_);
     ReplyErrorCode(msg.id, AVCS_ERR_OK);
     int32_t err = HCodec::OnQueueInputBuffer(mode, bufferInfo);
