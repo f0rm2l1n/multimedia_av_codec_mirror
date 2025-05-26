45afab82d20991eb9c41c1dd6eb8b382a5eed60b
wanganzhou <wanganzhou1@h-partners.com>
2025-05-26 12:11:47 +0000

    mp4 support moov fronting
    Signed-off-by: wanganzhou <wanganzhou1@h-partners.com>

--- interfaces/kits/c/native_avcodec_base.h
+++ interfaces/kits/c/native_avcodec_base.h
@@ -798,6 +798,13 @@ extern const char *OH_MD_KEY_CREATION_TIME;
  * @since 20
  */
 extern const char *OH_MD_KEY_VIDEO_ENCODER_ROI_PARAMS;
+/**
+ * @brief Key for the decision of setting moov in front or not, value type is int32_t.
+ *
+ * @syscap SystemCapability.Multimedia.Media.CodecBase
+ * @since 20
+ */
+extern const char *OH_MD_KEY_ENABLE_MOOV_FRONT;
 
 extern const char *OH_MD_KEY_TRACK_REFERENCE_TYPE;
 extern const char *OH_MD_KEY_TRACK_DESCRIPTION;
