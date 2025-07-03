f17bffb9341ae4482cebcbf4410589bbc23545a4
zss <5583155+zss1209@user.noreply.gitee.com>
2025-07-03 16:47:48 +0800

    视频mv播放开关能力
    Signed-off-by: zss <zhangshuangshuang14@h-partners.com>

--- services/media_engine/filters/decoder_surface_filter.cpp
+++ services/media_engine/filters/decoder_surface_filter.cpp
@@ -1609,6 +1609,7 @@ Status DecoderSurfaceFilter::DoReInitAndStart()
     }
     videoDecoder_->Flush();
     ret = DoStart();
+    FALSE_RETURN_MSG(eventReceiver_ != nullptr, "DoReInitAndStart without eventReceiver_");
     eventReceiver_->OnEvent({"DecoderSurfaceFilter", EventType::EVENT_VIDEO_DECODER_RESTART, Status::OK});
     FALSE_RETURN_V_MSG(ret == Status::OK, ret, "DoStart fail");
     return ret;
