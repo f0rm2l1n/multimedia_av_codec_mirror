eace9901a175611072240c9256d776ae11fb59d1
mediaTest <wangyang453@huawei.com>
2025-08-08 09:39:10 +0800

    补充fuzz测试用例
    Signed-off-by: mediaTest <wangyang453@huawei.com>

---
+++ test/fuzztest/hevchwencoderapi11_fuzzer/videoenc_api11_sample.cpp
@@ -0,0 +1,428 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <utility>
+#include "iconsumer_surface.h"
+#include "native_buffer_inner.h"
+#include "videoenc_api11_sample.h"
+using namespace OHOS;
+using namespace OHOS::Media;
+using namespace std;
+namespace {
+constexpr int64_t NANOS_IN_SECOND = 1000000000L;
+constexpr int64_t NANOS_IN_MICRO = 1000L;
+constexpr uint32_t FRAME_INTERVAL = 16666;
+constexpr uint32_t DEFAULT_BITRATE = 10000000;
+constexpr uint32_t DOUBLE = 2;
+constexpr uint32_t THREE = 3;
+sptr<Surface> cs = nullptr;
+sptr<Surface> ps = nullptr;
+VEncAPI11FuzzSample *g_vEncSample = nullptr;
+constexpr int32_t TIMESTAMP_BASE = 1000000;
+constexpr int32_t DURATION_BASE = 46000;
+} // namespace
+
+VEncAPI11FuzzSample::~VEncAPI11FuzzSample()
+{
+    if (surfInput && nativeWindow) {
+        OH_NativeWindow_DestroyNativeWindow(nativeWindow);
+        nativeWindow = nullptr;
+    }
+    Release();
+}
+
+static void VencError(OH_AVCodec *codec, int32_t errorCode, void *userData)
+{
+    cout << "Error errorCode=" << errorCode << endl;
+    g_vEncSample->isRunning_.store(false);
+    g_vEncSample->signal_->inCond_.notify_all();
+}
+
+static void VencFormatChanged(OH_AVCodec *codec, OH_AVFormat *format, void *userData)
+{
+    cout << "Format Changed" << endl;
+}
+
+static void onEncInputBufferAvailable(OH_AVCodec *codec, uint32_t index, OH_AVBuffer *buffer, void *userData)
+{
+    VEncSignal *signal = static_cast<VEncSignal *>(userData);
+    {
+        unique_lock<mutex> lock(signal->inMutex_);
+        if (g_vEncSample == nullptr || g_vEncSample->roiInfo == nullptr) {
+            cout << "enc_sample or roi is null" << endl;
+            return;
+        }
+        OH_AVFormat *parameter = OH_AVFormat_Create();
+        if (parameter == nullptr) {
+            cout << "Failed to create OH_AVFormat!" << endl;
+        }
+        OH_AVFormat_SetStringValue(parameter, OH_MD_KEY_VIDEO_ENCODER_ROI_PARAMS, g_vEncSample->roiInfo);
+        OH_AVBuffer_SetParameter(buffer, parameter);
+        OH_AVFormat_Destroy(parameter);
+        signal->inIdxQueue_.push(index);
+        signal->inBufferQueue_.push(buffer);
+    }
+    signal->inCond_.notify_all();
+}
+
+static void onEncOutputBufferAvailable(OH_AVCodec *codec, uint32_t index, OH_AVBuffer *buffer, void *userData)
+{
+    OH_VideoEncoder_FreeOutputBuffer(codec, index);
+}
+
+static void onEncInputParam(OH_AVCodec *codec, uint32_t index, OH_AVFormat *parameter, void *userData)
+{
+    OH_AVFormat_SetIntValue(parameter, OH_MD_KEY_BITRATE, DEFAULT_BITRATE);
+    OH_VideoEncoder_PushInputParameter(codec, index);
+    return;
+}
+
+int64_t VEncAPI11FuzzSample::GetSystemTimeUs()
+{
+    struct timespec now;
+    (void)clock_gettime(CLOCK_BOOTTIME, &now);
+    int64_t nanoTime = reinterpret_cast<int64_t>(now.tv_sec) * NANOS_IN_SECOND + now.tv_nsec;
+
+    return nanoTime / NANOS_IN_MICRO;
+}
+
+int32_t VEncAPI11FuzzSample::ConfigureVideoEncoder()
+{
+    OH_AVFormat *format = OH_AVFormat_Create();
+    if (format == nullptr) {
+        return AV_ERR_UNKNOWN;
+    }
+    (void)OH_AVFormat_SetIntValue(format, OH_MD_KEY_WIDTH, defaultWidth);
+    (void)OH_AVFormat_SetIntValue(format, OH_MD_KEY_HEIGHT, defaultHeight);
+    (void)OH_AVFormat_SetIntValue(format, OH_MD_KEY_PIXEL_FORMAT, defaultPixFmt);
+    (void)OH_AVFormat_SetDoubleValue(format, OH_MD_KEY_FRAME_RATE, defaultFrameRate);
+    (void)OH_AVFormat_SetIntValue(format, OH_MD_KEY_I_FRAME_INTERVAL, defaultKeyFrameInterval);
+    if (defaultBitRate == CQ) {
+        (void)OH_AVFormat_SetIntValue(format, OH_MD_KEY_QUALITY, defaultQuality);
+    }
+    (void)OH_AVFormat_SetIntValue(format, OH_MD_KEY_VIDEO_ENCODE_BITRATE_MODE, defaultBitrateMode);
+    if (factorEnable) {
+        (void)OH_AVFormat_SetIntValue(format, OH_MD_KEY_SQR_FACTOR, defaultSqrFactor);
+    }
+    if (maxbiteEnable) {
+        (void)OH_AVFormat_SetLongValue(format, OH_MD_KEY_MAX_BITRATE, defaultMaxBitrate);
+    }
+    if (bEnable) {
+        (void)OH_AVFormat_SetIntValue(format, OH_MD_KEY_VIDEO_ENCODER_ENABLE_B_FRAME, defaultBframe);
+    }
+    if (enablePTSBasedRateControl) {
+        (void)OH_AVFormat_SetIntValue(format, OH_MD_KEY_VIDEO_ENCODER_ENABLE_PTS_BASED_RATECONTROL, 1);
+    }
+    int ret = OH_VideoEncoder_Configure(venc_, format);
+    OH_AVFormat_Destroy(format);
+    return ret;
+}
+
+int32_t VEncAPI11FuzzSample::SetParameter()
+{
+    if (venc_) {
+        OH_AVFormat *format = OH_AVFormat_Create();
+        if (format == nullptr) {
+            return AV_ERR_UNKNOWN;
+        }
+        int32_t sqrfac = 51;
+        int64_t biterate = 10000000;
+        (void)OH_AVFormat_SetIntValue(format, OH_MD_KEY_SQR_FACTOR, sqrfac);
+        (void)OH_AVFormat_SetLongValue(format, OH_MD_KEY_MAX_BITRATE, biterate);
+        int ret = OH_VideoEncoder_SetParameter(venc_, format);
+        int32_t value = 0;
+        int64_t value2 = 0;
+        OH_AVFormat_GetIntValue(format, OH_MD_KEY_SQR_FACTOR, &value);
+        OH_AVFormat_GetLongValue(format, OH_MD_KEY_MAX_BITRATE, &value2);
+        cout << "value: " << value << endl;
+        cout << "value: " << value2 << endl;
+        OH_AVFormat_Destroy(format);
+        return ret;
+    }
+    return AV_ERR_UNKNOWN;
+}
+
+int32_t VEncAPI11FuzzSample::SetVideoEncoderCallback()
+{
+    signal_ = new VEncSignal();
+    if (signal_ == nullptr) {
+        cout << "Failed to new VEncSignal" << endl;
+        return AV_ERR_UNKNOWN;
+    }
+    if (surfInput) {
+        int32_t ret = OH_VideoEncoder_RegisterParameterCallback(venc_, onEncInputParam, static_cast<void *>(this));
+        if (ret != AV_ERR_OK) {
+            return ret;
+        }
+    }
+    cb_.onError = VencError;
+    cb_.onStreamChanged = VencFormatChanged;
+    cb_.onNeedInputBuffer = onEncInputBufferAvailable;
+    cb_.onNewOutputBuffer = onEncOutputBufferAvailable;
+    return OH_VideoEncoder_RegisterCallback(venc_, cb_, static_cast<void *>(signal_));
+}
+
+int32_t VEncAPI11FuzzSample::CreateSurface()
+{
+    int32_t ret = 0;
+    ret = OH_VideoEncoder_GetSurface(venc_, &nativeWindow);
+    if (ret != AV_ERR_OK) {
+        cout << "OH_VideoEncoder_GetSurface fail" << endl;
+        return ret;
+    }
+    ret = OH_NativeWindow_NativeWindowHandleOpt(nativeWindow, SET_FORMAT, GRAPHIC_PIXEL_FMT_YCBCR_420_SP);
+    if (ret != AV_ERR_OK) {
+        cout << "NativeWindowHandleOpt SET_FORMAT fail" << endl;
+        return ret;
+    }
+    ret = OH_NativeWindow_NativeWindowHandleOpt(nativeWindow, SET_BUFFER_GEOMETRY, defaultWidth, defaultHeight);
+    if (ret != AV_ERR_OK) {
+        cout << "NativeWindowHandleOpt SET_BUFFER_GEOMETRY fail" << endl;
+        return ret;
+    }
+    return AV_ERR_OK;
+}
+
+int32_t VEncAPI11FuzzSample::OpenFile()
+{
+    if (fuzzMode) {
+        return AV_ERR_OK;
+    }
+    int32_t ret = AV_ERR_OK;
+    inFile_ = make_unique<ifstream>();
+    if (inFile_ == nullptr) {
+        isRunning_.store(false);
+        (void)OH_VideoEncoder_Stop(venc_);
+        return AV_ERR_UNKNOWN;
+    }
+    inFile_->open(inpDir, ios::in | ios::binary);
+    if (!inFile_->is_open()) {
+        cout << "file open fail" << endl;
+        isRunning_.store(false);
+        (void)OH_VideoEncoder_Stop(venc_);
+        inFile_->close();
+        inFile_.reset();
+        inFile_ = nullptr;
+        return AV_ERR_UNKNOWN;
+    }
+    return ret;
+}
+
+void VEncAPI11FuzzSample::GetStride()
+{
+    OH_AVFormat *format = OH_VideoEncoder_GetInputDescription(venc_);
+    int32_t inputStride = 0;
+    OH_AVFormat_GetIntValue(format, "stride", &inputStride);
+    stride_ = inputStride;
+    OH_AVFormat_Destroy(format);
+}
+
+int32_t VEncAPI11FuzzSample::StartVideoEncoder()
+{
+    isRunning_.store(true);
+    int32_t ret = 0;
+    if (surfInput) {
+        ret = CreateSurface();
+        if (ret != AV_ERR_OK) {
+            return ret;
+        }
+    }
+    if (OpenFile() != AV_ERR_OK) {
+        return AV_ERR_UNKNOWN;
+    }
+    ret = OH_VideoEncoder_Start(venc_);
+    GetStride();
+    if (ret != AV_ERR_OK) {
+        isRunning_.store(false);
+        signal_->inCond_.notify_all();
+        return ret;
+    }
+    if (surfInput) {
+        inputLoop_ = make_unique<thread>(&VEncAPI11FuzzSample::InputFuncSurface, this);
+    } else {
+        inputLoop_ = make_unique<thread>(&VEncAPI11FuzzSample::InputFunc, this);
+    }
+    if (inputLoop_ == nullptr) {
+        isRunning_.store(false);
+        (void)OH_VideoEncoder_Stop(venc_);
+        return AV_ERR_UNKNOWN;
+    }
+    return AV_ERR_OK;
+}
+
+int32_t VEncAPI11FuzzSample::CreateVideoEncoder(const char *codecName)
+{
+    venc_ = OH_VideoEncoder_CreateByMime(codecName);
+    g_vEncSample = this;
+    return venc_ == nullptr ? AV_ERR_UNKNOWN : AV_ERR_OK;
+}
+
+void VEncAPI11FuzzSample::WaitForEOS()
+{
+    if (inputLoop_) {
+        inputLoop_->join();
+    }
+    inputLoop_ = nullptr;
+}
+
+uint32_t VEncAPI11FuzzSample::FlushSurf(OHNativeWindowBuffer *ohNativeWindowBuffer, OH_NativeBuffer *nativeBuffer)
+{
+    struct Region region;
+    struct Region::Rect *rect = new Region::Rect();
+    rect->x = 0;
+    rect->y = 0;
+    rect->w = defaultWidth;
+    rect->h = defaultHeight;
+    region.rects = rect;
+    NativeWindowHandleOpt(nativeWindow, SET_UI_TIMESTAMP, GetSystemTimeUs());
+    int32_t err = OH_NativeBuffer_Unmap(nativeBuffer);
+    if (err != 0) {
+        return 1;
+    }
+    err = OH_NativeWindow_NativeWindowFlushBuffer(nativeWindow, ohNativeWindowBuffer, -1, region);
+    delete rect;
+    if (err != 0) {
+        return 1;
+    }
+    return 0;
+}
+
+void VEncAPI11FuzzSample::InputFuncSurface()
+{
+    while (isRunning_.load()) {
+        OHNativeWindowBuffer *ohNativeWindowBuffer;
+        int fenceFd = -1;
+        if (nativeWindow == nullptr) {
+            cout << "nativeWindow == nullptr" << endl;
+            isRunning_.store(false);
+            break;
+        }
+        int32_t err = OH_NativeWindow_NativeWindowRequestBuffer(nativeWindow, &ohNativeWindowBuffer, &fenceFd);
+        if (err != 0) {
+            cout << "RequestBuffer failed, GSError=" << err << endl;
+            isRunning_.store(false);
+            break;
+        }
+        if (fenceFd > 0) {
+            close(fenceFd);
+        }
+        OH_NativeBuffer *nativeBuffer = OH_NativeBufferFromNativeWindowBuffer(ohNativeWindowBuffer);
+        void *virAddr = nullptr;
+        OH_NativeBuffer_Config config;
+        OH_NativeBuffer_GetConfig (nativeBuffer, &config);
+        err = OH_NativeBuffer_Map(nativeBuffer, &virAddr);
+        if (err != 0) {
+            cout << "OH_NativeBuffer_Map failed, GSError=" << err << endl;
+            isRunning_.store(false);
+            break;
+        }
+        uint8_t *dst = (uint8_t *)virAddr;
+        if (dst == nullptr) {
+            break;
+        }
+        if (memcpy_s(dst, (config.stride * config.height * THREE) / DOUBLE, fuzzData, fuzzSize) != EOK) {
+            break;
+        }
+        if (frameCount == maxFrameInput) {
+            err = OH_VideoEncoder_NotifyEndOfStream(venc_);
+            if (err != 0) {
+                cout << "OH_VideoEncoder_NotifyEndOfStream failed" << endl;
+                isRunning_.store(false);
+            }
+            break;
+        }
+        if (FlushSurf(ohNativeWindowBuffer, nativeBuffer)) {
+            break;
+        }
+        usleep(FRAME_INTERVAL);
+        frameCount++;
+    }
+}
+
+void VEncAPI11FuzzSample::SetEOS(uint32_t index, OH_AVBuffer *buffer)
+{
+    OH_AVCodecBufferAttr attr;
+    attr.pts = 0;
+    attr.size = 0;
+    attr.offset = 0;
+    attr.flags = AVCODEC_BUFFER_FLAGS_EOS;
+    OH_AVBuffer_SetBufferAttr(buffer, &attr);
+    int32_t res = OH_VideoEncoder_PushInputBuffer(venc_, index);
+    cout << "OH_VideoEncoder_PushInputBuffer    EOS   res: " << res << endl;
+    unique_lock<mutex> lock(signal_->inMutex_);
+}
+
+void VEncAPI11FuzzSample::InputFunc()
+{
+    errCount = 0;
+    while (isRunning_.load()) {
+        unique_lock<mutex> lock(signal_->inMutex_);
+        signal_->inCond_.wait(lock, [this]() {
+            if (!isRunning_.load()) {
+                return true;
+            }
+            return signal_->inIdxQueue_.size() > 0;
+        });
+        if (!isRunning_.load()) {
+            break;
+        }
+        uint32_t index = signal_->inIdxQueue_.front();
+        auto buffer = signal_->inBufferQueue_.front();
+        signal_->inIdxQueue_.pop();
+        signal_->inBufferQueue_.pop();
+        lock.unlock();
+        OH_AVCodecBufferAttr attr;
+        int32_t bufferSize = OH_AVBuffer_GetCapacity(buffer);
+        uint8_t *fileBuffer = OH_AVBuffer_GetAddr(buffer);
+        if (fileBuffer == nullptr) {
+            break;
+        }
+        if (memcpy_s(fileBuffer, bufferSize, fuzzData, fuzzSize) != EOK) {
+            cout << "Fatal: memcpy fail" << endl;
+            break;
+        }
+        attr.size = fuzzSize;
+        if (frameCount == maxFrameInput) {
+            SetEOS(index, buffer);
+            break;
+        }
+        if (enablePTSBasedRateControl) {
+            attr.pts = TIMESTAMP_BASE + DURATION_BASE * frameIndex_;
+        } else {
+            attr.pts = GetSystemTimeUs();
+        }
+        frameIndex_++;
+        attr.offset = 0;
+        attr.flags = AVCODEC_BUFFER_FLAGS_NONE;
+        OH_AVBuffer_SetBufferAttr(buffer, &attr);
+        OH_VideoEncoder_PushInputBuffer(venc_, index);
+        frameCount++;
+        if (sleepOnFPS) {
+            usleep(FRAME_INTERVAL);
+        }
+    }
+}
+
+int32_t VEncAPI11FuzzSample::Release()
+{
+    int ret = OH_VideoEncoder_Destroy(venc_);
+    venc_ = nullptr;
+    if (signal_ != nullptr) {
+        delete signal_;
+        signal_ = nullptr;
+    }
+    return ret;
+}
---
+++ test/fuzztest/hevchwencodersetparameter_fuzzer/hevchwencodersetparameter_fuzzer.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <cstddef>
+#include <cstdint>
+#include <fuzzer/FuzzedDataProvider.h>
+#include <fstream>
+
+#include "native_avcodec_videoencoder.h"
+#include "native_averrors.h"
+#include "native_avcodec_base.h"
+#include "native_avcapability.h"
+#include "videoenc_api11_sample.h"
+
+using namespace std;
+using namespace OHOS;
+using namespace OHOS::Media;
+#define FUZZ_PROJECT_NAME "hwencodersetparam_fuzzer"
+
+VEncAPI11FuzzSample *g_vEncSample = nullptr;
+
+void SaveCorpus(const uint8_t *data, size_t size, const std::string& filename)
+{
+    std::ofstream file(filename, std::ios::out | std::ios::binary);
+    if (file.is_open()) {
+        file.write(reinterpret_cast<const char*>(data), size);
+        file.close();
+    }
+}
+
+bool ReleaseSample()
+{
+    delete g_vEncSample;
+    g_vEncSample = nullptr;
+    return true;
+}
+
+namespace OHOS {
+bool HwEncoderSetParamFuzzTest(const uint8_t *data, size_t size)
+{
+    if (size < sizeof(int32_t)) {
+        return false;
+    }
+    std::string filename = "/data/test/corpus-EncoderAPI11FuzzTest";
+    SaveCorpus(data, size, filename);
+    FuzzedDataProvider fdp(data, size);
+    bool data2 = fdp.ConsumeBool();
+    int64_t maxBite = 100000000;
+    int32_t sqrFactor = 32;
+    g_vEncSample = new VEncAPI11FuzzSample();
+    g_vEncSample->fuzzData = data;
+    g_vEncSample->fuzzSize = size;
+    g_vEncSample->surfInput = data2;
+    g_vEncSample->fuzzMode = true;
+    g_vEncSample->defaultBitrateMode = SQR;
+    g_vEncSample->factorEnable = true;
+    g_vEncSample->maxbiteEnable = true;
+    g_vEncSample->defaultSqrFactor = sqrFactor;
+    g_vEncSample->defaultMaxBitrate = maxBite;
+    OH_AVCapability *cap = OH_AVCodec_GetCapabilityByCategory("video/hevc", true, HARDWARE);
+    string tmpCodecName = OH_AVCapability_GetName(cap);
+    if (g_vEncSample->CreateVideoEncoder(tmpCodecName.c_str()) != AV_ERR_OK) {
+        return ReleaseSample();
+    }
+    if (g_vEncSample->SetVideoEncoderCallback() != AV_ERR_OK) {
+        return ReleaseSample();
+    }
+    if (g_vEncSample->ConfigureVideoEncoder() != AV_ERR_OK) {
+        return ReleaseSample();
+    }
+    if (g_vEncSample->StartVideoEncoder() != AV_ERR_OK) {
+        return ReleaseSample();
+    }
+    g_vEncSample->SetParameter();
+    g_vEncSample->WaitForEOS();
+    return ReleaseSample();
+}
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::HwEncoderSetParamFuzzTest(data, size);
+    return 0;
+}
