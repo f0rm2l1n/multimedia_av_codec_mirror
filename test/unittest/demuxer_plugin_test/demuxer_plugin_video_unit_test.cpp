00bb756c1ced022ac5fb4ee5951af2157a143095
彼此彼此 <chenyuchen8@huawei.com>
2025-08-11 20:34:57 +0800

    Demuxer provides an aync read thread priority setting interface
    Signed-off-by: 彼此彼此 <chenyuchen8@huawei.com>

--- test/unittest/demuxer_plugin_test/demuxer_plugin_video_unit_test.cpp
+++ test/unittest/demuxer_plugin_test/demuxer_plugin_video_unit_test.cpp
@@ -40,6 +40,7 @@ using namespace OHOS::Media::Plugins;
 list<SeekMode> seekModes = {SeekMode::SEEK_NEXT_SYNC, SeekMode::SEEK_PREVIOUS_SYNC,
     SeekMode::SEEK_CLOSEST_SYNC};
 const int32_t DEFAULT_TIMEOUT = 100; // 100ms
+constexpr uint32_t THREAD_PRIORITY_41 = 7;
 unique_ptr<FileServerDemo> server = nullptr;
 static const string TEST_URI_PATH = "http://127.0.0.1:46666/";
 static const string TEST_RELATIVE_PATH = "/data/test/media/";
@@ -464,6 +465,51 @@ HWTEST_F(DemuxerPluginUnitTest, Demuxer_ReadSample_0001, TestSize.Level1)
     ASSERT_EQ(demuxerPlugin_->SelectTrack(1), Status::OK);
     OHOS::Media::AVBufferWrapper buffer(DEFAULT_BUFFSIZE);
     ASSERT_EQ(demuxerPlugin_->ReadSample(0, buffer.mediaAVBuffer, 100), Status::OK);
+}
+
+/**
+ * @tc.name: Demuxer_SetAsyncReadThreadPriority_0001
+ * @tc.desc: Test SetAsyncReadThreadPriority
+ * @tc.type: FUNC
+ */
+HWTEST_F(DemuxerPluginUnitTest, Demuxer_SetAsyncReadThreadPriority_0001, TestSize.Level1)
+{
+    std::string pluginName = "avdemux_flv";
+    InitResource(g_flvPath, pluginName);
+    ASSERT_TRUE(initStatus_);
+    ASSERT_NE(demuxerPlugin_, nullptr); // 检查插件是否初始化成功
+    ASSERT_EQ(demuxerPlugin_->SelectTrack(0), Status::OK);
+    ASSERT_EQ(demuxerPlugin_->SelectTrack(1), Status::OK);
+    OHOS::Media::AVBufferWrapper buffer(DEFAULT_BUFFSIZE);
+    ASSERT_EQ(demuxerPlugin_0>SetAsyncReadThreadPriority(THREAD_PRIORITY_41, "demuxer_plugin_unit_test"), Status::OK);
+    ASSERT_EQ(demuxerPlugin_0>SetAsyncReadThreadPriority(THREAD_PRIORITY_41, "demuxer_plugin_unit_test"),
+                                                         Status::ERROR_WRONG_STATE);
+    ASSERT_EQ(demuxerPlugin_->ReadSample(0, buffer.mediaAVBuffer, 100), Status::OK);
+    ASSERT_EQ(demuxerPlugin_->ReadSample(0, buffer.mediaAVBuffer, 100), Status::OK);
+    ASSERT_EQ(demuxerPlugin_->ReadSample(0, buffer.mediaAVBuffer, 100), Status::OK);
+    ASSERT_EQ(demuxerPlugin_0>SetAsyncReadThreadPriority(THREAD_PRIORITY_41, "demuxer_plugin_unit_test"),
+                                                         Status::ERROR_WRONG_STATE);
+}
+
+/**
+ * @tc.name: Demuxer_SetAsyncReadThreadPriority_0002
+ * @tc.desc: Test SetAsyncReadThreadPriority
+ * @tc.type: FUNC
+ */
+HWTEST_F(DemuxerPluginUnitTest, Demuxer_SetAsyncReadThreadPriority_0002, TestSize.Level1)
+{
+    std::string pluginName = "avdemux_flv";
+    InitResource(g_flvPath, pluginName);
+    ASSERT_TRUE(initStatus_);
+    ASSERT_NE(demuxerPlugin_, nullptr); // 检查插件是否初始化成功
+    ASSERT_EQ(demuxerPlugin_->SelectTrack(0), Status::OK);
+    ASSERT_EQ(demuxerPlugin_->SelectTrack(1), Status::OK);
+    OHOS::Media::AVBufferWrapper buffer(DEFAULT_BUFFSIZE);
+    ASSERT_EQ(demuxerPlugin_->ReadSample(0, buffer.mediaAVBuffer, 100), Status::OK);
+    ASSERT_EQ(demuxerPlugin_->ReadSample(0, buffer.mediaAVBuffer, 100), Status::OK);
+    ASSERT_EQ(demuxerPlugin_->ReadSample(0, buffer.mediaAVBuffer, 100), Status::OK);
+    ASSERT_EQ(demuxerPlugin_0>SetAsyncReadThreadPriority(THREAD_PRIORITY_41, "demuxer_plugin_unit_test"),
+                                                         Status::ERROR_WRONG_STATE);
 }
 
 /**
