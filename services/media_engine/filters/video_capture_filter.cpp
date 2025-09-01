67ae0d4986864bd7e5628795d76fdf07adffd0b2
chenning <chenning39@huawei.com>
2025-09-01 13:02:19 +0000

    update services/media_engine/filters/video_capture_filter.cpp.
    Signed-off-by: chenning <chenning39@huawei.com>

--- services/media_engine/filters/video_capture_filter.cpp
+++ services/media_engine/filters/video_capture_filter.cpp
@@ -103,7 +103,9 @@ VideoCaptureFilter::VideoCaptureFilter(std::string name, FilterType type): Filte
 VideoCaptureFilter::~VideoCaptureFilter()
 {
     MEDIA_LOG_I("video capture filter destroy");
-    inputSurface_->UnregisterConsumerListener();
+    if (inputSurface_ != nullptr) {
+        inputSurface_->UnregisterConsumerListener();
+    }
 }
 
 Status VideoCaptureFilter::SetCodecFormat(const std::shared_ptr<Meta> &format)
