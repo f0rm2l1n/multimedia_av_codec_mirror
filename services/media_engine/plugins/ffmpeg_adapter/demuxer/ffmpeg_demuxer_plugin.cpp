c9744c05bb9481b9b2a0f51e1f3919294fd3c8ff
彼此彼此 <chenyuchen8@huawei.com>
2025-09-05 10:23:12 +0800

    demuxer will stop calling readat after the first EOS is received
    Signed-off-by: 彼此彼此 <chenyuchen8@huawei.com>

--- services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.cpp
+++ services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.cpp
@@ -520,6 +520,7 @@ void FFmpegDemuxerPlugin::ResetParam()
     ioContext_.retry = false;
     ioContext_.eos = false;
     ioContext_.initDownloadDataSize = 0;
+    ioContext_.isGetFirstEos.store(false);
     mediaInfo_ = MediaInfo();
     for (size_t i = 0; i < selectedTrackIds_.size(); ++i) {
         cacheQueue_.RemoveTrackQueue(selectedTrackIds_[i]);
@@ -1928,6 +1929,7 @@ bool FFmpegDemuxerPlugin::IsUseFirstFrameDts(int trackIndex, int64_t seekTime)
 Status FFmpegDemuxerPlugin::DoSeekInternal(int trackIndex, int64_t seekTime, int64_t ffTime,
     SeekMode mode, int64_t& realSeekTime)
 {
+    ioContext_.isGetFirstEos.store(true);
     auto avStream = formatContext_->streams[trackIndex];
     FALSE_RETURN_V_MSG_E(avStream != nullptr, Status::ERROR_NULL_POINTER, "AVStream is nullptr");
     realSeekTime = ConvertTimeFromFFmpeg(ffTime, avStream->time_base);
