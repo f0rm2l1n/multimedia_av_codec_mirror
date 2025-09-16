247b74963e9e97086d82a6482a2830e118ca4192
孙志威 <sunzhiwei13@h-partners.com>
2025-09-16 14:47:05 +0800

    修改门禁问题
    Signed-off-by: 孙志威 <sunzhiwei13@h-partners.com>

--- services/media_engine/modules/source/source.cpp
+++ services/media_engine/modules/source/source.cpp
@@ -204,7 +204,7 @@ Status Source::GetBitRates(std::vector<uint32_t>& bitRates)
         MEDIA_LOG_E("GetBitRates failed, plugin_ is nullptr");
         return Status::ERROR_INVALID_OPERATION;
     }
-    return plugin_->GetBitRates(bitRates); 
+    return plugin_->GetBitRates(bitRates);
 }
 
 Status Source::SelectBitRate(uint32_t bitRate)
