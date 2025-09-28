c5b59d7bd0ef07885912bdc5e890712f3eb1c097
zengyoulin <zengyoulin@h-partners.com>
2025-09-28 10:39:17 +0800

    update: 更新文件 audio_decoder_aac_new_demo.cpp
    Signed-off-by: zengyoulin <zengyoulin@h-partners.com>

--- test/fuzztest/audiodecoderaac_fuzzer/audio_decoder_aac_new_demo.cpp
+++ test/fuzztest/audiodecoderaac_fuzzer/audio_decoder_aac_new_demo.cpp
@@ -64,7 +64,7 @@ bool AacFuzzDemo::DoAacParserWithParserAPI(const uint8_t *data, size_t size)
     }
     BaseFuzzDemo base;
     base.signal_ = new ADecBufferSignal();
-    base.audioDec_ = OH_AudioCodec_CreateByName((AVCodecCodecName::AUDIO_DECODER_AMRNB_NAME).data());
+    base.audioDec_ = OH_AudioCodec_CreateByName((AVCodecCodecName::AUDIO_DECODER_AAC_NAME).data());
     base.cb_ = base.GetCbFunc();
     DEMO_CHECK_AND_RETURN_RET_LOG(OH_AudioCodec_RegisterCallback(base.audioDec_, base.cb_, base.signal_) == AV_ERR_OK,
                                   false, "Fatal: OH_AudioCodec_RegisterCallback fail");
