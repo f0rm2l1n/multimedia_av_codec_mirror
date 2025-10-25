a51168c4b3f1cbb95312c7ee649abae36d10a4e7
jcc461883977 <jinqiao5@h-partners.com>
2025-10-25 14:28:39 +0800

    add alac tdd test
    Signed-off-by: jcc461883977 <jinqiao5@h-partners.com>

---
+++ test/moduletest/demuxer/src/alacfunc_test.cpp
@@ -0,0 +1,475 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+
+#include "native_avcodec_base.h"
+#include "native_avdemuxer.h"
+#include "native_avformat.h"
+#include "native_avsource.h"
+#include "native_avmemory.h"
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <fcntl.h>
+#include <cmath>
+#include <thread>
+#include <sys/stat.h>
+
+namespace OHOS {
+namespace Media {
+class DemuxerAlacFuncNdkTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+    static void TearDownTestCase(void);
+    void SetUp(void);
+    void TearDown(void);
+
+protected:
+    const char* ALAC_FILE_1 = "/data/test/media/ALAC_16bit_44100Hz.m4a";
+    const char* ALAC_FILE_2 = "/data/test/media/ALAC_24bit_48000Hz.m4a";
+    const char* ALAC_FILE_3 = "/data/test/media/ALAC_32bit_96000Hz.m4a";
+};
+
+static OH_AVMemory* memory = nullptr;
+static OH_AVSource* source = nullptr;
+static OH_AVErrCode ret = AV_ERR_OK;
+static OH_AVDemuxer* demuxer = nullptr;
+static OH_AVFormat* sourceFormat = nullptr;
+static OH_AVFormat* trackFormat = nullptr;
+static OH_AVBuffer* avBuffer = nullptr;
+static OH_AVFormat* format = nullptr;
+static int32_t g_trackCount;
+static int32_t g_memorySize = 1024 * 1024;
+constexpr int32_t THOUSAND = 1000;
+constexpr int32_t DEFAULT_TRACK_COUNT = 1;
+
+void DemuxerAlacFuncNdkTest::SetUpTestCase() {}
+void DemuxerAlacFuncNdkTest::TearDownTestCase() {}
+
+void DemuxerAlacFuncNdkTest::SetUp() {
+    memory = OH_AVMemory_Create(g_memorySize);
+    g_trackCount = 0;
+}
+
+void DemuxerAlacFuncNdkTest::TearDown() {
+    if (trackFormat != nullptr) {
+        OH_AVFormat_Destroy(trackFormat);
+        trackFormat = nullptr;
+    }
+    if (sourceFormat != nullptr) {
+        OH_AVFormat_Destroy(sourceFormat);
+        sourceFormat = nullptr;
+    }
+    if (format != nullptr) {
+        OH_AVFormat_Destroy(format);
+        format = nullptr;
+    }
+    if (memory != nullptr) {
+        OH_AVMemory_Destroy(memory);
+        memory = nullptr;
+    }
+    if (source != nullptr) {
+        OH_AVSource_Destroy(source);
+        source = nullptr;
+    }
+    if (demuxer != nullptr) {
+        OH_AVDemuxer_Destroy(demuxer);
+        demuxer = nullptr;
+    }
+    if (avBuffer != nullptr) {
+        OH_AVBuffer_Destroy(avBuffer);
+        avBuffer = nullptr;
+    }
+}
+} // namespace Media
+} // namespace OHOS
+
+using namespace std;
+using namespace OHOS;
+using namespace OHOS::Media;
+using namespace testing::ext;
+
+static int64_t GetFileSize(const char* fileName) {
+    int64_t fileSize = 0;
+    if (fileName != nullptr) {
+        struct stat fileStatus {};
+        if (stat(fileName, &fileStatus) == 0) {
+            fileSize = static_cast<int64_t>(fileStatus.st_size);
+        }
+    }
+    return fileSize;
+}
+
+struct seekInfo {
+    const char* fileName;
+    OH_AVSeekMode seekmode;
+    int64_t millisecond;
+    int32_t audioCount;
+};
+
+static void CheckSeekModeFromEnd(seekInfo seekInfo) {
+    int trackType = 0;
+    int fd = open(seekInfo.fileName, O_RDONLY);
+    int64_t size = GetFileSize(seekInfo.fileName);
+    cout << seekInfo.fileName << "-------fd:" << fd << "-------size:" << size << endl;
+    
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    
+    demuxer = OH_AVDemuxer_CreateWithSource(source);
+    ASSERT_NE(demuxer, nullptr);
+    
+    sourceFormat = OH_AVSource_GetSourceFormat(source);
+    ASSERT_NE(sourceFormat, nullptr);
+    ASSERT_TRUE(OH_AVFormat_GetIntValue(sourceFormat, OH_MD_KEY_TRACK_COUNT, &g_trackCount));
+    cout << "track count: " << g_trackCount << endl;
+    
+    for (int32_t index = 0; index < g_trackCount; index++) {
+        ASSERT_EQ(AV_ERR_OK, OH_AVDemuxer_SelectTrackByID(demuxer, index));
+    }
+    
+    for (int32_t index = 0; index < g_trackCount; index++) {
+        trackFormat = OH_AVSource_GetTrackFormat(source, index);
+        ASSERT_NE(trackFormat, nullptr);
+        ASSERT_TRUE(OH_AVFormat_GetIntValue(trackFormat, OH_MD_KEY_TRACK_TYPE, &trackType));
+        ASSERT_EQ(AV_ERR_UNKNOWN, OH_AVDemuxer_SeekToTime(
+            demuxer, seekInfo.millisecond / THOUSAND, seekInfo.seekmode));
+    }
+    close(fd);
+}
+
+static void CheckSeekMode(seekInfo seekInfo) {
+    int trackType = 0;
+    OH_AVCodecBufferAttr attr;
+    int fd = open(seekInfo.fileName, O_RDONLY);
+    int64_t size = GetFileSize(seekInfo.fileName);
+    cout << seekInfo.fileName << "-------fd:" << fd << "-------size:" << size << endl;
+    
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    
+    demuxer = OH_AVDemuxer_CreateWithSource(source);
+    ASSERT_NE(demuxer, nullptr);
+    
+    sourceFormat = OH_AVSource_GetSourceFormat(source);
+    ASSERT_NE(sourceFormat, nullptr);
+    ASSERT_TRUE(OH_AVFormat_GetIntValue(sourceFormat, OH_MD_KEY_TRACK_COUNT, &g_trackCount));
+    cout << "track count: " << g_trackCount << endl;
+    
+    for (int32_t index = 0; index < g_trackCount; index++) {
+        ASSERT_EQ(AV_ERR_OK, OH_AVDemuxer_SelectTrackByID(demuxer, index));
+    }
+    
+    for (int32_t index = 0; index < g_trackCount; index++) {
+        trackFormat = OH_AVSource_GetTrackFormat(source, index);
+        ASSERT_NE(trackFormat, nullptr);
+        ASSERT_TRUE(OH_AVFormat_GetIntValue(trackFormat, OH_MD_KEY_TRACK_TYPE, &trackType));
+        
+        ASSERT_EQ(AV_ERR_OK, OH_AVDemuxer_SeekToTime(
+            demuxer, seekInfo.millisecond / THOUSAND, seekInfo.seekmode));
+        
+        bool readEnd = false;
+        int32_t frameNum = 0;
+        while (!readEnd) {
+            ASSERT_EQ(AV_ERR_OK, OH_AVDemuxer_ReadSample(demuxer, index, memory, &attr));
+            if (attr.flags & OH_AVCodecBufferFlags::AVCODEC_BUFFER_FLAGS_EOS) {
+                readEnd = true;
+                break;
+            }
+            frameNum++;
+        }
+        
+        if (trackType == MEDIA_TYPE_AUD) {
+            cout << "audio frame count after seek: " << frameNum << endl;
+            ASSERT_EQ(seekInfo.audioCount, frameNum);
+        }
+    }
+    close(fd);
+}
+
+static void DemuxerAlacResult(const char* fileName, int32_t expectedFrames, int32_t expectedKeyFrames) {
+    int trackType = 0;
+    OH_AVCodecBufferAttr attr;
+    bool isEnd = false;
+    int32_t frameCount = 0;
+    int32_t keyFrameCount = 0;
+    
+    int fd = open(fileName, O_RDONLY);
+    int64_t size = GetFileSize(fileName);
+    cout << fileName << "-------fd:" << fd << "-------size:" << size << endl;
+    
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    
+    demuxer = OH_AVDemuxer_CreateWithSource(source);
+    ASSERT_NE(demuxer, nullptr);
+    
+    sourceFormat = OH_AVSource_GetSourceFormat(source);
+    ASSERT_TRUE(OH_AVFormat_GetIntValue(sourceFormat, OH_MD_KEY_TRACK_COUNT, &g_trackCount));
+    ASSERT_EQ(g_trackCount, DEFAULT_TRACK_COUNT);
+    
+    for (int32_t index = 0; index < g_trackCount; index++) {
+        ASSERT_EQ(AV_ERR_OK, OH_AVDemuxer_SelectTrackByID(demuxer, index));
+        trackFormat = OH_AVSource_GetTrackFormat(source, index);
+        ASSERT_NE(trackFormat, nullptr);
+        ASSERT_TRUE(OH_AVFormat_GetIntValue(trackFormat, OH_MD_KEY_TRACK_TYPE, &trackType));
+        ASSERT_EQ(trackType, MEDIA_TYPE_AUD);
+        
+        while (!isEnd) {
+            ASSERT_EQ(AV_ERR_OK, OH_AVDemuxer_ReadSample(demuxer, index, memory, &attr));
+            if (attr.flags & OH_AVCodecBufferFlags::AVCODEC_BUFFER_FLAGS_EOS) {
+                isEnd = true;
+                break;
+            }
+            frameCount++;
+            if (attr.flags & OH_AVCodecBufferFlags::AVCODEC_BUFFER_FLAGS_SYNC_FRAME) {
+                keyFrameCount++;
+            }
+        }
+    }
+    
+    ASSERT_EQ(frameCount, expectedFrames);
+    ASSERT_EQ(keyFrameCount, expectedKeyFrames);
+    close(fd);
+}
+
+/**
+ * @tc.number    : DEMUXER_ALAC_FUNC_0001
+ * @tc.name      : demuxer ALAC ,GetTrackFormat,OH_MD_KEY_AUD_BIT_DEPTH
+ * @tc.desc      : function test
+ */
+HWTEST_F(DemuxerAlacFuncNdkTest, DEMUXER_ALAC_FUNC_0001, TestSize.Level2) {
+    int32_t bitDepth = 0;
+    const char* file = ALAC_FILE_1;
+    int fd = open(file, O_RDONLY);
+    int64_t size = GetFileSize(file);
+    cout << file << "-------fd:" << fd << "-------size:" << size << endl;
+    
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    
+    trackFormat = OH_AVSource_GetTrackFormat(source, 0);
+    ASSERT_NE(trackFormat, nullptr);
+    ASSERT_TRUE(OH_AVFormat_GetIntValue(trackFormat, OH_MD_KEY_AUD_BIT_DEPTH, &bitDepth));
+    ASSERT_EQ(bitDepth, 16);
+    
+    close(fd);
+}
+
+/**
+ * @tc.number    : DEMUXER_ALAC_FUNC_0002
+ * @tc.name      : demuxer ALAC ,GetTrackFormat,OH_MD_KEY_AUD_SAMPLE_RATE
+ * @tc.desc      : function test
+ */
+HWTEST_F(DemuxerAlacFuncNdkTest, DEMUXER_ALAC_FUNC_0002, TestSize.Level2) {
+    int32_t sampleRate = 0;
+    const char* file = ALAC_FILE_2;
+    int fd = open(file, O_RDONLY);
+    int64_t size = GetFileSize(file);
+    cout << file << "-------fd:" << fd << "-------size:" << size << endl;
+    
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    
+    trackFormat = OH_AVSource_GetTrackFormat(source, 0);
+    ASSERT_NE(trackFormat, nullptr);
+    ASSERT_TRUE(OH_AVFormat_GetIntValue(trackFormat, OH_MD_KEY_AUD_SAMPLE_RATE, &sampleRate));
+    ASSERT_EQ(sampleRate, 48000);
+    
+    close(fd);
+}
+
+/**
+ * @tc.number    : DEMUXER_ALAC_FUNC_0003
+ * @tc.name      : demuxer ALAC ,GetTrackFormat,OH_MD_KEY_AUD_CHANNEL_COUNT
+ * @tc.desc      : function test
+ */
+HWTEST_F(DemuxerAlacFuncNdkTest, DEMUXER_ALAC_FUNC_0003, TestSize.Level2) {
+    int32_t channelCount = 0;
+    const char* file = ALAC_FILE_2;
+    int fd = open(file, O_RDONLY);
+    int64_t size = GetFileSize(file);
+    cout << file << "-------fd:" << fd << "-------size:" << size << endl;
+    
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    
+    trackFormat = OH_AVSource_GetTrackFormat(source, 0);
+    ASSERT_NE(trackFormat, nullptr);
+    ASSERT_TRUE(OH_AVFormat_GetIntValue(trackFormat, OH_MD_KEY_AUD_CHANNEL_COUNT, &channelCount));
+    ASSERT_EQ(channelCount, 6);
+    
+    close(fd);
+}
+
+/**
+ * @tc.number    : DEMUXER_ALAC_FUNC_0004
+ * @tc.name      : demuxer ALAC ,GetTrackFormat,OH_MD_KEY_CODEC_MIME
+ * @tc.desc      : function test
+ */
+HWTEST_F(DemuxerAlacFuncNdkTest, DEMUXER_ALAC_FUNC_0004, TestSize.Level2) {
+    const char* mimeType = nullptr;
+    const char* file = ALAC_FILE_1;
+    int fd = open(file, O_RDONLY);
+    int64_t size = GetFileSize(file);
+    cout << file << "-------fd:" << fd << "-------size:" << size << endl;
+    
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    
+    trackFormat = OH_AVSource_GetTrackFormat(source, 0);
+    ASSERT_NE(trackFormat, nullptr);
+    ASSERT_TRUE(OH_AVFormat_GetStringValue(trackFormat, OH_MD_KEY_CODEC_MIME, &mimeType));
+    ASSERT_STREQ(mimeType, OH_AVCODEC_MIMETYPE_AUDIO_ALAC);
+    
+    close(fd);
+}
+
+/**
+ * @tc.number    : DEMUXER_ALAC_FUNC_0005
+ * @tc.name      : demuxer ALAC ,GetSourceFormat,OH_MD_KEY_TRACK_COUNT
+ * @tc.desc      : function test
+ */
+HWTEST_F(DemuxerAlacFuncNdkTest, DEMUXER_ALAC_FUNC_0005, TestSize.Level2) {
+    const char* file = ALAC_FILE_3;
+    int fd = open(file, O_RDONLY);
+    int64_t size = GetFileSize(file);
+    cout << file << "-------fd:" << fd << "-------size:" << size << endl;
+    
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    
+    sourceFormat = OH_AVSource_GetSourceFormat(source);
+    ASSERT_TRUE(OH_AVFormat_GetIntValue(sourceFormat, OH_MD_KEY_TRACK_COUNT, &g_trackCount));
+    ASSERT_EQ(g_trackCount, DEFAULT_TRACK_COUNT);
+    
+    close(fd);
+}
+
+/**
+ * @tc.number    : DEMUXER_ALAC_FUNC_0006
+ * @tc.name      : demuxer ALAC ,GetTrackFormat,OH_MD_KEY_TRACK_TYPE
+ * @tc.desc      : function test
+ */
+HWTEST_F(DemuxerAlacFuncNdkTest, DEMUXER_ALAC_FUNC_0006, TestSize.Level2) {
+    int32_t trackType = 0;
+    const char* file = ALAC_FILE_1;
+    int fd = open(file, O_RDONLY);
+    int64_t size = GetFileSize(file);
+    cout << file << "-------fd:" << fd << "-------size:" << size << endl;
+    
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    
+    trackFormat = OH_AVSource_GetTrackFormat(source, 0);
+    ASSERT_NE(trackFormat, nullptr);
+    ASSERT_TRUE(OH_AVFormat_GetIntValue(trackFormat, OH_MD_KEY_TRACK_TYPE, &trackType));
+    ASSERT_EQ(trackType, MEDIA_TYPE_AUD);
+    
+    close(fd);
+}
+
+/**
+ * @tc.number    : DEMUXER_ALAC_FUNC_0007
+ * @tc.name      : demuxer ALAC ,ReadSample,check sample size
+ * @tc.desc      : function test
+ */
+HWTEST_F(DemuxerAlacFuncNdkTest, DEMUXER_ALAC_FUNC_0007, TestSize.Level3) {
+    OH_AVCodecBufferAttr attr{};
+    const char* file = ALAC_FILE_1;
+    int fd = open(file, O_RDONLY);
+    int64_t size = GetFileSize(file);
+    cout << file << "-------fd:" << fd << "-------size:" << size << endl;
+    
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    
+    demuxer = OH_AVDemuxer_CreateWithSource(source);
+    ASSERT_NE(demuxer, nullptr);
+    ASSERT_EQ(OH_AVDemuxer_SelectTrackByID(demuxer, 0), AV_ERR_OK);
+    
+    ASSERT_EQ(OH_AVDemuxer_ReadSample(demuxer, 0, memory, &attr), AV_ERR_OK);
+    ASSERT_GT(attr.size, 0);
+    ASSERT_GT(attr.pts, 0);
+    
+    close(fd);
+}
+
+/**
+ * @tc.number    : DEMUXER_ALAC_FUNC_0008
+ * @tc.name      : demuxer ALAC ,full demux,16bit stereo
+ * @tc.desc      : function test
+ */
+HWTEST_F(DemuxerAlacFuncNdkTest, DEMUXER_ALAC_FUNC_0008, TestSize.Level3) {
+    DemuxerAlacResult(ALAC_FILE_1, 128, 8);
+}
+
+/**
+ * @tc.number    : DEMUXER_ALAC_FUNC_0009
+ * @tc.name      : demuxer ALAC ,full demux,24bit 5.1
+ * @tc.desc      : function test
+ */
+HWTEST_F(DemuxerAlacFuncNdkTest, DEMUXER_ALAC_FUNC_0009, TestSize.Level3) {
+    DemuxerAlacResult(ALAC_FILE_2, 96, 6);
+}
+
+/**
+ * @tc.number    : DEMUXER_ALAC_FUNC_0010
+ * @tc.name      : demuxer ALAC ,GetTrackFormat,OH_MD_KEY_BITRATE
+ * @tc.desc      : function test
+ */
+HWTEST_F(DemuxerAlacFuncNdkTest, DEMUXER_ALAC_FUNC_0010, TestSize.Level2) {
+    int64_t bitRate = 0;
+    const char* file = ALAC_FILE_1;
+    int fd = open(file, O_RDONLY);
+    int64_t size = GetFileSize(file);
+    cout << file << "-------fd:" << fd << "-------size:" << size << endl;
+    
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    
+    trackFormat = OH_AVSource_GetTrackFormat(source, 0);
+    ASSERT_NE(trackFormat, nullptr);
+    ASSERT_TRUE(OH_AVFormat_GetLongValue(trackFormat, OH_MD_KEY_BITRATE, &bitRate));
+    ASSERT_GT(bitRate, 0);
+    ASSERT_LT(bitRate, 2000000);
+    
+    close(fd);
+}
+
+/**
+ * @tc.number    : DEMUXER_ALAC_FUNC_0011
+ * @tc.name      : demuxer ALAC ,GetTrackFormat,OH_MD_KEY_AUD_CHANNEL_LAYOUT
+ * @tc.desc      : function test
+ */
+HWTEST_F(DemuxerAlacFuncNdkTest, DEMUXER_ALAC_FUNC_0011, TestSize.Level2) {
+    int64_t channelLayout = 0;
+    const char* file = ALAC_FILE_2;
+    int fd = open(file, O_RDONLY);
+    int64_t size = GetFileSize(file);
+    cout << file << "-------fd:" << fd << "-------size:" << size << endl;
+    
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    
+    trackFormat = OH_AVSource_GetTrackFormat(source, 0);
+    ASSERT_NE(trackFormat, nullptr);
+    ASSERT_TRUE(OH_AVFormat_GetLongValue(trackFormat, OH_MD_KEY_AUD_CHANNEL_LAYOUT, &channelLayout));
+    ASSERT_EQ(channelLayout, 0x3F);
+    
+    close(fd);
+}
