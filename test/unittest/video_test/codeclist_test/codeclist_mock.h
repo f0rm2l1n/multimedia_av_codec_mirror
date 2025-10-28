55ba717a28fe0eb7068e0fa720674906f662fdc1
TFDeeplearner <yanghaolong2@huawei.com>
2025-10-28 10:34:40 +0800

    update OH_AVCapability_GetVideoSupportedNativeBufferFormats Signed-off-by: TFDeeplearner <yanghaolong2@huawei.com>

--- test/unittest/video_test/codeclist_test/codeclist_mock.h
+++ test/unittest/video_test/codeclist_test/codeclist_mock.h
@@ -121,6 +121,11 @@ const std::vector<int32_t> DEFAULT_AUDIO_ACC_SAMPLES = {7350, 8000,  11025, 1200
 const std::vector<int32_t> DEFAULT_VIDEO_AVC_PIXFORMATS = {
     static_cast<int32_t>(VideoPixelFormat::YUVI420), static_cast<int32_t>(VideoPixelFormat::NV12),
     static_cast<int32_t>(VideoPixelFormat::NV21), static_cast<int32_t>(VideoPixelFormat::RGBA)};
+const std::vector<int32_t> DEFAULT_VIDEO_AVC_GRAPHIC_FORMATS = {
+    static_cast<int32_t>(GraphicPixelFormat::GRAPHIC_PIXEL_FMT_YCBCR_420_P),
+    static_cast<int32_t>(GraphicPixelFormat::GRAPHIC_PIXEL_FMT_YCBCR_420_SP),
+    static_cast<int32_t>(GraphicPixelFormat::GRAPHIC_PIXEL_FMT_YCRCB_420_SP),
+    static_cast<int32_t>(GraphicPixelFormat::GRAPHIC_PIXEL_FMT_RGBA_8888)};
 const std::vector<int32_t> DEFAULT_VIDEO_AVC_PROFILES = {AVC_PROFILE_BASELINE, AVC_PROFILE_HIGH, AVC_PROFILE_MAIN};
 const std::vector<int32_t> DEFAULT_VIDEO_AVC_LEVELS = {
     AVC_LEVEL_1,  AVC_LEVEL_1b, AVC_LEVEL_11, AVC_LEVEL_12, AVC_LEVEL_13, AVC_LEVEL_2,  AVC_LEVEL_21,
