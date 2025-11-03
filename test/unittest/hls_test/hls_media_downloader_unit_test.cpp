b3a98b37c0e7c156a65c16e6f7d7c32f0c2598c0
xiangtongtong <xiangtongtong1@h-partners.com>
2025-11-03 16:03:31 +0800

    TDD错误用例修复
    Signed-off-by: xiangtongtong <xiangtongtong1@h-partners.com>

--- test/unittest/hls_test/hls_media_downloader_unit_test.cpp
+++ test/unittest/hls_test/hls_media_downloader_unit_test.cpp
@@ -1632,17 +1632,20 @@ HWTEST_F(HlsMediaDownloaderUnitTest, REMOVE_FMP4_PADDING_DATA_001, TestSize.Leve
     readDataInfo.streamId_ = 1;
     readDataInfo.wantReadLength_ = 100;
     readDataInfo.realReadLength_ = 16;
+    unsigned int originRealReadLength = 16;
     downloader->RemoveFmp4PaddingData(buffer, readDataInfo);
-    EXPECT_EQ(readDataInfo.realReadLength_, 16);
+    EXPECT_EQ(readDataInfo.realReadLength_, originRealReadLength);
     downloader->keyLen_ = 1;
     downloader->RemoveFmp4PaddingData(buffer, readDataInfo);
     EXPECT_NE(readDataInfo.realReadLength_, 16);
     readDataInfo.realReadLength_ = 0;
+    originRealReadLength = 0;
     downloader->RemoveFmp4PaddingData(buffer, readDataInfo);
-    EXPECT_EQ(readDataInfo.realReadLength_, 0);
+    EXPECT_EQ(readDataInfo.realReadLength_, originRealReadLength);
     readDataInfo.realReadLength_ = 5;
+    originRealReadLength = 5
     downloader->RemoveFmp4PaddingData(buffer, readDataInfo);
-    EXPECT_EQ(readDataInfo.realReadLength_, 5);
+    EXPECT_EQ(readDataInfo.realReadLength_, originRealReadLength);
 }
 
 HWTEST_F(HlsMediaDownloaderUnitTest, READ_HEADER_DATA_001, TestSize.Level1)
