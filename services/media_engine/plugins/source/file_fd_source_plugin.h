4de999d892955b7d77d9a9b65eea188aa781417a
gcw_4z3jiPvB <huweinan1@h-partners.com>
2025-11-17 19:59:23 +0800

    update: 更新文件 file_fd_source_plugin.h
    Signed-off-by: gcw_4z3jiPvB <huweinan1@h-partners.com>

--- services/media_engine/plugins/source/file_fd_source_plugin.h
+++ services/media_engine/plugins/source/file_fd_source_plugin.h
@@ -117,7 +117,6 @@ private:
     int64_t lastReadTime_ {0};
     bool isEnableFdCache_{ true };
     int loc_ {0};
-    int32_t maxBitRate_ {0};
 };
 } // namespace FileSource
 } // namespace Plugins
