8b791c261fe79e8888bf02f7c2b76c570040c6ec
xujifeng <xujifeng2@h-partners.com>
2025-11-20 21:23:30 +0800

    解封装优化MP4初始化获取视频首帧
    Signed-off-by: xujifeng <xujifeng2@h-partners.com>

--- services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.h
+++ services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.h
@@ -328,6 +328,8 @@ private:
     bool IsMultiVideoTrack();
     int AVReadFrameLimit(AVPacket *pkt);
     Status SetAVReadFrameLimit();
+    void SetAVReadFrameLimitDefault();
+    bool Mp4CheckKeyFrame(AVStream* stream);
 
     Status WaitForLoop(const uint32_t trackId, const uint32_t timeout);
     bool ShouldWaitForRead(uint32_t trackId);
