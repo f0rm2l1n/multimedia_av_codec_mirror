f37e4824982024437a09780c08d614ced6a568eb
zhaona45 <zhaona45@huawei.com>
2025-12-10 15:03:50 +0800

    modify rv40 resource
    Signed-off-by: zhaona45 <zhaona45@huawei.com>
    Change-Id: I810e9f5d374aa301b1ba0bd3f7e2a389f9e075e6

--- test/unittest/video_test/vcodec_framework_test/func_sample/sample/decoder/vdec_async_sample.cpp
+++ test/unittest/video_test/vcodec_framework_test/func_sample/sample/decoder/vdec_async_sample.cpp
@@ -1065,7 +1065,7 @@ int32_t VideoDecAsyncSample::OutputLoopInnerExt()
                         testParam_ == VCodecTestParam::SW_MPEG4 || testParam_ == VCodecTestParam::SW_H263 ||
                         testParam_ == VCodecTestParam::SW_VC1 || testParam_ == VCodecTestParam::SW_MSVIDEO1 ||
                         testParam_ == VCodecTestParam::SW_WMV3 || testParam_ == VCodecTestParam::SW_RV30 ||
-                        testParam_ == VCodecTestParam::SW_RV40 || testParam_ == VCodecTestParam::SW_WVC1)
+                        testParam_ == VCodecTestParam::SW_RV40_TEST || testParam_ == VCodecTestParam::SW_WVC1)
                            ? attr.size : buffer->GetNativeBuffer()->GetSize();
         UNITTEST_CHECK_AND_RETURN_RET_LOG(bufferAddr != nullptr, AV_ERR_INVALID_VAL,
                                           "Fatal: GetOutputBuffer fail, exit, index: %d", index);
