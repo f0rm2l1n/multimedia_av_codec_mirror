1e4f672aab270ab206a9165e1ea5e9676eedc460
naplescncn <chenning39@huawei.com>
2025-12-30 00:41:46 +0800

    update: 更新文件 http_source_plugin.cpp
    Signed-off-by: naplescncn <chenning39@huawei.com>

--- services/media_engine/plugins/source/http_source/http_source_plugin.cpp
+++ services/media_engine/plugins/source/http_source/http_source_plugin.cpp
@@ -79,7 +79,7 @@ std::string HttpSourcePlugin::GetContentType()
     return downloader_->GetContentType();
 }
 
-Status HttpSourcePlugin::Init()
+Status HttpSourcePlugin::Init() const
 {
     MEDIA_LOG_D("Init enter.");
     return Status::OK;
