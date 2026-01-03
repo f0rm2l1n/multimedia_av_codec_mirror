af215d58c171cdd30f0f4bceea74a1b060a83dff
yu_qi0 <suyixin9@h-partners.com>
2026-01-03 16:40:52 +0800

    流媒体离线缓存
    Signed-off-by: yu_qi0 <suyixin9@h-partners.com>

---
+++ services/media_engine/plugins/source/http_source/utils/storage_usage_util.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#ifndef STORAGE_USAGE_UTIL_H
+#define STORAGE_USAGE_UTIL_H
+ 
+#include "common/media_core.h"
+ 
+namespace OHOS {
+namespace Media {
+class StorageUsageUtil {
+public:
+    StorageUsageUtil() = default;
+    ~StorageUsageUtil() = default;
+    int64_t GetFreeSize();
+    int64_t GetTotalSize();
+    bool hasEnoughStorage();
+};
+} // namespace Media
+} // namespace OHOS
+#endif // STORAGE_USAGE_UTIL_H
