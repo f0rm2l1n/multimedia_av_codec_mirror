3c5546916b75f77c82f4253bcb2a30edb16191de
westyao <weihouyao@huawei.com>
2026-01-06 15:36:19 +0800

    【修改说明】fix incompatible code in codeclist
    Signed-off-by: westyao <weihouyao@huawei.com>

--- frameworks/native/avcodeclist/avcodeclist_impl.cpp
+++ frameworks/native/avcodeclist/avcodeclist_impl.cpp
@@ -116,7 +116,11 @@ CapabilityData *AVCodecListImpl::GetCapability(const std::string &mime, const bo
         "Get capability failed from service, mime: %{public}s, isEnc: %{public}d, category: %{public}d",
         mime.c_str(), isEncoder, category);
 
-    mimeCapsMap_.emplace(mime, capData);
+    if (std::find_if(mimeCapsRange.first, mimeCapsRange.second, [&capData](const auto &item) {
+            return item.second->codecName == capData->codecName;
+        }) == mimeCapsRange.second) {
+        mimeCapsMap_.emplace(mime, capData);
+    }
     AVCODEC_LOGD("Get capabilityData successfully, mime: %{public}s, isEnc: %{public}d, category: %{public}d",
         mime.c_str(), isEncoder, category);
     return capData.get();
