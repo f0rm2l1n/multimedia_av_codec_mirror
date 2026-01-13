9ea204beb4248e8b86533f784d2b2f98be3d986d
cyc2414106632 <chenyuchen8@h-partners.com>
2026-01-13 15:15:06 +0800

    demuxer add SeekToFrameByDts function
    Signed-off-by: cyc2414106632 <chenyuchen8@h-partners.com>
    Change-Id: I91dbf6c2cfa6f2906a39d53a682b67b075030df0

--- services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.cpp
+++ services/media_engine/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.cpp
@@ -3032,13 +3032,12 @@ Status FFmpegDemuxerPlugin::SeekToFrameByDts(int32_t trackId, int64_t seekTime,
     if (readThread_ != nullptr) {
         SyncSeekThread();
     }
-    FALSE_RETURN_V_MSG_E(formatContext_ != nullptr, Status::ERROR_NULL_POINTER,
-        "AVFormatContext is nullptr");
-    FALSE_RETURN_V_MSG_E(!selectedTrackIds_.empty(), Status::ERROR_INVALID_OPERATION,
-        "No track has been selected");
-    FALSE_RETURN_V_MSG_E(seekTime >= 0 && seekTime <= INT64_MAX / MS_TO_NS,
-        Status::ERROR_INVALID_PARAMETER,
+    FALSE_RETURN_V_MSG_E(formatContext_ != nullptr, Status::ERROR_NULL_POINTER, "AVFormatContext is nullptr");
+    FALSE_RETURN_V_MSG_E(!selectedTrackIds_.empty(), Status::ERROR_INVALID_OPERATION, "No track has been selected"); 
+    FALSE_RETURN_V_MSG_E(seekTime >= 0 && seekTime <= INT64_MAX / MS_TO_NS, Status::ERROR_INVALID_PARAMETER,
         "Seek time " PUBLIC_LOG_D64 " is not supported", seekTime);
+    FALSE_RETURN_V_MSG_E(mode == SeekMode::SEEK_CLOSEST, Status::ERROR_INVALID_PARAMETER,
+        "Seek mode " PUBLIC_LOG_U32 " is not supported", static_cast<uint32_t>(mode));
     int trackIndex = SelectSeekTrack();
     MEDIA_LOG_D("Seek by DTS based on track " PUBLIC_LOG_D32, trackIndex);
     AVStream* avStream = formatContext_->streams[trackIndex];
@@ -3048,6 +3047,12 @@ Status FFmpegDemuxerPlugin::SeekToFrameByDts(int32_t trackId, int64_t seekTime,
     MEDIA_LOG_D("Seek DTS: " PUBLIC_LOG_D64 " ms -> " PUBLIC_LOG_D64 " (FFmpeg time_base)",
         seekTime, ffDts);
     int ffRet = AVSeekFrameLock(trackIndex, ffDts, AVSEEK_FLAG_ANY);
+    // 如果不支持（返回 EPERM 或其他错误），回退到 AVSEEK_FLAG_FRAME
+    if (ffRet < 0) {
+        MEDIA_LOG_W("AVSEEK_FLAG_ANY failed (err: %s), fallback to AVSEEK_FLAG_FRAME", 
+            AVStrError(ffRet).c_str());
+        ffRet = AVSeekFrameLock(trackIndex, ffDts, AVSEEK_FLAG_FRAME);
+    }
     FALSE_RETURN_V_MSG_E(ffRet >= 0, Status::ERROR_UNKNOWN,
         "Call av_seek_frame failed, err: " PUBLIC_LOG_S, AVStrError(ffRet).c_str());
     formatContext_->pb->error = 0;
