6bcb82877cf304df3182e856622eb687f39f3a32
yu_qi0 <suyixin9@h-partners.com>
2026-01-23 20:53:33 +0800

    流媒体安全问题
    Signed-off-by: yu_qi0 <suyixin9@h-partners.com>

--- services/media_engine/plugins/source/http_source/hls/hls_tags.cpp
+++ services/media_engine/plugins/source/http_source/hls/hls_tags.cpp
@@ -12,13 +12,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#include <cstdint>
 #include <cstring>
 #include <sstream>
 #include <stack>
 #include <utility>
 #include "hls_tags.h"
-
 namespace OHOS {
 namespace Media {
 namespace Plugins {
