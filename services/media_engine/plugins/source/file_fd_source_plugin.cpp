f0b379387ab33a813df2cd1628a02328d82c7a4e
孙志威 <sunzhiwei13@h-partners.com>
2026-01-27 19:09:31 +0800

    修改门禁问题
    Signed-off-by: 孙志威 <sunzhiwei13@h-partners.com>

--- services/media_engine/plugins/source/file_fd_source_plugin.cpp
+++ services/media_engine/plugins/source/file_fd_source_plugin.cpp
@@ -401,11 +401,11 @@ Status FileFdSourcePlugin::ParseUriInfo(const std::string& uri)
         if (static_cast<uint64_t>(offset_) > fileSize_) {
             offset_ = static_cast<int64_t>(fileSize_);
         }
-        int64_t tempSize_ = 0;
+        int64_t tempSize = 0;
         std::string sizeStr = fdUriMatch[3].str();
-        FALSE_RETURN_V_MSG_E(StrToLong(sizeStr, tempSize_), Status::ERROR_INVALID_PARAMETER,
+        FALSE_RETURN_V_MSG_E(StrToLong(sizeStr, tempSize), Status::ERROR_INVALID_PARAMETER,
             "Failed to read size.");
-        size_ = static_cast<uint64_t>(tempSize_);
+        size_ = static_cast<uint64_t>(tempSize);
         uint64_t remainingSize = fileSize_ - static_cast<uint64_t>(offset_);
         if (size_ > remainingSize) {
             size_ = remainingSize;
