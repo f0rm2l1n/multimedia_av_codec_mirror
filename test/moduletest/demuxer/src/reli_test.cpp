c0612d51f14535f4bfda09b779c2b22984888ddd
zhaona45 <zhaona45@huawei.com>
2026-01-28 16:35:58 +0800

    add sunAu UT FUZZ module test
    Signed-off-by: zhaona45 <zhaona45@huawei.com>
    Change-Id: I50aee1f30b351b38f071128f8563dd4429c4d8f1

--- test/moduletest/demuxer/src/reli_test.cpp
+++ test/moduletest/demuxer/src/reli_test.cpp
@@ -2881,6 +2881,50 @@ HWTEST_F(DemuxerReliNdkTest, DEMUXER_RELI_10200, TestSize.Level3)
 }
 
 /**
+ * @tc.number    : DEMUXER_RELI_10300
+ * @tc.name      : create source with fd, sun au
+ * @tc.desc      : function test
+ */
+HWTEST_F(DemuxerReliNdkTest, DEMUXER_RELI_10300, TestSize.Level3)
+{
+    OH_AVCodecBufferAttr attr;
+    bool isEnd = false;
+    const char *file = "/data/test/media/au_pcm_alaw.au";
+    int fd = open(file, O_RDONLY);
+    int64_t size = GetFileSize(file);
+    cout << file << "----------------------" << fd << "---------" << size << endl;
+    source = OH_AVSource_CreateWithFD(fd, 0, size);
+    ASSERT_NE(source, nullptr);
+    demuxer = OH_AVDemuxer_CreateWithSource(source);
+    ASSERT_NE(demuxer, nullptr);
+    sourceFormat = OH_AVSource_GetSourceFormat(source);
+    ASSERT_TRUE(OH_AVFormat_GetIntValue(sourceFormat, OH_MD_KEY_TRACK_COUNT, &g_trackCount));
+    for (int32_t index = 0; index < g_trackCount; index++) {
+        ASSERT_EQ(AV_ERR_OK, OH_AVDemuxer_SelectTrackByID(demuxer, index));
+    }
+    int audioFrame = 0;
+    int keyCount = 0;
+    while (!isEnd) {
+        for (int32_t index = 0; index < g_trackCount; index++) {
+            ASSERT_EQ(AV_ERR_OK, OH_AVDemuxer_ReadSampleBuffer(demuxer, index, buffer));
+            ASSERT_EQ(AV_ERR_OK, OH_AVBuffer_GetBufferAttr(buffer, &attr));
+            cout << attr.size << "size---------------pts:" << attr.pts << endl;
+            if (attr.flags == OH_AVCodecBufferFlags::AVCODEC_BUFFER_FLAGS_EOS) {
+                isEnd = true;
+                cout << "isend !!!!!!!!!!!!!!!" << endl;
+                continue;
+            }
+            audioFrame++;
+            if (attr.flags & OH_AVCodecBufferFlags::AVCODEC_BUFFER_FLAGS_SYNC_FRAME) {
+                keyCount++;
+            }
+        }
+    }
+    ASSERT_EQ(audioFrame, 33);
+    ASSERT_EQ(keyCount, 33);
+}
+
+/**
  * @tc.number    : DEMUXER_RELI_10700
  * @tc.name      : create source with fd, asf
  * @tc.desc      : function test
