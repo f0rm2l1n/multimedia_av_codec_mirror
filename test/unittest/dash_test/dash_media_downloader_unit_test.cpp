ae9614fb47ac4a07bcbc4937ab929290171a4400
yu_qi0 <suyixin9@h-partners.com>
2026-02-06 17:28:44 +0800

    流媒体安全问题
    Signed-off-by: yu_qi0 <suyixin9@h-partners.com>

--- test/unittest/dash_test/dash_media_downloader_unit_test.cpp
+++ test/unittest/dash_test/dash_media_downloader_unit_test.cpp
@@ -400,7 +400,6 @@ HWTEST_F(DashMediaDownloaderUnitTest, TEST_SEEK_TO_TIME, TestSize.Level1)
 HWTEST_F(DashMediaDownloaderUnitTest, TEST_GET_READ, TestSize.Level1)
 {
     std::shared_ptr<DashMediaDownloader> mediaDownloader = std::make_shared<DashMediaDownloader>(nullptr);
-    int32_t appUid = 0;
     mediaDownloader->Init();
     std::string testUrl = MPD_SEGMENT_LIST;
     std::map<std::string, std::string> httpHeader;
@@ -411,7 +410,7 @@ HWTEST_F(DashMediaDownloaderUnitTest, TEST_GET_READ, TestSize.Level1)
 
     mediaDownloader->Open(testUrl, httpHeader);
     mediaDownloader->GetSeekable();
-    appUid = 1;
+    int32_t appUid = 1;
     mediaDownloader->SetAppUid(appUid);
     std::vector<StreamInfo> streams;
     mediaDownloader->GetStreamInfo(streams);
