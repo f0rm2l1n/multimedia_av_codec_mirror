77bb71d5a88ce187f606438e865a2b59d600eaa7
zzs110 <zhouzishuai@huawei.com>
2026-02-11 11:06:10 +0800

    蓝黄同步
    Signed-off-by: zzs110 <zhouzishuai@huawei.com>

--- services/media_engine/modules/demuxer/base_stream_demuxer.h
+++ services/media_engine/modules/demuxer/base_stream_demuxer.h
@@ -112,7 +112,6 @@ public:
     bool GetIsIgnoreParse();
     Plugins::Seekable GetSeekable();
     virtual void SetInterruptState(bool isInterruptNeeded);
-    virtual std::string SnifferMediaType(int32_t streamID);
     virtual std::string SnifferMediaType(const StreamInfo& streamInfo);
     bool IsDash() const;
     void SetIsDash(bool flag);
