10e34102c5bddccd0a3e3a9ee3276e1ba449b065
zzs110 <zhouzishuai@huawei.com>
2026-02-23 14:39:40 +0800

    蓝黄不一致
    Signed-off-by: zzs110 <zhouzishuai@huawei.com>

--- services/media_engine/plugins/source/http_source/hls/hls_playlist_downloader.cpp
+++ services/media_engine/plugins/source/http_source/hls/hls_playlist_downloader.cpp
@@ -633,9 +633,7 @@ bool HlsPlayListDownloader::ReadMediaHeader(const std::list<std::shared_ptr<M3U8
             !stream->m3u8_->isHeaderReady_) {
             continue;
         }
-        errno_t err = memcpy_s(buffer, wantLen,
-            stream->m3u8_->fmp4Header_,
-            stream->m3u8_->downloadHeaderLen_);
+        errno_t err = memcpy_s(buffer, wantLen, stream->m3u8_->fmp4Header_, stream->m3u8_->downloadHeaderLen_);
         if (err == 0) {
             readLen = stream->m3u8_->downloadHeaderLen_;
             return true;
@@ -655,8 +653,7 @@ bool HlsPlayListDownloader::ReadStreamHeader(const std::list<std::shared_ptr<M3U
             !stream->m3u8_->isHeaderReady_) {
             continue;
         }
-        errno_t err = memcpy_s(buffer, wantLen, stream->m3u8_->fmp4Header_,
-            stream->m3u8_->downloadHeaderLen_);
+        errno_t err = memcpy_s(buffer, wantLen, stream->m3u8_->fmp4Header_, stream->m3u8_->downloadHeaderLen_);
         if (err == 0) {
             readLen = stream->m3u8_->downloadHeaderLen_;
             return true;
@@ -712,7 +709,7 @@ void HlsPlayListDownloader::GetMediaStreams(StreamType streamType, std::vector<S
         if (streamType == StreamType::SUBTITLE) {
             isDefault = currentVariant_->defaultSubtitles_ != nullptr &&
                 media->streamId_ == currentVariant_->defaultSubtitles_->streamId_;
-                streamInfo.sniffSize = DEFAULT_SUBTITLE_SNIFFSIZE;
+            streamInfo.sniffSize = DEFAULT_SUBTITLE_SNIFFSIZE;
         }
         
         if (isDefault) {
