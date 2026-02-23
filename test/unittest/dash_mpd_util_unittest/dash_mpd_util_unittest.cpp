ccc40af69ff4847a2fc2e7106e1ae9bf967e118a
linxinyu93 <linxinyu1@h-partners.com>
2026-02-23 17:35:06 +0800

    fix: too many string copy in GetSubstitution
    Signed-off-by: linxinyu93 <linxinyu1@h-partners.com>

--- test/unittest/dash_mpd_util_unittest/dash_mpd_util_unittest.cpp
+++ test/unittest/dash_mpd_util_unittest/dash_mpd_util_unittest.cpp
@@ -120,6 +120,204 @@ HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_002, TestSize.Level0)
 }
 
 /**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_003
+ * @tc.desc  : Test GetSubstitutionStr with format width 5, substitutionStr "5" -> "00005"
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_003, TestSize.Level0)
+{
+    std::string segTmpltStr = "segment_$Number%05d$.m4s";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "5";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "segment_00005.m4s");
+}
+
+/**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_004
+ * @tc.desc  : Test GetSubstitutionStr with format width equals string length
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_004, TestSize.Level0)
+{
+    std::string segTmpltStr = "segment_$Number%05d$.m4s";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "12345";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "segment_12345.m4s");
+}
+
+/**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_005
+ * @tc.desc  : Test GetSubstitutionStr with format width less than string length
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_005, TestSize.Level0)
+{
+    std::string segTmpltStr = "segment_$Number%03d$.m4s";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "12345";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "segment_12345.m4s");
+}
+
+/**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_006
+ * @tc.desc  : Test GetSubstitutionStr with format width 1
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_006, TestSize.Level0)
+{
+    std::string segTmpltStr = "segment_$Number%01d$.m4s";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "5";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "segment_5.m4s");
+}
+
+/**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_007
+ * @tc.desc  : Test GetSubstitutionStr with format width equals MAX_SUBSTITUTION_FORMAT_WIDTH (32)
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_007, TestSize.Level0)
+{
+    std::string segTmpltStr = "segment_$Number%032d$.m4s";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "5";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "segment_00000000000000000000000000000005.m4s");
+}
+
+/**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_008
+ * @tc.desc  : Test GetSubstitutionStr with format width exceeds MAX_SUBSTITUTION_FORMAT_WIDTH (32)
+ *             Should use MAX_SUBSTITUTION_FORMAT_WIDTH
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_008, TestSize.Level0)
+{
+    std::string segTmpltStr = "segment_$Number%033d$.m4s";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "5";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "segment_00000000000000000000000000000005.m4s");
+}
+
+/**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_009
+ * @tc.desc  : Test GetSubstitutionStr with format width 0
+ *             Should not apply padding
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_009, TestSize.Level0)
+{
+    std::string segTmpltStr = "segment_$Number%00d$.m4s";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "5";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "segment_5.m4s");
+}
+
+/**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_010
+ * @tc.desc  : Test GetSubstitutionStr with negative format width
+ *             Should not apply padding
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_010, TestSize.Level0)
+{
+    std::string segTmpltStr = "segment_$Number%0-5d$.m4s";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "5";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "segment_5.m4s");
+}
+
+/**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_011
+ * @tc.desc  : Test GetSubstitutionStr with empty substitutionStr
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_011, TestSize.Level0)
+{
+    std::string segTmpltStr = "segment_$Number%05d$.m4s";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "segment_00000.m4s");
+}
+
+/**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_012
+ * @tc.desc  : Test GetSubstitutionStr with long substitutionStr
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_012, TestSize.Level0)
+{
+    std::string segTmpltStr = "segment_$Number%05d$.m4s";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "1234567890";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "segment_1234567890.m4s");
+}
+
+/**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_013
+ * @tc.desc  : Test GetSubstitutionStr with multiple identifiers
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_013, TestSize.Level0)
+{
+    std::string segTmpltStr = "$RepresentationID_$Number%05d$.m4s";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "123";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "$RepresentationID_00123.m4s");
+}
+
+/**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_014
+ * @tc.desc  : Test GetSubstitutionStr with identifier at the end
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_014, TestSize.Level0)
+{
+    std::string segTmpltStr = "segment_$Number%05d$";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "5";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "segment_00005");
+}
+
+/**
+ * @tc.name  : Test DashSubstituteTmpltStr with GetSubstitutionStr
+ * @tc.number: DashSubstituteTmpltStr_015
+ * @tc.desc  : Test GetSubstitutionStr with identifier at the beginning
+ */
+HWTEST_F(DashMpdUtilUnitTest, DashSubstituteTmpltStr_015, TestSize.Level0)
+{
+    std::string segTmpltStr = "$Number%05d$_segment.m4s";
+    std::string segTmpltIdentifier = "$Number";
+    std::string substitutionStr = "5";
+    int32_t ret = DashSubstituteTmpltStr(segTmpltStr, segTmpltIdentifier, substitutionStr);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(segTmpltStr, "00005_segment.m4s");
+}
+
+/**
  * @tc.name  : Test DashStrToDuration
  * @tc.number: DashStrToDuration_001
  * @tc.desc  : Test case 'D'
