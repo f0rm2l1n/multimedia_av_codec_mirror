334ca18e6d8d28c7199ffd99f319793850e2ae6a
liuxiang <liuxiang193@h-partners.com>
2026-03-14 22:30:29 +0800

    【录制框架】AVRecorder补充NDK接口
    Signed-off-by: liuxiang <liuxiang193@h-partners.com>

--- services/media_engine/filters/muxer_filter.cpp
+++ services/media_engine/filters/muxer_filter.cpp
@@ -173,6 +173,7 @@ Status MuxerFilter::DoResume()
 Status MuxerFilter::DoStop()
 {
     MediaAVCodec::AVCodecTrace trace("MuxerFilter::Stop");
+    FALSE_RETURN_V_MSG(mediaMuxer_ != nullptr, Status::ERROR_NULL_POINTER, "MediaMuxer is nullptr.");
     stopCount_++;
     MEDIA_LOG_I("MuxerFilter Stop, stopCount_: %{public}d, preFilterCount_: %{public}d",
         stopCount_, preFilterCount_);
@@ -206,12 +207,14 @@ void MuxerFilter::SetParameter(const std::shared_ptr<Meta> &parameter)
 {
     MEDIA_LOG_I("SetParameter");
     MediaAVCodec::AVCodecTrace trace("MuxerFilter::SetParameter");
+    FALSE_RETURN_V_MSG(mediaMuxer_ != nullptr, Status::ERROR_NULL_POINTER, "MediaMuxer is nullptr.");
     mediaMuxer_->SetParameter(parameter);
 }
 
 void MuxerFilter::SetUserMeta(const std::shared_ptr<Meta> &userMeta)
 {
     MEDIA_LOG_I("SetUserMeta enter");
+    FALSE_RETURN_V_MSG(mediaMuxer_ != nullptr, Status::ERROR_NULL_POINTER, "MediaMuxer is nullptr.");
     Status ret = mediaMuxer_->SetUserMeta(userMeta);
     if (ret != Status::OK) {
         MEDIA_LOG_I("SetUserMeta failed");
@@ -221,6 +224,7 @@ void MuxerFilter::SetUserMeta(const std::shared_ptr<Meta> &userMeta)
 Status MuxerFilter::SetCustomInfo(const std::shared_ptr<Meta> &customInfo)
 {
     MEDIA_LOG_I("SetCustomInfo enter");
+    FALSE_RETURN_V_MSG(mediaMuxer_ != nullptr, Status::ERROR_NULL_POINTER, "MediaMuxer is nullptr.");
     Status ret = mediaMuxer_->SetUserMeta(customInfo);
     if (ret != Status::OK) {
         MEDIA_LOG_I("SetCustomInfo failed");
