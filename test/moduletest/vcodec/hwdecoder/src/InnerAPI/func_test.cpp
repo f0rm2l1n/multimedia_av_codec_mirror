ca5656f6dd7e6c661bdd5c783a9a83091fa62213
gcw_0yKWaVfW <caoyafei6@h-partners.com>
2026-03-17 17:21:47 +0800

    新增解码用例
    Signed-off-by: gcw_0yKWaVfW <caoyafei6@h-partners.com>

--- test/moduletest/vcodec/hwdecoder/src/InnerAPI/func_test.cpp
+++ test/moduletest/vcodec/hwdecoder/src/InnerAPI/func_test.cpp
@@ -1733,8 +1733,8 @@ HWTEST_F(HwdecInnerFuncNdkTest, VIDEO_DECODE_INNER_GET_PIDS_0040, TestSize.Level
     if (cap != nullptr) {
         auto vDecSample = make_shared<VDecNdkInnerSample>();
         auto vDecSample2 = make_shared<VDecNdkInnerSample>();
-        if (vDecSample->CreateByName("OMX.hisi.video.decoder.avc.secure") == AVCS_ERR_OK && 
-        vDecSample2->CreateByName("OMX.hisi.video.decoder.avc.secure") == AVCS_ERR_OK) {
+        if (vDecSample->CreateByName("OMX.hisi.video.decoder.avc.secure") == AVCS_ERR_OK &&
+    vDecSample2->CreateByName("OMX.hisi.video.decoder.avc.secure") == AVCS_ERR_OK) {
             std::vector<pid_t> pidList;
             auto ret = AVCodecMonitor::GetActiveSecureDecoderPids(pidList);
             ASSERT_EQ(AVCS_ERR_OK, ret);
