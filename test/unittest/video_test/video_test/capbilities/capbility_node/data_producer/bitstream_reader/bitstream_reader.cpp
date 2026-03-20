dd3a9a5249f6878e9d913893663f2445ae3dfd8d
wangyanko <wangyongkai5@h-partners.com>
2026-03-20 09:51:43 +0800

    【修改说明】 video codec demo frame splitting
    Signed-off-by: wangyanko <wangyongkai5@h-partners.com>

--- test/unittest/video_test/video_test/capbilities/capbility_node/data_producer/bitstream_reader/bitstream_reader.cpp
+++ test/unittest/video_test/video_test/capbilities/capbility_node/data_producer/bitstream_reader/bitstream_reader.cpp
@@ -147,7 +147,7 @@ int32_t BitstreamReader::NalUnitReader::ReadNalUnit(uint8_t *bufferAddr, int32_t
 
     bufferSize = nalUnit_->size() - readSize_;
     auto ret =memcpy_s(bufferAddr, bufferSize, nalUnit_->data() + readSize_, bufferSize);
-    CHECK_AND_RETURN_LOG(ret == EOK, "ReadNalUnit failed");
+    CHECK_AND_RETURN_RET_LOG(ret == EOK, AVCODEC_SAMPLE_ERR_ERROR, "ReadNalUnit failed");
 
     if (bufferSize > bufferCapacity) {
         readSize_ = bufferSize;
